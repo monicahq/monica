3b0ef066b4dd99804c3a28a9ad3a47fe93b8eb7d
Regis Freyd <djaiss@users.noreply.github.com>
2017-05-29 21:35:25 -0400

    Remove encryption

--- database/migrations/2017_05_30_002239_remove_predefined_reminders.php
+++ database/migrations/2017_05_30_002239_remove_predefined_reminders.php
@@ -14,19 +14,19 @@ class RemovePredefinedReminders extends Migration
      */
     public function up()
     {
-        $reminders = Reminder::all();
-        foreach ($reminders as $reminder) {
-            echo $reminder->id.' ';
-            if (!is_null ($reminder->title)) {
-                $reminder->title = decrypt($reminder->title);
-            }
+        // $reminders = Reminder::all();
+        // foreach ($reminders as $reminder) {
+        //     echo $reminder->id.' ';
+        //     if (!is_null ($reminder->title)) {
+        //         $reminder->title = decrypt($reminder->title);
+        //     }
 
-            if (!is_null ($reminder->description)) {
-                $reminder->description = decrypt($reminder->description);
-            }
+        //     if (!is_null ($reminder->description)) {
+        //         $reminder->description = decrypt($reminder->description);
+        //     }
 
-            $reminder->save();
-        }
+        //     $reminder->save();
+        // }
 
         Schema::table('reminders', function (Blueprint $table) {
             $table->dropColumn(
