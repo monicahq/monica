b58de22e422c4f66665d9f35b4cc04844d7d4abe
Regis Freyd <djaiss@users.noreply.github.com>
2017-06-02 17:46:03 -0400

    Add Intercom

--- resources/views/layouts/skeleton.blade.php
+++ resources/views/layouts/skeleton.blade.php
@@ -40,5 +40,18 @@
       ga('send', 'pageview');
 
     </script>
+
+    @if (env('APP_ENV') == 'local')
+    <script>
+      window.intercomSettings = {
+        app_id: "j25qx4na",
+        name: "{{ \Auth::user()->first_name.' '.\Auth::user()->last_name }}",
+        email: "{{ \Auth::user()->email }}",
+        created_at: {{ \Auth::user()->created_at->timestamp }}
+      };
+    </script>
+    <script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/j25qx4na';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()
+    </script>
+    @endif
   </body>
 </html>
