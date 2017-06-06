c0952b835247d66c633a276773a98f4fa1d3865e
sikander <siqbal@gmail.com>
2017-06-06 19:07:34 -0400

    Remove ending periods

--- resources/lang/fr/passwords.php
+++ resources/lang/fr/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'password' => 'Les mots de passe doivent contenir au moins six caractères et doivent être identiques.',
-    'reset'    => 'Votre mot de passe a été réinitialisé !',
-    'sent'     => 'Nous vous avons envoyé par courriel le lien de réinitialisation du mot de passe !',
-    'token'    => "Ce jeton de réinitialisation du mot de passe n'est pas valide.",
-    'user'     => "Aucun utilisateur n'a été trouvé avec cette adresse e-mail.",
+    'password' => 'Les mots de passe doivent contenir au moins six caractères et doivent être identiques',
+    'reset'    => 'Votre mot de passe a été réinitialisé!',
+    'sent'     => 'Nous vous avons envoyé par courriel le lien de réinitialisation du mot de passe!',
+    'token'    => "Ce jeton de réinitialisation du mot de passe n'est pas valide",
+    'user'     => "Aucun utilisateur n'a été trouvé avec cette adresse e-mail",
 
 ];
