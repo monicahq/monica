68731f183ee696ebe46e6ca174c95990b2112954
sikander <siqbal@gmail.com>
2017-06-06 19:00:43 -0400

    Remove ending periods

--- resources/lang/en/journal.php
+++ resources/lang/en/journal.php
@@ -3,10 +3,10 @@
 return [
     'journal_add' => 'Add a journal entry',
     'journal_entry_delete' => 'Delete',
-    'entry_delete_success' => 'The journal entry has been deleted with success.',
+    'entry_delete_success' => 'The journal entry has been deleted successfully',
     'journal_add_title' => 'Title (optional)',
     'journal_add_post' => 'Entry',
     'journal_add_cta' => 'Save',
     'journal_blank_cta' => 'Add your first journal entry',
-    'journal_blank_description' => 'The journal lets you write events that happened to you, and remember them.',
+    'journal_blank_description' => 'The journal lets you write events that happened to you, and remember them',
 ];
