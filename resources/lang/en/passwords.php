bae1cd78864d67bdda1fc042ea16ee2311f78f7e
sikander <siqbal@gmail.com>
2017-06-06 19:02:05 -0400

    Rewording

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'password' => 'Passwords must be at least six characters and match the confirmation',
+    'reset' => 'Your password has been reset',
+    'sent' => 'The password reset link has been emailed to your email address',
+    'token' => 'This password reset token is invalid',
+    'user' => "Unable to find User with given email address",
 
 ];
