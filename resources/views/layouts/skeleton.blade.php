a1e32dfb617ed0a302a1f0cf4d948bb8f80d0fd0
Kovah <mail@kovah.de>
2017-06-07 09:51:45 +0200

    Do not output GA and Intercom scripts if no keys are set (#74)

--- resources/views/layouts/skeleton.blade.php
+++ resources/views/layouts/skeleton.blade.php
@@ -28,29 +28,31 @@
       var userList = new List('search-list', options);
     </script>
     <script src="{{ elixir('js/app.js') }}"></script>
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-      ga('create', '{{ env('GOOGLE_ANALYTICS') }}', 'auto');
-      ga('send', 'pageview');
+    @if(!empty(env('GOOGLE_ANALYTICS')))
+      <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-    </script>
+        ga('create', '{{ env('GOOGLE_ANALYTICS') }}', 'auto');
+        ga('send', 'pageview');
+      </script>
+    @endif
 
-    @if (env('APP_ENV') != 'local')
-    <script>
-      window.intercomSettings = {
-        app_id: "{{ env('INTERCOM') }}",
-        user_id: {{ \Auth::user()->id }},
-        name: "{{ \Auth::user()->first_name.' '.\Auth::user()->last_name }}",
-        email: "{{ \Auth::user()->email }}",
-        created_at: {{ \Auth::user()->created_at->timestamp }}
-      };
-    </script>
-    <script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/j25qx4na';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()
-    </script>
+    @if (env('APP_ENV') != 'local' && !empty(env('INTERCOM')))
+      <script>
+        window.intercomSettings = {
+          app_id: "{{ env('INTERCOM') }}",
+          user_id: {{ \Auth::user()->id }},
+          name: "{{ \Auth::user()->first_name.' '.\Auth::user()->last_name }}",
+          email: "{{ \Auth::user()->email }}",
+          created_at: {{ \Auth::user()->created_at->timestamp }}
+        };
+      </script>
+      <script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/j25qx4na';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()
+      </script>
     @endif
   </body>
 </html>
