95643d08cad8df6e7f0ed4e263396e5f9a73e1ee
Alexey Teterin <7018407@gmail.com>
2017-06-08 08:31:45 +0300

    russian translation, start

---
+++ resources/lang/ru/auth.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'These credentials do not match our records.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'not_authorized' => 'You are not authorized to execute his action.',
+    'signup_disabled' => 'The registration is currently disabled.',
+
+];
