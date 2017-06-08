39f163e55665aec5aca88544dbda4e4f1acb1014
Kirk Strauser <kirk@strauser.com>
2017-06-08 12:48:43 -0700

    Seed ActivityTypes, too

--- docker/test-server.sh
+++ docker/test-server.sh
@@ -4,5 +4,6 @@ ARTISAN=/var/www/monica/artisan
 
 php ${ARTISAN} migrate --force
 php ${ARTISAN} storage:link
+php ${ARTISAN} db:seed --class ActivityTypesTableSeeder
 php ${ARTISAN} db:seed --class CountriesSeederTable
 httpd -e info -DFOREGROUND
