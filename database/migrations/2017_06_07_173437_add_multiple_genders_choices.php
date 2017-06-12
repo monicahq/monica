1d136b3a3016523d779633396d8cf47144c8fad5
Marti Markov <marti.markov@icloud.com>
2017-06-13 00:16:13 +0100

    Grammar agnostic enum change

--- database/migrations/2017_06_07_173437_add_multiple_genders_choices.php
+++ database/migrations/2017_06_07_173437_add_multiple_genders_choices.php
@@ -1,11 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class AddMultipleGendersChoices extends Migration
 {
+
     /**
      * Run the migrations.
      *
@@ -13,8 +14,44 @@ class AddMultipleGendersChoices extends Migration
      */
     public function up()
     {
-        DB::statement("ALTER TABLE contacts CHANGE COLUMN gender gender ENUM('male', 'female', 'none')");
-        DB::statement("ALTER TABLE significant_others CHANGE COLUMN gender gender ENUM('male', 'female', 'none')");
-        DB::statement("ALTER TABLE kids CHANGE COLUMN gender gender ENUM('male', 'female', 'none')");
+        $enumValues = ['male', 'female', 'none'];
+        $this->migrateTables($enumValues);
     }
+
+    /**
+     * @param $enumValues
+     */
+    protected function migrateTables($enumValues)
+    {
+        $this->addNewGenderEnumsForTable('contacts', $enumValues);
+        $this->addNewGenderEnumsForTable('significant_others', $enumValues);
+        $this->addNewGenderEnumsForTable('kids', $enumValues);
+    }
+
+    private function addNewGenderEnumsForTable($tableName, $enumValues)
+    {
+        Schema::table($tableName, function (Blueprint $table) {
+            $table->renameColumn('gender', 'tmp_gender');
+        });
+
+        Schema::table($tableName, function (Blueprint $table) use ($enumValues) {
+            $table->enum('gender', $enumValues)->default('none');
+        });
+
+        $all = DB::table($tableName)->get();
+        foreach ($all as $item) {
+            DB::table($tableName)->where('id', $item['id'])->update(['gender' => $item['tmp_gender']]);
+        }
+
+        Schema::table($tableName, function (Blueprint $table) {
+            $table->dropColumn('tmp_gender');
+        });
+    }
+
+    public function down()
+    {
+        $enumValues = ['male', 'female'];
+        $this->migrateTables($enumValues);
+    }
+
 }
