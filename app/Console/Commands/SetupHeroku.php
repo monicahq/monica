d1824bb208d9f04365b12ba7fcb9d42f2eaf020d
Brandon Turner <brandon@Brandons-MacBook-Pro.local>
2017-06-12 03:35:48 +0100

    Updated setup command & app.json.

--- app/Console/Commands/SetupHeroku.php
+++ app/Console/Commands/SetupHeroku.php
@@ -4,21 +4,22 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
-class Setup extends Command
+class SetupHeroku extends Command
 {
+
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'setup:production';
+    protected $signature = 'setup:heroku {--force}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Perform setup of Monica';
+    protected $description = 'Perform setup of Monica when deployed to Heroku.';
 
     /**
      * Create a new command instance.
@@ -37,6 +38,17 @@ class Setup extends Command
      */
     public function handle()
     {
+        /**
+         * Check if the .env file exists, if not create one.
+         * If a .env file is absent, key generation will fail.
+         */
+        if( !file_exists('.env') )
+        {
+            touch('.env');
+            file_put_contents('.env', "HEROKU=true\r\nDB_CONNECTION=heroku");
+        }
+
+        $this->call('key:generate', ['--force' => true]);
         $this->call('migrate', ['--force' => true,]);
         $this->call('db:seed', ['--class' => 'ActivityTypesTableSeeder', '--force' => true]);
         $this->call('db:seed', ['--class' => 'CountriesSeederTable', '--force' => true]);
