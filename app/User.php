922fe52a39cf2911d65fe6127e8423509d0a97df
Zach Robichaud <zach2825@turtlebytes.com>
2017-06-25 15:47:15 -0700

    issues/345 - /people slow with many contacts
    Removed extra comma from the fillable array

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'timezone', 'locale', 'currency_id', 'fluid_container', 'name_order',
+        'name', 'email', 'password', 'timezone', 'locale', 'currency_id', 'fluid_container', 'name_order'
     ];
 
     /**
