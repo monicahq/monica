ec48e9c7113f91afe56146ee99836c4990d55aea
Unknown <dwe@eganrm.com>
2017-06-27 01:05:59 -0700

    Fix migration class name
    Fixed migration class name

--- database/migrations/2017_06_25_200318_add_contact_avatar_location.php
+++ database/migrations/2017_06_25_200318_add_contact_avatar_location.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddAvatarLocationContacts extends Migration
+class AddContactAvatarLocation extends Migration
 {
     /**
      * Run the migrations.
