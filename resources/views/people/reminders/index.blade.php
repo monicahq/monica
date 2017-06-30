faf9a4841e1fab32053f332b0929db37f42560c4
Rocco Palladino <rpalladino@gmail.com>
2017-06-30 10:51:18 -0500

    Fix display of reminders on people detail view (#244)
    - Display reminders on the people detail view consistently with
    activities, gifts, etc., using a list rather than table.
    - Remove obsolete strings from translation files.

--- resources/views/people/reminders/index.blade.php
+++ resources/views/people/reminders/index.blade.php
@@ -25,55 +25,38 @@
 
     <p>{{ trans('people.reminders_description') }}</p>
 
-    <table class="table table-sm table-hover">
-      <thead>
-        <tr>
-          <th>{{ trans('people.reminders_date') }}</th>
-          <th>{{ trans('people.reminders_frequency') }}</th>
-          <th>{{ trans('people.reminders_content') }}</th>
-          <th class="actions">{{ trans('people.reminders_actions') }}</th>
-        </tr>
-      </thead>
-      <tbody>
-        @foreach($contact->reminders as $reminder)
-          <tr>
-            <td class="date">{{ \App\Helpers\DateHelper::getShortDate($reminder->getNextExpectedDate()) }}</td>
-
-            <td class="date">
-              @if ($reminder->frequency_type != 'one_time')
-                {{ trans_choice('people.reminder_frequency_'.$reminder->frequency_type, $reminder->frequency_number, ['number' => $reminder->frequency_number]) }}
-              @else
-                {{ trans('people.reminders_one_time') }}
-              @endif
-            </td>
-
-            <td>
-              {{ $reminder->getTitle() }}
-            </td>
-
-            <td class="actions">
-
-              {{-- Only display this if the reminder can be deleted - ie if it's not a reminder added automatically for birthdates --}}
-              @if ($reminder->is_birthday == 'false')
-              <div class="reminder-actions">
-                <ul class="horizontal">
-                  <li><a href="/people/{{ $contact->id }}/reminders/{{ $reminder->id }}/delete" onclick="return confirm('{{ trans('people.reminders_delete_confirmation') }}')">{{ trans('people.reminders_delete_cta') }}</a></li>
-                </ul>
-              </div>
-              @endif
-
-            </td>
-
+    <ul class="table">
+      @foreach($contact->reminders as $reminder)
+      <li class="table-row">
+        <div class="table-cell date">
+          {{ \App\Helpers\DateHelper::getShortDate($reminder->getNextExpectedDate()) }}
+        </div>
+        <div class="table-cell frequency-type">
+          @if ($reminder->frequency_type != 'one_time')
+            {{ trans_choice('people.reminder_frequency_'.$reminder->frequency_type, $reminder->frequency_number, ['number' => $reminder->frequency_number]) }}
+          @else
+            {{ trans('people.reminders_one_time') }}
+          @endif
+        </div>
+        <div class="table-cell title">
+          {{ $reminder->getTitle() }}
+        </div>
+        <div class="table-cell comment">
             @if (!is_null($reminder->getDescription()))
-            <td class="reminder-comment">
               {{ $reminder->getDescription() }}
-            </td>
             @endif
-
-          </tr>
-        @endforeach
-      </tbody>
-    </table>
+        </div>
+        <div class="table-cell list-actions">
+          {{-- Only display this if the reminder can be deleted - ie if it's not a reminder added automatically for birthdates --}}
+          @if ($reminder->is_birthday == 'false')
+            <a href="/people/{{ $contact->id }}/reminders/{{ $reminder->id }}/delete" onclick="return confirm('{{ trans('people.reminders_delete_confirmation') }}')">
+              <i class="fa fa-trash-o" aria-hidden="true"></i>
+            </a>
+          @endif
+        </div>
+      </li>
+      @endforeach
+    </ul>
   </div>
 
 @endif
