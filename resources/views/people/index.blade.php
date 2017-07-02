876c8bbecba5d4a0c48ace295a613305ca169df3
Régis Freyd <djaiss@users.noreply.github.com>
2017-07-02 10:13:50 -0400

    Fix kid counts number on dashboard (#423)
    Fix #416

--- resources/views/people/index.blade.php
+++ resources/views/people/index.blade.php
@@ -107,7 +107,7 @@
                     </span>
 
                     <span class="people-list-item-information">
-                      {{ trans_choice('people.people_list_number_kids', $contact->kids_count, ['count' => $contact->kids_count]) }} <br />
+                      {{ trans_choice('people.people_list_number_kids', $contact->getNumberOfKids(), ['count' => $contact->getNumberOfKids()]) }} <br />
                       <span>{{ trans('people.people_list_last_updated') }} {{ \App\Helpers\DateHelper::getShortDate($contact->updated_at) }}</span>
                     </span>
                   </a>
