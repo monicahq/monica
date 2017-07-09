d5792ffe2dc64db8fb83fdbdb398ed9eef326828
Voryzen <piro.travesura@gmail.com>
2017-07-10 06:08:52 +1000

    Fix journal deletion translation key (#437)

--- resources/lang/en/journal.php
+++ resources/lang/en/journal.php
@@ -3,10 +3,11 @@
 return [
     'journal_add' => 'Add a journal entry',
     'journal_entry_delete' => 'Delete',
-    'entry_delete_success' => 'The journal entry has been deleted with success.',
+    'entry_delete_success' => 'The journal entry has been successfully deleted.',
     'journal_add_title' => 'Title (optional)',
     'journal_add_post' => 'Entry',
     'journal_add_cta' => 'Save',
     'journal_blank_cta' => 'Add your first journal entry',
     'journal_blank_description' => 'The journal lets you write events that happened to you, and remember them.',
+    'delete_confirmation' => 'Are you sure you want to delete this journal entry?',
 ];
