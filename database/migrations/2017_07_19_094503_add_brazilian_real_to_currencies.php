dfb91ed3ad2bbeb3bb134ba5c808b153a3c17232
Neri Junior <neri@nerijunior.com>
2017-07-19 10:46:03 -0300

    brazilian real added to currencies in migration

---
+++ database/migrations/2017_07_19_094503_add_brazilian_real_to_currencies.php
@@ -0,0 +1,17 @@
+<?php
+
+use DB;
+use Illuminate\Database\Migrations\Migration;
+
+class AddBrazilianRealToCurrencies extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('currencies')->insert(['iso' => 'BRL', 'name' => 'Brazilian Real', 'symbol' => 'R$']);
+    }
+}
