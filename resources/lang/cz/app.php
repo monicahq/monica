00eb17aad0c5fe35f05b31dec6f8fb75cf19e9e3
Jaroslav Lichtblau <svetlemodry@users.noreply.github.com>
2017-07-19 03:42:23 +0200

    Add Czech translation (#466)

---
+++ resources/lang/cz/app.php
@@ -0,0 +1,47 @@
+<?php
+
+return [
+    'update' => 'Aktualizovat',
+    'save' => 'Uložit',
+    'add' => 'Přidat',
+    'cancel' => 'Zrušit',
+    'delete' => 'Smazat',
+    'edit' => 'Upravit',
+    'upload' => 'Nahrát',
+
+    'header_settings_link' => 'Nastavení',
+    'header_logout_link' => 'Odhlásit',
+
+    'main_nav_cta' => 'Přidat osobu',
+    'main_nav_dashboard' => 'Dashboard',
+    'main_nav_family' => 'Osoby',
+    'main_nav_journal' => 'Deník',
+    'main_nav_activities' => 'Aktivity',
+    'main_nav_tasks' => 'Úkoly',
+    'main_nav_trash' => 'Koš',
+
+    'footer_remarks' => 'Další poznámky?',
+    'footer_send_email' => 'Poslat email',
+    'footer_privacy' => 'Podmínky používání',
+    'footer_release' => 'Poznámky k vydání',
+    'footer_newsletter' => 'Newsletter',
+    'footer_source_code' => 'Monica na GitHubu',
+    'footer_version' => 'Verze: :version',
+
+    'breadcrumb_dashboard' => 'Dashboard',
+    'breadcrumb_list_contacts' => 'Seznam kontaktů',
+    'breadcrumb_journal' => 'Deník',
+    'breadcrumb_settings' => 'Nastavení',
+    'breadcrumb_settings_export' => 'Export',
+    'breadcrumb_settings_users' => 'Uživatelé',
+    'breadcrumb_settings_users_add' => 'Přidat uživatele',
+    'breadcrumb_settings_subscriptions' => 'Odběry',
+    'breadcrumb_settings_import' => 'Import',
+    'breadcrumb_settings_import_report' => 'Importovat report',
+    'breadcrumb_settings_import_upload' => 'Nahrát',
+    'breadcrumb_settings_tags' => 'Tagy',
+
+    'gender_male' => 'Muž',
+    'gender_female' => 'Žena',
+    'gender_none' => 'Nepovím',
+];
