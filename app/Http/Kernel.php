0b8efa11f6ae4135d638309190bba7c68389e5be
David Egan <david@eganrm.com>
2017-08-19 23:04:44 -0700

    Created middleware to check if oauth provider is supported
    Middleware checks config/socail.php for requested service
    Checks if requested service is disabled

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,5 +54,6 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        'social' => \App\Http\Middleware\Social::class,
     ];
 }
