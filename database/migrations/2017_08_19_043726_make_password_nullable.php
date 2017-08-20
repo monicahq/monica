4b137f268490417fe929e253339415fd2823fefa
David Egan <david@eganrm.com>
2017-08-19 23:00:20 -0700

    Added table for tracking user linked social accounts per user
    Created table to track users linked social accounts
    Set password field in user tabled to nullable, no password will be set if login via oauth

---
+++ database/migrations/2017_08_19_043726_make_password_nullable.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MakePasswordNullable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('password')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('password')->change();
+        });
+    }
+}
