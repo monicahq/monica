2e57aabc5e3cbf9163bbf712b8aa63904daa74b6
Regis Freyd <djaiss@users.noreply.github.com>
2017-08-21 19:25:24 -0400

    New blank dashboard screen

--- resources/lang/pt-br/dashboard.php
+++ resources/lang/pt-br/dashboard.php
@@ -1,6 +1,10 @@
 <?php
 
 return [
+    'dashboard_blank_title' => 'Welcome to your account!',
+    'dashboard_blank_description' => 'Monica is the place to organize all the interactions you have with the ones you care about.',
+    'dashboard_blank_cta' => 'Add your first contact',
+
     'reminders_title' => 'Próximos lembretes',
     'reminders_blank_description' => 'Não há lembretes planejados.',
     'event_title' => 'Últimas ações',
@@ -23,9 +27,6 @@ return [
     'event_update_debt' => 'updated a debt',
     'event_create_gift' => 'added a gift',
     'event_update_gift' => 'updated a gift',
-
-    'blank_title' => 'Você ainda não possui nenhuma atividade.',
-    'blank_cta' => 'Adicionar contato',
 
     'tab_last_edited_contacts' => 'Últimos contatos editados',
     'tab_whats_coming' => 'O que está por vir',
