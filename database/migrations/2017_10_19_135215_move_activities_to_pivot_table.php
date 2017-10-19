b11e80af37d465ad97b39be3b725ec77b3b056e4
Michael Heap <m@michaelheap.com>
2017-10-19 18:57:55 +0100

    Add migrations to create activity_contact table and move existing data

---
+++ database/migrations/2017_10_19_135215_move_activities_to_pivot_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MoveActivitiesToPivotTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Select all activities
+        $activities = DB::table('activities')->select('id', 'contact_id')->get();
+
+        foreach ($activities as $activity) {
+            DB::table('activity_contact')->insert(
+                ['contact_id' => $activity->contact_id, 'activity_id' => $activity->id]
+            );
+        }
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::table('activity_contact')->truncate();
+    }
+}
