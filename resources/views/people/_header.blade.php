8c903addf81c9e214383a84c6a4e80880aaddcb5
Regis Freyd <djaiss@users.noreply.github.com>
2017-12-13 11:39:14 -0500

    Fix errors when deploying to Heroku
    Updated composer.json to match the current version of Laravel (5.5).

--- resources/views/people/_header.blade.php
+++ resources/views/people/_header.blade.php
@@ -13,7 +13,7 @@
             @if (! is_null($contact->gravatar_url))
               <img src="{{ $contact->gravatar_url }}" width="87">
             @else
-              @if (count($contact->getInitials()) == 1)
+              @if (strlen($contact->getInitials()) == 1)
               <div class="avatar one-letter" style="background-color: {{ $contact->getAvatarColor() }};">
                 {{ $contact->getInitials() }}
               </div>
