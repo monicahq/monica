6166c1b9158383293371df9e838756ead8cde561
Regis Freyd <djaiss@users.noreply.github.com>
2017-12-21 14:29:14 -0500

    i18n

--- resources/lang/de/journal.php
+++ resources/lang/de/journal.php
@@ -2,8 +2,13 @@
 
 return [
     'journal_add' => 'Tagebucheintrag hinzufügen',
-    'journal_entry_delete' => 'Löschen',
-    'entry_delete_success' => 'Der Eintrag wurde erfolgreich gelöscht.',
+    'journal_rate' => 'How was your day? You can rate it once a day.',
+    'journal_come_back' => 'Thanks. Come back tomorrow to rate your day again.',
+    'journal_description' => 'Note: the journal lists both manual journal entries, and automatic entries like Activities done with your contacts. While you can delete journal entries manually, you\'ll have to delete the activity directly on the contact page.',
+    'journal_created_automatically' => 'Created automatically',
+    'journal_entry_type_journal' => 'Journal entry',
+    'journal_entry_type_activity' => 'Activity',
+    'journal_entry_rate' => 'You rated your day.',
     'journal_add_title' => 'Titel (optional)',
     'journal_add_post' => 'Eintrag',
     'journal_add_cta' => 'Speichern',
