b470e986e02fc7649ffda6a8f1e9ed04aa53c156
Danny Waldron <jenosdeaddog@gmail.com>
2018-01-04 13:09:54 -0600

    Testing Fix #5

--- resources/views/people/modal/activity_view.blade.php
+++ resources/views/people/modal/activity_view.blade.php
@@ -3,7 +3,7 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h7 class="modal-title">{{ $activity->getSummary() }}</h7>
+        {{ $activity->getSummary() }}
         <button type="button" class="close" data-dismiss="modal">
           <span aria-hidden="true">&times;</span>
         </button>
