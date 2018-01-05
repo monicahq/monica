46a8f9390cf2dfb97dfbaec8ea08a80ed2341d1b
Alexis Saettler <alexis@saettler.org>
2018-01-05 17:04:06 +0100

    Try with a new script (but no analysis on external PR)

---
+++ travis-sonar.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+COMMON_PARAMS=-Dsonar.php.tests.reportPath=junit.xml \
+              -Dsonar.php.coverage.reportPaths=phpunit.coverage.xml \
+              -Dsonar.projectVersion=$(php artisan monica:getversion)
+
+if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo 'Analyze master'
+  sonar-scanner $COMMON_PARAMS \
+    -Dsonar.analysis.buildNumber=$TRAVIS_BUILD_NUMBER \
+    -Dsonar.analysis.pipeline=$TRAVIS_BUILD_NUMBER \
+    -Dsonar.analysis.sha1=$TRAVIS_COMMIT \
+    -Dsonar.analysis.repository=$TRAVIS_REPO_SLUG
+
+elif [[ "$TRAVIS_BRANCH" == "branch-"* ]] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo 'Analyze release branch'
+  sonar-scanner $COMMON_PARAMS \
+    -Dsonar.branch.name=$TRAVIS_BRANCH \
+    -Dsonar.analysis.buildNumber=$TRAVIS_BUILD_NUMBER \
+    -Dsonar.analysis.pipeline=$TRAVIS_BUILD_NUMBER \
+    -Dsonar.analysis.sha1=$TRAVIS_COMMIT \
+    -Dsonar.analysis.repository=$TRAVIS_REPO_SLUG
+
+elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN:-}" ]; then
+  echo 'Analyze internal pull request'
+  sonar-scanner $COMMON_PARAMS \
+    -Dsonar.branch.name=$TRAVIS_PULL_REQUEST_BRANCH \
+    -Dsonar.branch.target=$TRAVIS_BRANCH \
+    -Dsonar.analysis.buildNumber=$TRAVIS_BUILD_NUMBER \
+    -Dsonar.analysis.pipeline=$TRAVIS_BUILD_NUMBER \
+    -Dsonar.analysis.sha1=$TRAVIS_PULL_REQUEST_SHA \
+    -Dsonar.analysis.prNumber=$TRAVIS_PULL_REQUEST \
+    -Dsonar.analysis.repository=$TRAVIS_REPO_SLUG \
+    -Dsonar.pullrequest.github.id=$TRAVIS_PULL_REQUEST \
+    -Dsonar.pullrequest.github.repository=$TRAVIS_REPO_SLUG
+
+else
+  echo 'No analysis for external pull request .. yet'
+
+fi
