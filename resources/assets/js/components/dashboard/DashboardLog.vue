d28cf3c6f788a0c3a72ce9a337231a14d07e3028
Regis Freyd <djaiss@users.noreply.github.com>
2018-01-06 10:24:40 -0500

    Display avatar in notes section on dashboard

--- resources/assets/js/components/dashboard/DashboardLog.vue
+++ resources/assets/js/components/dashboard/DashboardLog.vue
@@ -43,7 +43,7 @@
       <div v-if="activeTab == 'notes'">
         <div class="pb3 cf" v-for="note in notes" v-if="notes.length != 0">
           <div class="fl w-10">
-            <avatar v-bind:contact="note.contact"></avatar>
+            <avatar v-bind:contact="note.contact" v-bind:clickable="true"></avatar>
           </div>
           <div class="pl3 fl w-90">
             <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="15px" height="15px" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve">
