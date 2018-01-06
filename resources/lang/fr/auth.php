8d0e1bf63f2c1d644fbd9bef02a85dd1726c2d71
Alexis Saettler <alexis@saettler.org>
2018-01-06 03:02:23 +0100

    Fix stylceci

--- resources/lang/fr/auth.php
+++ resources/lang/fr/auth.php
@@ -21,5 +21,5 @@ return [
     '2fa_wrong_validation' => 'The two factor authentication has failed.',
     '2fa_one_time_password' => 'Authentication code',
     '2fa_recuperation_code' => 'Enter a two factor recovery code',
-    
+
 ];
