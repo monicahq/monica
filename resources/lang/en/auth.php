620c5de2b04ce38d234ee86d18017e3a735d5798
Alexis Saettler <alexis@saettler.org>
2018-01-09 00:17:06 +0100

    Add Two Factor Authentication (2FA) (#724)

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -18,5 +18,9 @@ return [
     'not_authorized' => 'You are not authorized to execute this action',
     'signup_disabled' => 'Registration is currently disabled',
     'back_homepage' => 'Back to homepage',
+    '2fa_title' => 'Two Factor Authentication',
+    '2fa_wrong_validation' => 'The two factor authentication has failed.',
+    '2fa_one_time_password' => 'Authentication code',
+    '2fa_recuperation_code' => 'Enter a two factor recovery code',
 
 ];
