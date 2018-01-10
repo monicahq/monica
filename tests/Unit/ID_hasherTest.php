f214e74569d19534f21c4e6fe210e3339d70ef04
Danny Waldron <jenosdeaddog@gmail.com>
2018-01-10 09:51:25 -0600

    Add tests

---
+++ tests/Unit/ID_hasherTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Helpers\ID_hasher;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ID_hasherTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testPrependH()
+    {
+        $ID_hasher = new ID_hasher();
+
+        $test_id = rand();
+
+        $test_hash = $ID_hasher->encode_id($test_id);
+
+        $test_value = ('h' == substr($test_hash,0,1));
+
+        $this->assertTrue($test_value);
+    }
+
+    public function testGetIDback()
+    {
+        $ID_hasher = new ID_hasher();
+
+        $test_id = rand();
+
+        $test_hash = $ID_hasher->encode_id($test_id);
+
+        $test_value = ($test_id == $ID_hasher->decode_id($test_hash));
+
+        $this->assertTrue($test_value);
+
+        $test_value = ($test_id == $ID_hasher->decode_id($test_id));
+
+        $this->assertTrue($test_value);
+    }
+}
