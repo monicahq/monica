92e98516e93e6a293f54876c20923b3f6752b806
Steven Maguire <stevenmaguire@gmail.com>
2018-01-18 18:02:56 -0600

    Add pet api support (#779)

---
+++ app/Http/Resources/Pet/PetCategory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources\Pet;
+
+use Illuminate\Http\Resources\Json\Resource;
+
+class PetCategory extends Resource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'object' => 'pet_category',
+            'name' => $this->name,
+            'is_common' => (bool) $this->is_common,
+        ];
+    }
+}
