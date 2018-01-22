4f3994f94d220969db8537d9ef93c73f7e61b4a3
Alexis Saettler <alexis@saettler.org>
2018-01-22 18:00:31 +0100

    Add Code Coverage to Browser tests (#813)
    * Using Live-code-coverage to get Code Coverage from http server
    * Add a new test to pass Login form
    * Add FakeUserTableSeeder to seed in testing env
    * Using SESSION_DRIVER=file in travis env
    * Add saucelabs connection (disable right now)
    * Update composer

--- tests/BrowserSelenium/BaseTestCase.php
+++ tests/BrowserSelenium/BaseTestCase.php
@@ -4,6 +4,7 @@ namespace Tests\BrowserSelenium;
 
 use Lmc\Steward\ConfigProvider;
 use Lmc\Steward\Test\AbstractTestCase;
+use Facebook\WebDriver\WebDriverExpectedCondition;
 
 /**
  * Abstract class for custom tests, could eg. define some properties or instantiate some common components in setUp().
@@ -24,7 +25,10 @@ abstract class BaseTestCase extends AbstractTestCase
         // Set base url according to environment
         switch (ConfigProvider::getInstance()->env) {
             case 'dev':
-                self::$baseUrl = env('APP_URL');
+                self::$baseUrl = 'http://monica.test/'; // env('APP_URL');
+                break;
+            case 'travis':
+                self::$baseUrl = 'http://localhost:8000/';
                 break;
             case 'laravel':
                 self::$baseUrl = 'http://127.0.0.1:8000/';
@@ -41,5 +45,43 @@ abstract class BaseTestCase extends AbstractTestCase
         if (ConfigProvider::getInstance()->env == 'production') {
             $this->warn('The tests are run against production, so be careful!');
         }
+    }
+
+    /**
+     * Init the WebDriver.
+     * (init should be run with "before" phpunit annotation, but it doesn't work !).
+     */
+    public function init()
+    {
+        $this->wd->get(self::$baseUrl);
+    }
+
+    /**
+     * Init WebDriver and pass the login form.
+     */
+    public function initAndLogin()
+    {
+        $this->init();
+
+        if ($this->getCurrentPath() == '/') {
+            //$url = $this->wd->getCurrentURL();
+            $this->findById('email')->sendKeys('admin@admin.com');
+            $this->findById('password')->sendKeys('admin');
+            $this->findByTag('button')->submit();
+
+            $this->wd->wait()->until(
+                WebDriverExpectedCondition::urlContains('/dashboard')
+            );
+        }
+    }
+
+    /**
+     * Get the current url path.
+     */
+    public function getCurrentPath()
+    {
+        return urldecode(
+            parse_url($this->wd->getCurrentURL(), PHP_URL_PATH)
+        );
     }
 }
--- tests/BrowserSelenium/SimpleTest.php
+++ tests/BrowserSelenium/SimpleTest.php
@@ -5,19 +5,26 @@ namespace Tests\BrowserSelenium;
 class SimpleTest extends BaseTestCase
 {
     /**
-     * init should be run with "before" phpunit annotation, but it doesn't work !
-     */
-    public function init()
-    {
-        $this->wd->get(self::$baseUrl);
-    }
-
-    /**
      * A basic browser test example.
      */
     public function testBasicExample()
     {
         $this->init();
+
         $this->assertContains('Monica', $this->wd->getTitle());
+        $this->assertEquals('/', $this->getCurrentPath());
+    }
+
+    /**
+     * Test Login page.
+     */
+    public function testLogin()
+    {
+        $this->initAndLogin();
+        $this->assertContains('Monica', $this->wd->getTitle());
+        $this->assertEquals('/dashboard', $this->getCurrentPath());
+
+        $link = $this->findByXpath("//a[@href='/logout']");
+        $this->assertNotNull($link);
     }
 }
