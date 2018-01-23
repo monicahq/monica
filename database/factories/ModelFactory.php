da00115209e0d8d192baf2a2156a41073d14c5a0
Regis Freyd <djaiss@users.noreply.github.com>
2018-01-22 23:43:01 -0500

    Extends unit test for User model

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -146,6 +146,12 @@ $factory->define(App\Entry::class, function (Faker\Generator $faker) {
     ];
 });
 
+$factory->define(App\Day::class, function (Faker\Generator $faker) {
+    return [
+        'account_id' => 1,
+    ];
+});
+
 $factory->define(App\ContactFieldType::class, function (Faker\Generator $faker) {
     return [
         'id' => 1,
