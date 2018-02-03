a57c1f94c896fc6e5f8e57ef7be98de4c18467e0
Alexis Saettler <alexis@saettler.org>
2018-02-03 03:08:56 +0100

    Fix StyleCI

--- database/migrations/2017_10_14_083556_change_gift_column_structure.php
+++ database/migrations/2017_10_14_083556_change_gift_column_structure.php
@@ -13,11 +13,9 @@ class ChangeGiftColumnStructure extends Migration
      */
     public function up()
     {
-        if (DB::connection()->getDriverName() == 'pgsql')
-        {
+        if (DB::connection()->getDriverName() == 'pgsql') {
             DB::statement('ALTER TABLE gifts ALTER COLUMN about_object_id TYPE integer USING (trim(about_object_id)::integer);');
-        }else
-        {
+        } else {
             DB::statement('ALTER TABLE gifts MODIFY about_object_id INTEGER;');
         }
 
