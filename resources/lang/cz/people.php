16d91437d55e824ed54386a6c6a8f47d386f340b
Alexis Saettler <alexis@saettler.org>
2018-02-03 11:48:31 +0100

    Add gifts_update_success

--- resources/lang/cz/people.php
+++ resources/lang/cz/people.php
@@ -273,6 +273,7 @@ return [
     'gifts_received' => 'Gifts received',
     'gifts_view_comment' => 'View comment',
     'gifts_mark_offered' => 'Mark as offered',
+    'gifts_update_success' => 'The gift has been updated successfully',
 
     // debts
     'debt_delete_confirmation' => 'Opravdu chcete smazat tento dluh?',
