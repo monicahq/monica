670bfae4eef0e2b74a370b1d7a970a1d964132ca
Régis Freyd <djaiss@users.noreply.github.com>
2018-02-25 19:26:56 -0500

    New translations mail.php (Russian)

--- resources/lang/ru/mail.php
+++ resources/lang/ru/mail.php
@@ -8,4 +8,7 @@ return [
     'for' => 'О:',
     'footer_contact_info' => 'Добавить, просмотреть, завершить и изменить информацию об этом контакте',
 
+    'notification_subject_line' => 'You have an upcoming event',
+    'notification_description' => 'In :count days (on :date), the following event will happen:',
+
 ];
