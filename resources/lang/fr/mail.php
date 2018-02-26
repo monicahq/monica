2a4a2c09ecbe5a02d35b7c987fd5d4c24c81e5d2
Régis Freyd <djaiss@users.noreply.github.com>
2018-02-25 19:39:28 -0500

    New translations mail.php (French)

--- resources/lang/fr/mail.php
+++ resources/lang/fr/mail.php
@@ -2,13 +2,13 @@
 
 return [
 
-    'subject_line' => 'Reminder for :contact',
-    'greetings' => 'Hi :username',
-    'want_reminded_of' => 'YOU WANTED TO BE REMINDED OF',
-    'for' => 'FOR:',
-    'footer_contact_info' => 'Add, view, complete, and change information about this contact:',
+    'subject_line' => 'Rappel pour :contact',
+    'greetings' => 'Bonjour :username',
+    'want_reminded_of' => 'VOUS VOULIEZ ÊTRE RAPPELÉ DE',
+    'for' => 'POUR:',
+    'footer_contact_info' => 'Ajouter, afficher, compléter et modifier les informations sur ce contact :',
 
-    'notification_subject_line' => 'You have an upcoming event',
-    'notification_description' => 'In :count days (on :date), the following event will happen:',
+    'notification_subject_line' => 'Vous avez 1 événement à venir',
+    'notification_description' => 'Dans :count les jours (le :date), l’événement suivant se produira :',
 
 ];
