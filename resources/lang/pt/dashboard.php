8baea5ee7e46bececc409e95b761945db649a754
Régis Freyd <djaiss@users.noreply.github.com>
2018-03-01 19:00:59 -0500

    New translations dashboard.php (Portuguese)

--- resources/lang/pt/dashboard.php
+++ resources/lang/pt/dashboard.php
@@ -2,7 +2,7 @@
 
 return [
     'dashboard_blank_title' => 'Seja bem-vindo à sua conta!',
-    'dashboard_blank_description' => 'Monica is the place to organize all the interactions you have with the ones you care about.',
+    'dashboard_blank_description' => 'Com Monica pode organizar todas as suas interações com as pessoas que são importantes para si.',
     'dashboard_blank_cta' => 'Adicione o seu primeiro contacto',
 
     'notes_title' => 'You don\'t any notes yet.',
