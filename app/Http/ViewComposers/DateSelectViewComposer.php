1d423dfbeb6074b4b6b80474af656be931f2f647
Alexis Saettler <alexis@saettler.org>
2018-03-02 15:23:58 +0100

    Use DateHelper::getListOfMonths

--- app/Http/ViewComposers/DateSelectViewComposer.php
+++ app/Http/ViewComposers/DateSelectViewComposer.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\ViewComposers;
 
+use App\Helpers\DateHelper;
 use Illuminate\View\View;
 use Jenssegers\Date\Date;
 
@@ -20,15 +21,11 @@ class DateSelectViewComposer
     public function compose(View $view)
     {
         // Months
-        Date::setLocale(auth()->user()->locale);
-        $months = [];
-        $currentDate = Date::now();
-        $currentDate->day = 1;
-
-        for ($month = 1; $month < 13; $month++) {
-            $currentDate->month = $month;
-            array_push($months, mb_convert_case($currentDate->format('F'), MB_CASE_TITLE, 'UTF-8'));
-        }
+        $months = DateHelper::getListOfMonths();
+//        foreach (DateHelper::getListOfMonths() as $month)
+//        {
+//            array_push($months, $month['name']);
+//        }
 
         // Years
         $years = [];
