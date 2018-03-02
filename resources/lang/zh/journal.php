577d851821a74fe1ed71e631763d54521b8790d4
Régis Freyd <djaiss@users.noreply.github.com>
2018-03-01 21:00:53 -0500

    New translations journal.php (Chinese Simplified)

--- resources/lang/zh/journal.php
+++ resources/lang/zh/journal.php
@@ -1,19 +1,19 @@
 <?php
 
 return [
-    'journal_rate' => 'How was your day? You can rate it once a day.',
-    'journal_come_back' => 'Thanks. Come back tomorrow to rate your day again.',
-    'journal_description' => 'Note: the journal lists both manual journal entries, and automatic entries like Activities done with your contacts. While you can delete journal entries manually, you\'ll have to delete the activity directly on the contact page.',
-    'journal_add' => 'Add a journal entry',
+    'journal_rate' => '今天过得怎么样？你可以每天给它一次评价。',
+    'journal_come_back' => '谢谢. 明天再来给你的一天评价一下。',
+    'journal_description' => '注意: 记录里列出了全部手动记录的条目, 以及您与您的联系人进行的活动等自动条目。虽然可以手动删除记录条目, 但必须直接在 "联系人" 页上进行删除。',
+    'journal_add' => '添加记录条目',
     'journal_created_automatically' => 'Created automatically',
     'journal_entry_type_journal' => 'Journal entry',
     'journal_entry_type_activity' => 'Activity',
     'journal_entry_rate' => 'You rated your day.',
     'entry_delete_success' => 'The journal entry has been successfully deleted.',
     'journal_add_title' => 'Title (optional)',
-    'journal_add_post' => 'Entry',
-    'journal_add_cta' => 'Save',
-    'journal_blank_cta' => 'Add your first journal entry',
+    'journal_add_post' => '条目',
+    'journal_add_cta' => '保存',
+    'journal_blank_cta' => '添加您的第一个记录条目',
     'journal_blank_description' => 'The journal lets you write events that happened to you, and remember them.',
     'delete_confirmation' => 'Are you sure you want to delete this journal entry?',
 ];
