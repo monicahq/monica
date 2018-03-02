06d56e016494344ffa7c5777141f999d0be397a2
Regis Freyd <djaiss@users.noreply.github.com>
2018-03-01 20:07:05 -0500

    clean test

--- tests/Unit/Jobs/ScheduleNotificationTest.php
+++ tests/Unit/Jobs/ScheduleNotificationTest.php
@@ -44,40 +44,4 @@ class ScheduleNotificationTest extends TestCase
 
         Bus::assertDispatched(ScheduleNotification::class);
     }
-
-    // public function test_it_sets_the_number_of_emails_required_to_eventually_deleted_the_notification()
-    // {
-    //     Mail::fake();
-
-    //     Carbon::setTestNow(Carbon::create(2017, 1, 1, 7, 0, 0));
-
-    //     $account = factory('App\Account')->create([
-    //         'default_time_reminder_is_sent' => '07:00',
-    //     ]);
-    //     $contact = factory('App\Contact')->create(['account_id' => $account->id]);
-    //     $user = factory('App\User')->create([
-    //         'account_id' => $account->id,
-    //         'email' => 'john@doe.com',
-    //     ]);
-    //     $reminder = factory('App\Reminder')->create([
-    //         'account_id' => $account->id,
-    //         'contact_id' => $contact->id,
-    //         'next_expected_date' => '2017-01-01',
-    //     ]);
-    //     $notification = factory('App\Notification')->create([
-    //         'account_id' => $account->id,
-    //         'contact_id' => $contact->id,
-    //         'reminder_id' => $reminder->id,
-    //     ]);
-
-    //     dispatch(new SendNotificationEmail($notification, $user));
-
-        // Mail::assertSent(NotificationEmail::class, function ($mail) {
-        //     return $mail->hasTo('john@doe.com');
-        // });
-
-    //     Mail::assertNotSent(NotificationEmail::class, function ($mail) {
-    //         return $mail->hasTo('jane@doe.com');
-    //     });
-    // }
 }
