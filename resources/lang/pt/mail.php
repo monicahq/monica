a0b4deb353f37d3c659e8ee37c5e96eae0e391fd
Régis Freyd <djaiss@users.noreply.github.com>
2018-03-03 09:35:14 -0500

    Update translations for it, pt, cz, ru, fr, de, cs (#940)

--- resources/lang/pt/mail.php
+++ resources/lang/pt/mail.php
@@ -8,7 +8,7 @@ return [
     'for' => 'PARA:',
     'footer_contact_info' => 'Adicionar, visualizar, completar e alterar informações sobre este contato:',
 
-    'notification_subject_line' => 'You have an upcoming event',
-    'notification_description' => 'In :count days (on :date), the following event will happen:',
+    'notification_subject_line' => 'Você tem um evento futuro',
+    'notification_description' => 'Em :count dias (em :date), acontecerá o evento seguinte:',
 
 ];
