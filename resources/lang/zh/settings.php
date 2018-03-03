50cc3253c3f217cf4407cddebf9f4abf7662378a
Régis Freyd <djaiss@users.noreply.github.com>
2018-03-03 03:52:21 -0500

    New translations settings.php (Chinese Simplified)

--- resources/lang/zh/settings.php
+++ resources/lang/zh/settings.php
@@ -1,21 +1,21 @@
 <?php
 
 return [
-    'sidebar_settings' => 'Account settings',
-    'sidebar_personalization' => 'Personalization',
-    'sidebar_settings_export' => 'Export data',
-    'sidebar_settings_users' => 'Users',
-    'sidebar_settings_subscriptions' => 'Subscription',
-    'sidebar_settings_import' => 'Import data',
-    'sidebar_settings_tags' => 'Tags management',
+    'sidebar_settings' => '帐户设置',
+    'sidebar_personalization' => '个性化',
+    'sidebar_settings_export' => '导出数据',
+    'sidebar_settings_users' => '用户',
+    'sidebar_settings_subscriptions' => '订阅',
+    'sidebar_settings_import' => '导入数据',
+    'sidebar_settings_tags' => '标签管理',
     'sidebar_settings_api' => 'API',
-    'sidebar_settings_security' => 'Security',
+    'sidebar_settings_security' => '安全',
 
-    'export_title' => 'Export your account data',
-    'export_be_patient' => 'Click the button to start the export. It might take several minutes to process the export - please be patient and do not spam the button.',
-    'export_title_sql' => 'Export to SQL',
+    'export_title' => '导出帐户数据',
+    'export_be_patient' => '单击按钮开始导出。处理导出可能需要几分钟时间，请耐心等待, 不要取消按钮。',
+    'export_title_sql' => '导出到 SQL',
     'export_sql_explanation' => 'Exporting your data in SQL format allows you to take your data and import it to your own Monica instance. This is only valuable if you do have your own server.',
-    'export_sql_cta' => 'Export to SQL',
+    'export_sql_cta' => '导出到 SQL',
     'export_sql_link_instructions' => 'Note: <a href=":url">read the instructions</a> to learn more about importing this file to your instance.',
 
     'name_order' => 'Name order',
