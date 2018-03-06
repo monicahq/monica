53555c03d0e3b16298b71c1595a1dfdc5efee1ff
Alexis Saettler <alexis@saettler.org>
2018-03-06 23:10:59 +0100

    Modify register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,7 +9,8 @@
           <div class="signup-box">
             <img class="logo" src="/img/small-logo.png" alt="">
             @if($first)
-              <h2>Create the first Monica account</h2>
+              <h1>Welcome to your newly installed Monica instance</h1>
+              <h2>You need to create a Monica account</h2>
             @else
               <h2>Create your Monica account</h2>
               <h3><a href="/login">Log in</a> if you already have an account.</h3>
