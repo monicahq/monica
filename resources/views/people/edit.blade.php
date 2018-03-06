c430d04584fa42c73a59c4d11064e81066dae1e7
Alexis Saettler <alexis@saettler.org>
2018-03-06 22:52:31 +0100

    wip

--- resources/views/people/edit.blade.php
+++ resources/views/people/edit.blade.php
@@ -121,7 +121,7 @@
                 {{ trans('people.deceased_mark_person_deceased') }}
               </label>
             </div>
-            <div class="form-check {{ ! $contact->is_dead ? 'hidden' : '' }}" id="datePersonDeceased">
+            <div class="form-check {{ $contact->is_dead ? '' : 'hidden' }}" id="datePersonDeceased">
             <label class="form-check-label">
               <input class="form-check-input" id="checkboxDatePersonDeceased" name="checkboxDatePersonDeceased" type="checkbox" value="checkboxDatePersonDeceased" {{ ($contact->deceasedDate != null) ? 'checked' : '' }}>
               {{ trans('people.deceased_know_date') }}
@@ -129,7 +129,7 @@
               @include('partials.components.date-select', ['contact' => $contact, 'specialDate' => $contact->deceasedDate, 'class' => 'deceased_date'])
 
             </div>
-            <div class="form-check {{ ($contact->deceasedDate == null) ? 'hidden' : '' }}" id="reminderDeceased">
+            <div class="form-check {{ $contact->deceasedDate == null ? 'hidden' : '' }}" id="reminderDeceased">
               <label class="form-check-label">
                 <input class="form-check-input" id="addReminderDeceased" name="addReminderDeceased" type="checkbox" value="addReminderDeceased" {{ ($contact->deceasedDate != null) ? (($contact->deceasedDate->reminder_id != null) ? 'checked' : '') : '' }}>
                 {{ trans('people.deceased_add_reminder') }}
