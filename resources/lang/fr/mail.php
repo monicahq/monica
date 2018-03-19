df6fb64bc4a8be40328975b3a1e9444dec140426
Régis Freyd <djaiss@users.noreply.github.com>
2018-03-19 07:03:01 -0400

    New translations mail.php (French)

--- resources/lang/fr/mail.php
+++ resources/lang/fr/mail.php
@@ -5,7 +5,7 @@ return [
     'subject_line' => 'Rappel pour :contact',
     'greetings' => 'Bonjour :username',
     'want_reminded_of' => 'VOUS VOULIEZ ÊTRE RAPPELÉ DE',
-    'for' => 'POUR:',
+    'for' => 'POUR :',
     'footer_contact_info' => 'Ajouter, afficher, compléter et modifier les informations sur ce contact :',
 
     'notification_subject_line' => 'Vous avez 1 événement à venir',
