4ade29d58329340d7bf7250ddc39a3019f401377
Regis Freyd <djaiss@users.noreply.github.com>
2018-03-22 20:04:39 -0400

    wip

--- database/migrations/2018_03_18_090345_migrate_current_relationship_table_to_new_relationship_structure.php
+++ database/migrations/2018_03_18_090345_migrate_current_relationship_table_to_new_relationship_structure.php
@@ -20,54 +20,33 @@ class MigrateCurrentRelationshipTableToNewRelationshipStructure extends Migratio
                 $relationshipTypeId = $account->getRelationshipTypeByType('partner')->id;
                 $itemsToDelete = [];
 
-                // we have to clean the table if it contains bilateral relationships
-                Relationship::where('account_id', $account->id)->chunk(200, function ($relationships) use (&$itemsToDelete, $account) {
-                    foreach ($relationships as $relationship) {
-                        if (in_array($relationship->id, $itemsToDelete)) {
-                            continue;
-                        }
+                $relationships = Relationship::where('account_id', $account->id)->get()->keyBy('id');
 
-                        $bilateralRow = DB::table('relationships')
-                                            ->where('account_id', '=', $account->id)
-                                            ->where('contact_id', '=', $relationship->with_contact_id)
-                                            ->where('with_contact_id', '=', $relationship->contact_id)
-                                            ->first();
-
-                        // if we find such a relationship, we need to delete it - otherwise we will
-                        // create a duplicate entry in the relationship table
-                        // we can't delete it in the same for loop, otherwise we'll probably lose some
-                        // data as we change page
-                        if ($bilateralRow) {
-                            array_push($itemsToDelete, $bilateralRow->id);
+                foreach($relationships as $relationship) {
+                    foreach ($relationships as $bilateralRelationship) {
+                        if ($relationship->contact_id == $bilateralRelationship->with_contact_id
+                            && $relationship->with_contact_id == $bilateralRelationship->contact_id) {
+                            $relationships->forget($bilateralRelationship->id);
                         }
                     }
-                });
 
-                // Now we delete the rows that we don't need
-                foreach ($itemsToDelete as $id) {
-                    DB::table('relationships')->where('id', '=', $id)->delete();
+                    DB::table('temp_relationships_table')->insert([
+                        [
+                            'account_id' => $account->id,
+                            'contact_id_main' => $relationship->contact_id,
+                            'contact_id_secondary' => $relationship->with_contact_id,
+                            'relationship_type_id' => $relationshipTypeId,
+                            'relationship_type_name' => 'partner',
+                        ],
+                        [
+                            'account_id' => $account->id,
+                            'contact_id_main' => $relationship->with_contact_id,
+                            'contact_id_secondary' => $relationship->contact_id,
+                            'relationship_type_id' => $relationshipTypeId,
+                            'relationship_type_name' => 'partner',
+                        ]
+                    ]);
                 }
-
-                Relationship::where('account_id', $account->id)->chunk(200, function ($relationships) use ($account, $relationshipTypeId) {
-                    foreach ($relationships as $relationship) {
-                        DB::table('temp_relationships_table')->insert([
-                            [
-                                'account_id' => $account->id,
-                                'contact_id_main' => $relationship->contact_id,
-                                'contact_id_secondary' => $relationship->with_contact_id,
-                                'relationship_type_id' => $relationshipTypeId,
-                                'relationship_type_name' => 'partner',
-                            ],
-                            [
-                                'account_id' => $account->id,
-                                'contact_id_main' => $relationship->with_contact_id,
-                                'contact_id_secondary' => $relationship->contact_id,
-                                'relationship_type_id' => $relationshipTypeId,
-                                'relationship_type_name' => 'partner',
-                            ]
-                        ]);
-                    }
-                });
             }
         });
 
