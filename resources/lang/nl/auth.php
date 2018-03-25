bbb38751a433888af8a23fbc8576edcf3e791bc6
Régis Freyd <djaiss@users.noreply.github.com>
2018-03-24 20:00:55 -0400

    New translations auth.php (Dutch)

---
+++ resources/lang/nl/auth.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'These credentials do not match our records.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'not_authorized' => 'You are not authorized to execute this action',
+    'signup_disabled' => 'Registration is currently disabled',
+    'back_homepage' => 'Back to homepage',
+    '2fa_title' => 'Two Factor Authentication',
+    '2fa_wrong_validation' => 'The two factor authentication has failed.',
+    '2fa_one_time_password' => 'Authentication code',
+    '2fa_recuperation_code' => 'Enter a two factor recovery code',
+
+];
