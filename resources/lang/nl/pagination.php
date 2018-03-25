fdbd32e33ef26b9a2d8bfd1befc875f6f5d729ef
Régis Freyd <djaiss@users.noreply.github.com>
2018-03-25 08:01:15 -0400

    New translations pagination.php (Dutch)

--- resources/lang/nl/pagination.php
+++ resources/lang/nl/pagination.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'previous' => '&laquo; Previous',
-    'next'     => 'Next &raquo;',
+    'previous' => '&laquo; Vorige',
+    'next'     => 'Volgende &raquo;',
 
 ];
