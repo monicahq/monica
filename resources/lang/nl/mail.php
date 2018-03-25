e224ac911134f687c44d9a64eb43c77d011465b1
Régis Freyd <djaiss@users.noreply.github.com>
2018-03-25 08:01:14 -0400

    New translations mail.php (Dutch)

--- resources/lang/nl/mail.php
+++ resources/lang/nl/mail.php
@@ -9,6 +9,6 @@ return [
     'footer_contact_info' => 'Toevoegen, bekijken, afmaken en aanpassen van information over dit contact:',
 
     'notification_subject_line' => 'Je hebt een aankomende gebeurtenis',
-    'notification_description' => 'In :count days (on :date), the following event will happen:',
+    'notification_description' => 'Over :count dagen (op :date), zal de volgende gebeurtenis plaatsvinden:',
 
 ];
