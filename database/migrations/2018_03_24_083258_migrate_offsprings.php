4cf1c10d31e01d861a4095a5cd6b418032ba176d
Regis Freyd <djaiss@users.noreply.github.com>
2018-03-25 08:54:04 -0400

    wip

---
+++ database/migrations/2018_03_24_083258_migrate_offsprings.php
@@ -0,0 +1,48 @@
+<?php
+
+use App\Account;
+use App\Offspring;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MigrateOffsprings extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Account::chunk(200, function ($accounts) {
+            foreach ($accounts as $account) {
+                $relationshipChildTypeId = $account->getRelationshipTypeByType('child')->id;
+                $relationshipParentTypeId = $account->getRelationshipTypeByType('parent')->id;
+                $offsprings = DB::table('offsprings')->where('account_id', $account->id)->get();
+
+                foreach($offsprings as $offspring) {
+                    DB::table('relationships')->insert([
+                        [
+                            'account_id' => $account->id,
+                            'contact_id_main' => $offspring->contact_id,
+                            'contact_id_secondary' => $offspring->is_the_child_of,
+                            'relationship_type_id' => $relationshipChildTypeId,
+                            'relationship_type_name' => 'child',
+                        ],
+                        [
+                            'account_id' => $account->id,
+                            'contact_id_main' => $offspring->is_the_child_of,
+                            'contact_id_secondary' => $offspring->contact_id,
+                            'relationship_type_id' => $relationshipParentTypeId,
+                            'relationship_type_name' => 'parent',
+                        ]
+                    ]);
+                }
+            }
+        });
+
+        Schema::dropIfExists('offsprings');
+        Schema::dropIfExists('progenitors');
+    }
+}
