9d883ecba618a63bb1ea950fe8ae10807df941e3
Régis Freyd <djaiss@users.noreply.github.com>
2018-03-25 12:26:11 -0400

    Add yearly statistics to activities and calls object (#1049)

--- app/Http/Controllers/Api/ApiCallController.php
+++ app/Http/Controllers/Api/ApiCallController.php
@@ -27,7 +27,9 @@ class ApiCallController extends ApiController
             return $this->respondInvalidQuery();
         }
 
-        return CallResource::collection($calls);
+        return CallResource::collection($calls)->additional(['meta' => [
+            'statistics' => auth()->user()->account->getYearlyCallStatistics(),
+        ]]);
     }
 
     /**
@@ -169,8 +171,11 @@ class ApiCallController extends ApiController
         }
 
         $calls = $contact->calls()
+                ->orderBy($this->sort, $this->sortDirection)
                 ->paginate($this->getLimitPerPage());
 
-        return CallResource::collection($calls);
+        return CallResource::collection($calls)->additional(['meta' => [
+            'statistics' => auth()->user()->account->getYearlyCallStatistics(),
+        ]]);
     }
 }
