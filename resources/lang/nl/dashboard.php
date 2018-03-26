99520a7ebdacde714ff7e761a1d993d809e64ea1
Régis Freyd <djaiss@users.noreply.github.com>
2018-03-26 16:02:02 -0400

    New translations dashboard.php (Dutch)

--- resources/lang/nl/dashboard.php
+++ resources/lang/nl/dashboard.php
@@ -4,6 +4,7 @@ return [
     'dashboard_blank_title' => 'Welkom bij jouw account!',
     'dashboard_blank_description' => 'Monica is de plaats om alle interacties met de mensen waar je om geeft te organiseren.',
     'dashboard_blank_cta' => 'Voeg je eerste contact toe',
+    'dashboard_blank_illustration' => 'Illustration by <a href=":url">Freepik</a>',
 
     'notes_title' => 'Je hebt nog geen notities met een ster.',
 
@@ -15,4 +16,8 @@ return [
     'statistics_activities' => 'Activiteiten',
     'statistics_gifts' => 'Cadeaus',
 
+    'reminders_next_months' => 'Events in the next 3 months',
+    'reminders_none' => 'No reminder for this month',
+
+    'linkedin_profile' => 'LinkedIn',
 ];
