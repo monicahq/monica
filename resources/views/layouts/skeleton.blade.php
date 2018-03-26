169bc906fc629bcd7febfdcd8f499a8a10f82ed7
Alexis Saettler <alexis@saettler.org>
2018-03-26 20:11:53 +0200

    Add direction

--- resources/views/layouts/skeleton.blade.php
+++ resources/views/layouts/skeleton.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ \App::getLocale() }}">
+<html lang="{{ \App::getLocale() }}" dir="{{ \App\Helpers\LocaleHelper::getDirection() }}">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
