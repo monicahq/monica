dede47ca626704f79834b75bab58184023b0adc3
Régis Freyd <djaiss@users.noreply.github.com>
2018-03-28 12:15:29 -0400

    New translations journal.php (Spanish)

--- resources/lang/es/journal.php
+++ resources/lang/es/journal.php
@@ -12,7 +12,7 @@ return [
     'entry_delete_success' => 'The journal entry has been successfully deleted.',
     'journal_add_title' => 'Title (optional)',
     'journal_add_post' => 'Entry',
-    'journal_add_cta' => 'Save',
+    'journal_add_cta' => 'Guardar',
     'journal_blank_cta' => 'Add your first journal entry',
     'journal_blank_description' => 'The journal lets you write events that happened to you, and remember them.',
     'delete_confirmation' => 'Are you sure you want to delete this journal entry?',
