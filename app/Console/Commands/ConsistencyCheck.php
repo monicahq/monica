87d75eebeb72691f6c46ffa6f7b9bb0b636bf3c6
Kenny Nguyen <alphakennyn@gmail.com>
2018-03-31 12:37:30 -0400

    consistency check matching column names done

--- app/Console/Commands/ConsistencyCheck.php
+++ app/Console/Commands/ConsistencyCheck.php
@@ -42,6 +42,7 @@ class ConsistencyCheck extends Command
 
     /**
      * Check if tables in mysql exist in pgsql
+     * Increment $inconsistence_table_count if it doesn't
      *
      * @return void
      */
@@ -54,27 +55,35 @@ class ConsistencyCheck extends Command
             $table_name = $mtable->Tables_in_monica;
             if (DB::connection('pgsql')->table($table_name)->from != $table_name) {
                 $this->inconsistence_table_count++;
-            } else {
-                //$this->line(DB::connection('pgsql')->table($table_name)->from);
             }
         }
     }
 
-    private function checkTableColumns()
+    /**
+     * Check if tables have the same column names
+     * Increment $inconsistence_columns_count if it doesn't
+     *
+     * @return void
+     */
+    private function checkTableColumnsNamesMatch()
     {
         $mysql = DB::connection('mysql');
+        $pgsql = DB::connection('pgsql');
         $mtables = $mysql->select('show tables');
 
         foreach($mtables as $mtable) {
             $thisTable = $mysql->select('show columns from '. $mtable->Tables_in_monica );
-            $dataTable = $mysql->select('select * from '. $mtable->Tables_in_monica );
+            $pgTable = $pgsql->getSchemaBuilder()->getColumnListing($mtable->Tables_in_monica);
 
-            
-            foreach($thisTable as $row){
-                $this->line($row->Field);
+            sort($pgTable);
+            sort($thisTable);
+
+
+            for($x = 0; $x < count($thisTable); $x++){
+                if($thisTable[$x]->Field != $pgTable[$x]) {
+                    $this->inconsistence_columns_count++;
+                }
             }
-
-            break;
         }
     }
 
@@ -93,10 +102,11 @@ class ConsistencyCheck extends Command
         //$ptables = $pgsql->select('select * from');
 
         $this->checkTables();
-        $this->checkTableColumns();
+        $this->checkTableColumnsNamesMatch();
         $this->line("Inconsistent tables: ");
         $this->line($this->inconsistence_table_count);
-
+        $this->line("Inconsistent table columns: ");
+        $this->line($this->inconsistence_columns_count);
 
     }
 }
