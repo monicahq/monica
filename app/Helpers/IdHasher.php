692e42b775a3cba65dab08e508fa86236b464dab
Alexis Saettler <alexis@saettler.org>
2018-04-01 19:36:05 +0200

    Use default_prefix

--- app/Helpers/IdHasher.php
+++ app/Helpers/IdHasher.php
@@ -6,15 +6,32 @@ use Vinkla\Hashids\Facades\Hashids;
 
 class IdHasher
 {
+    /**
+     * Prefix for ids
+     *
+     * @var string
+     */
+    protected $prefix;
+
+    /**
+     * Create a new IdHasher.
+     *
+     * @param string|null $prefix
+     */
+    public function __construct($prefix = null)
+    {
+        $this->prefix = $prefix ?? config('hashids.default_prefix');
+    }
+
     public function encodeId($id)
     {
-        return 'h'.Hashids::encode($id);
+        return $this->prefix . Hashids::encode($id);
     }
 
     public function decodeId($hash)
     {
-        if (str_contains($hash, 'h')) {
-            $result = Hashids::decode(str_after($hash, 'h'));
+        if (str_contains($hash, $this->prefix)) {
+            $result = Hashids::decode(str_after($hash, $this->prefix));
 
             return $result[0]; // result is always an array due to quirk in Hashids libary
         } else {
