1f4ac37a92824d382bca9b242a2821e1bcb848fa
Alexis Saettler <alexis@saettler.org>
2018-04-01 19:36:21 +0200

    Add a ServiceProvider

--- app/Traits/Hasher.php
+++ app/Traits/Hasher.php
@@ -8,24 +8,18 @@ trait Hasher
 {
     public function getRouteKey()
     {
-        $ID_hasher = new IdHasher();
-
-        return $ID_hasher->encodeId(parent::getRouteKey());
+        return app('idhasher')->encodeId(parent::getRouteKey());
     }
 
     public function resolveRouteBinding($value)
     {
-        $ID_hasher = new IdHasher();
-
-        $value = $ID_hasher->encodeId($value);
+        $value = app('idhasher')->encodeId($value);
 
         return $this->where($this->getRouteKeyName(), $value)->first();
     }
 
     public function hashID()
     {
-        $ID_hasher = new IdHasher();
-
-        return $ID_hasher->encodeId($this->id);
+        return app('idhasher')->encodeId($this->id);
     }
 }
