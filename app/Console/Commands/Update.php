0d9b5e2e2df05a998cecc93a434427a5ad2734b6
Alexis Saettler <alexis@saettler.org>
2018-04-02 18:48:55 +0200

    Add some tests

--- app/Console/Commands/Update.php
+++ app/Console/Commands/Update.php
@@ -5,7 +5,8 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Console\ConfirmableTrait;
-use Symfony\Component\Console\Output\OutputInterface;
+use App\Console\Commands\Helpers\CommandExecutor;
+use App\Console\Commands\Helpers\CommandExecutorInterface;
 
 class Update extends Command
 {
@@ -26,6 +27,24 @@ class Update extends Command
     protected $description = 'Update monica dependencies and migrations after a new release';
 
     /**
+     * The Command Executor.
+     * 
+     * @var CommandExecutorInterface
+     */
+    protected $commandExecutor;
+
+    /**
+     * Create a new command.
+     * 
+     * @param CommandExecutorInterface
+     */
+    public function __construct(CommandExecutorInterface $commandExecutor = null)
+    {
+        $this->commandExecutor = $commandExecutor ?: new CommandExecutor($this);
+        parent::__construct();
+    }
+    
+    /**
      * Execute the console command.
      *
      * @return mixed
@@ -33,62 +52,33 @@ class Update extends Command
     public function handle()
     {
         if ($this->confirmToProceed()) {
-            $this->artisan('✓ Resetting config cache', 'config:cache');
+            $this->commandExecutor->artisan('✓ Resetting config cache', 'config:cache');
             try {
-                $this->artisan('✓ Maintenance mode: on', 'down', [
+                $this->commandExecutor->artisan('✓ Maintenance mode: on', 'down', [
                     '--message' => 'Upgrading Monica v'.config('monica.app_version'),
                     '--retry' => '10',
                     ]);
 
                 if ($this->option('composer-install') === true) {
-                    $this->exec('✓ Updating composer dependencies', 'composer install --no-interaction --no-suggest --ignore-platform-reqs'.($this->option('composer-install') === false ? '--no-dev' : ''));
+                    $this->commandExecutor->exec('✓ Updating composer dependencies', 'composer install --no-interaction --no-suggest --ignore-platform-reqs'.($this->option('composer-install') === false ? '--no-dev' : ''));
                 }
 
-                $this->artisan('✓ Performing migrations', 'migrate', ['--force' => 'true']);
+                $this->commandExecutor->artisan('✓ Performing migrations', 'migrate', ['--force' => 'true']);
 
                 if (DB::table('activity_types')->count() == 0) {
-                    $this->artisan('✓ Filling the Activity Types table', 'db:seed', ['--class' => 'ActivityTypesTableSeeder', '--force' => 'true']);
+                    $this->commandExecutor->artisan('✓ Filling the Activity Types table', 'db:seed', ['--class' => 'ActivityTypesTableSeeder', '--force' => 'true']);
                 }
                 if (DB::table('countries')->count() == 0) {
-                    $this->artisan('✓ Filling the Countries table', 'db:seed', ['--class' => 'CountriesSeederTable', '--force' => 'true']);
+                    $this->commandExecutor->artisan('✓ Filling the Countries table', 'db:seed', ['--class' => 'CountriesSeederTable', '--force' => 'true']);
                 }
                 if (! file_exists(public_path('storage'))) {
-                    $this->artisan('✓ Symlink the storage folder', 'storage:link');
+                    $this->commandExecutor->artisan('✓ Symlink the storage folder', 'storage:link');
                 }
             } finally {
-                $this->artisan('✓ Maintenance mode: off', 'up');
+                $this->commandExecutor->artisan('✓ Maintenance mode: off', 'up');
             }
 
             $this->line('Monica v'.config('monica.app_version').' is set up, enjoy.');
         }
-    }
-
-    public function exec($message, $command)
-    {
-        $this->info($message);
-        $this->line($command);
-        exec($command.' 2>&1', $output);
-        if ($this->getOutput()->getVerbosity() >= OutputInterface::VERBOSITY_VERBOSE) {
-            foreach ($output as $line) {
-                $this->line($line);
-            }
-        }
-        $this->line('');
-    }
-
-    public function artisan($message, $command, array $arguments = [])
-    {
-        $this->info($message);
-        $info = '';
-        foreach ($arguments as $key => $value) {
-            $info = $info.' '.$key.'='.$value;
-        }
-        $this->line('php artisan '.$command.$info);
-        if ($this->getOutput()->getVerbosity() >= OutputInterface::VERBOSITY_VERBOSE) {
-            $this->call($command, $arguments);
-        } else {
-            $this->callSilent($command, $arguments);
-        }
-        $this->line('');
     }
 }
---
+++ tests/Commands/UpdateCommandTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Commands;
+
+use Tests\TestCase;
+use App\Console\Commands\Update;
+use App\Console\Commands\Helpers\CommandExecutorInterface;
+
+class UpdateCommandTest extends TestCase
+{
+    public function test_update_command_default()
+    {
+        $commandExecutor = new CommandExecutorTester();
+        $command = new Update($commandExecutor);
+        $command->setLaravel($this->createApplication());
+
+        $command->run(new \Symfony\Component\Console\Input\ArrayInput([]), new \Symfony\Component\Console\Output\NullOutput());
+        
+        $this->assertCount(4, $commandExecutor->buffer);
+        $this->assertCommandContains($commandExecutor->buffer[0], 'Resetting config cache', 'php artisan config:cache');
+        $this->assertCommandContains($commandExecutor->buffer[1], 'Maintenance mode: on', 'php artisan down');
+        $this->assertCommandContains($commandExecutor->buffer[2], 'Performing migrations', 'php artisan migrate');
+        $this->assertCommandContains($commandExecutor->buffer[3], 'Maintenance mode: off', 'php artisan up');
+    }
+
+    public function test_update_command_composer()
+    {
+        $commandExecutor = new CommandExecutorTester();
+        $command = new Update($commandExecutor);
+        $command->setLaravel($this->createApplication());
+
+        $command->run(new \Symfony\Component\Console\Input\ArrayInput(['--composer-install' => true]), new \Symfony\Component\Console\Output\NullOutput());
+        
+        $this->assertCount(5, $commandExecutor->buffer);
+        $this->assertCommandContains($commandExecutor->buffer[0], 'Resetting config cache', 'php artisan config:cache');
+        $this->assertCommandContains($commandExecutor->buffer[1], 'Maintenance mode: on', 'php artisan down');
+        $this->assertCommandContains($commandExecutor->buffer[2], 'Updating composer dependencies', 'composer install');
+        $this->assertCommandContains($commandExecutor->buffer[3], 'Performing migrations', 'php artisan migrate');
+        $this->assertCommandContains($commandExecutor->buffer[4], 'Maintenance mode: off', 'php artisan up');
+    }
+
+    public function assertCommandContains($array, $message, $command)
+    {
+        $this->assertContains($message, $array['message']);
+        $this->assertContains($command, $array['command']);
+    }
+}
