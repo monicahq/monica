a99728736a6ff4ac6e41f2336c2e94af237b0a88
Régis Freyd <djaiss@users.noreply.github.com>
2018-04-04 18:12:35 -0400

    New translations journal.php (Latin)

---
+++ resources/lang/la/journal.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'journal_rate' => 'crwdns83:0crwdne83:0',
+    'journal_come_back' => 'crwdns84:0crwdne84:0',
+    'journal_description' => 'crwdns1148:0crwdne1148:0',
+    'journal_add' => 'crwdns86:0crwdne86:0',
+    'journal_created_automatically' => 'crwdns87:0crwdne87:0',
+    'journal_entry_type_journal' => 'crwdns88:0crwdne88:0',
+    'journal_entry_type_activity' => 'crwdns89:0crwdne89:0',
+    'journal_entry_rate' => 'crwdns90:0crwdne90:0',
+    'entry_delete_success' => 'crwdns91:0crwdne91:0',
+    'journal_add_title' => 'crwdns92:0crwdne92:0',
+    'journal_add_post' => 'crwdns93:0crwdne93:0',
+    'journal_add_cta' => 'crwdns94:0crwdne94:0',
+    'journal_blank_cta' => 'crwdns95:0crwdne95:0',
+    'journal_blank_description' => 'crwdns96:0crwdne96:0',
+    'delete_confirmation' => 'crwdns97:0crwdne97:0',
+];
