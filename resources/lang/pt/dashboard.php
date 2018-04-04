5ad7103f2527e5761a085d098ded5f0abb990e9d
Régis Freyd <djaiss@users.noreply.github.com>
2018-04-04 18:12:06 -0400

    New translations dashboard.php (Portuguese)

--- resources/lang/pt/dashboard.php
+++ resources/lang/pt/dashboard.php
@@ -6,11 +6,11 @@ return [
     'dashboard_blank_cta' => 'Adicione o seu primeiro contacto',
     'dashboard_blank_illustration' => 'Illustration by <a href=":url">Freepik</a>',
 
-    'notes_title' => 'You don\'t any notes yet.',
+    'notes_title' => 'You don’t have any starred notes yet.',
 
     'tab_recent_calls' => 'Chamadas recentes',
     'tab_favorite_notes' => 'Notas favoritas',
-    'tab_calls_blank' => 'Você ainda não registou chamadas.',
+    'tab_calls_blank' => 'You haven’t logged a call yet.',
 
     'statistics_contacts' => 'Contatos',
     'statistics_activities' => 'Atividades',
