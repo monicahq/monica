9faaaae7e1244589b23eb08db5e642182d975eff
Régis Freyd <djaiss@users.noreply.github.com>
2018-04-05 12:43:03 -0400

    New translations passwords.php (Italian)

--- resources/lang/it/passwords.php
+++ resources/lang/it/passwords.php
@@ -15,9 +15,9 @@ return [
 
     'password' => 'Le password devono essere di almeno sei caratteri e devono combaciare con la conferma.',
     'reset' => 'La tua password è stata reimpostata!',
-    'sent' => 'If the email you entered exists in our records, you’ve been sent a password reset link.',
+    'sent' => 'Se l\'email inserita esiste nei nostri archivi vi é stato inviato il link per reimpostare la tua password.',
     'token' => 'Questo token per reimpostare la password non è valido.',
-    'user' => 'If the email you entered exists in our records, you’ve been sent a password reset link.',
+    'user' => 'Se l\'email inserita esiste nei nostri archivi vi é stato inviato il link per reimpostare la tua password.',
     'changed' => 'Password changed successfuly.',
     'invalid' => 'Current password you entered is not correct.',
 
