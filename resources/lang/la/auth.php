8b99ff195386282b99406d2995a5898de370eb1f
Régis Freyd <djaiss@users.noreply.github.com>
2018-04-11 11:32:59 -0400

    New translations auth.php (Latin)

---
+++ resources/lang/la/auth.php
@@ -0,0 +1,62 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'crwdns64:0crwdne64:0',
+    'throttle' => 'crwdns65:0crwdne65:0',
+    'not_authorized' => 'crwdns66:0crwdne66:0',
+    'signup_disabled' => 'crwdns67:0crwdne67:0',
+    'back_homepage' => 'crwdns68:0crwdne68:0',
+    '2fa_title' => 'crwdns69:0crwdne69:0',
+    '2fa_wrong_validation' => 'crwdns70:0crwdne70:0',
+    '2fa_one_time_password' => 'crwdns71:0crwdne71:0',
+    '2fa_recuperation_code' => 'crwdns72:0crwdne72:0',
+
+    'login_to_account' => 'crwdns694:0crwdne694:0',
+    'email' => 'crwdns695:0crwdne695:0',
+    'password' => 'crwdns696:0crwdne696:0',
+    'login' => 'crwdns697:0crwdne697:0',
+    'button_remember' => 'crwdns698:0crwdne698:0',
+    'password_forget' => 'crwdns699:0crwdne699:0',
+    'password_reset' => 'crwdns700:0crwdne700:0',
+    'signup_no_account' => 'crwdns1144:0crwdne1144:0',
+    'signup' => 'crwdns702:0crwdne702:0',
+    'create_account' => 'crwdns703:0crwdne703:0',
+
+    'password_reset_title' => 'crwdns704:0crwdne704:0',
+    'password_reset_email' => 'crwdns705:0crwdne705:0',
+    'password_reset_send_link' => 'crwdns706:0crwdne706:0',
+    'password_reset_password' => 'crwdns707:0crwdne707:0',
+    'password_reset_password_confirm' => 'crwdns708:0crwdne708:0',
+    'password_reset_action' => 'crwdns709:0crwdne709:0',
+    'password_reset_email_content' => 'crwdns710:0crwdne710:0',
+
+    'register_title_welcome' => 'crwdns711:0crwdne711:0',
+    'register_create_account' => 'crwdns712:0crwdne712:0',
+    'register_title_create' => 'crwdns713:0crwdne713:0',
+    'register_login' => 'crwdns714:0crwdne714:0',
+    'register_email' => 'crwdns715:0crwdne715:0',
+    'register_email_example' => 'crwdns716:0crwdne716:0',
+    'register_firstname' => 'crwdns717:0crwdne717:0',
+    'register_firstname_example' => 'crwdns718:0crwdne718:0',
+    'register_lastname' => 'crwdns719:0crwdne719:0',
+    'register_lastname_example' => 'crwdns720:0crwdne720:0',
+    'register_password' => 'crwdns721:0crwdne721:0',
+    'register_password_example' => 'crwdns722:0crwdne722:0',
+    'register_password_confirmation' => 'crwdns723:0crwdne723:0',
+    'register_action' => 'crwdns724:0crwdne724:0',
+    'register_policy' => 'crwdns818:0crwdne818:0',
+    'register_invitation_email' => 'crwdns1145:0crwdne1145:0',
+
+];
