12d74fdd7bd08be5abe5dba35fe3b2d85f95aef0
Régis Freyd <djaiss@users.noreply.github.com>
2018-04-12 11:03:49 -0400

    New translations people.php (Hebrew)

--- resources/lang/he/people.php
+++ resources/lang/he/people.php
@@ -227,7 +227,7 @@ return [
     'gifts_delete_confirmation' => 'למחוק את המתנה הזאת?',
     'gifts_add_gift' => 'הוספת מתנה',
     'gifts_link' => 'קישור',
-    'gifts_for' => 'For:',
+    'gifts_for' => 'עבור:',
     'gifts_delete_cta' => 'מחיקה',
     'gifts_add_title' => 'ניהול מתנות עבור :name',
     'gifts_add_gift_idea' => 'רעיון למתנה',
