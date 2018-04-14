f5f7ac7d2ff8c58fa26928e37d35b0865dc85c9a
Regis Freyd <djaiss@users.noreply.github.com>
2018-04-14 08:29:03 -0400

    Fix gender id set to 0

---
+++ database/migrations/2018_04_14_081052_fix_wrong_gender.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Gender;
+use App\Account;
+use App\Contact;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class FixWrongGender extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $contacts = Contact::where('gender_id', 0)->where('account_id', '!=', 0)->get();
+        foreach ($contacts as $contact) {
+            $account = Account::find($contact->account_id);
+            $firstGender = Gender::where('account_id', $account->id)->first();
+            $contact->gender_id = $firstGender->id;
+            $contact->save();
+        }
+    }
+}
