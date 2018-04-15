0045e6a74d90747ce207524a289318f405d2370e
Régis Freyd <djaiss@users.noreply.github.com>
2018-04-15 10:51:02 -0400

    New translations changelog.php (Hebrew)

--- resources/lang/he/changelog.php
+++ resources/lang/he/changelog.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'title' => 'Product changes',
-    'note' => 'Note: unfortunately, this page is only in English.',
+    'title' => 'שינויים במוצר',
+    'note' => 'הערה: לרוע המזל, עמוד זה הוא באנגלית בלבד.',
 ];
