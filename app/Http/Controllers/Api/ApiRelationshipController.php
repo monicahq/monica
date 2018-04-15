8973d6d8cdddf62cdec8015bb7b18b92bbf3a535
Regis Freyd <djaiss@users.noreply.github.com>
2018-04-15 09:24:11 -0400

    method to store a relationship through the api

--- app/Http/Controllers/Api/ApiRelationshipController.php
+++ app/Http/Controllers/Api/ApiRelationshipController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Relationship;
+use App\RelationshipType;
 use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use App\Http\Resources\Relationship\Relationship as RelationshipResource;
@@ -26,5 +27,88 @@ class ApiRelationshipController extends ApiController
         }
 
         return new RelationshipResource($relationship);
+    }
+
+    /**
+     * Create a new relationship.
+     *
+     * @param  Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+        $isvalid = $this->validateUpdate($request);
+        if ($isvalid !== true) {
+            return $isvalid;
+        }
+
+        $relationshipType = RelationshipType::find($request->get('relationship_type_id'));
+
+        // Create the contact
+        try {
+            $relationship = Relationship::create(
+                $request->only([
+                    'contact_is',
+                    'relationship_type_id',
+                    'of_contact',
+                ]) + [
+                'relationship_type_name' => $relationshipType->name,
+            ]);
+        } catch (QueryException $e) {
+            return $this->respondNotTheRightParameters();
+        }
+    }
+
+    /**
+     * Validate the request for update.
+     *
+     * @param  Request $request
+     * @return mixed
+     */
+    private function validateUpdate(Request $request)
+    {
+        // Validates basic fields to create the entry
+        $validator = Validator::make($request->all(), [
+            'contact_is' => 'integer|required',
+            'relationship_type_id' => 'required|integer',
+            'of_contact' => 'integer|required',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->setErrorCode(32)
+                        ->respondWithError($validator->errors()->all());
+        }
+
+        if ($request->get('relationship_type_id')) {
+            try {
+                RelationshipType::where('account_id', auth()->user()->account_id)
+                    ->where('id', $request->input('relationship_type_id'))
+                    ->firstOrFail();
+            } catch (ModelNotFoundException $e) {
+                return $this->respondNotFound();
+            }
+        }
+
+        if ($request->get('contact_is')) {
+            try {
+                Contact::where('account_id', auth()->user()->account_id)
+                    ->where('id', $request->input('contact_is'))
+                    ->firstOrFail();
+            } catch (ModelNotFoundException $e) {
+                return $this->respondNotFound();
+            }
+        }
+
+        if ($request->get('of_contact')) {
+            try {
+                Contact::where('account_id', auth()->user()->account_id)
+                    ->where('id', $request->input('of_contact'))
+                    ->firstOrFail();
+            } catch (ModelNotFoundException $e) {
+                return $this->respondNotFound();
+            }
+        }
+
+        return true;
     }
 }
