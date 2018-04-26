42e8b633edb3810deca996b0fea283c47a6c263a
Regis Freyd <djaiss@users.noreply.github.com>
2018-04-26 17:42:48 -0400

    a bit of tests

--- app/Http/Controllers/Api/Statistics/ApiStatisticsController.php
+++ app/Http/Controllers/Api/Statistics/ApiStatisticsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\Statistics;
 
 use Exception;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Api\ApiController;
 
 class ApiStatisticsController extends ApiController
 {
@@ -14,13 +15,13 @@ class ApiStatisticsController extends ApiController
      */
     public function index(Request $request)
     {
-        if (! config('monica.allow_statistics_through_public_api_access')) {
-            throw new Exception(trans('people.stay_in_touch_invalid'));
+        if (config('monica.allow_statistics_through_public_api_access') == false) {
+            return $this->respondNotFound();
         }
 
         // Collecting statistics
-        $statistic = \App\Statistic::order_by('created_at', 'desc')->first();
-        $instance = Instance::first();
+        $statistic = \App\Statistic::orderBy('created_at', 'desc')->first();
+        $instance = \App\Instance::first();
 
         // Get the date of the monday of last week
         $dateMondayLastWeek = \Carbon\Carbon::now()->subDays(7);
@@ -34,14 +35,21 @@ class ApiStatisticsController extends ApiController
         $instanceLastMonday = \App\Statistic::whereDate('created_at', '=', $dateMondayLastWeek->format('Y-m-d'))->first();
         $instanceLastSunday = \App\Statistic::whereDate('created_at', '=', $dateSundayLastWeek->format('Y-m-d'))->first();
 
+        $numberNewUsers = 0;
+        if ($instanceLastMonday && $instanceLastSunday) {
+            $numberNewUsers = $instanceLastSunday->number_of_users - $instanceLastMonday->number_of_users;
+        }
+
         $statistics = collect();
         $statistics->push([
             'instance_creation_date' => $instance->created_at,
-            'number_of_contacts' => $statistic->number_of_contacts,
-            'number_of_users' => $statistic->number_of_users,
-            'number_of_activities' => $statistic->number_of_activities,
-            'number_of_reminders' => $statistic->number_of_reminders,
-            'number_of_new_users_last_week' => ,
+            'number_of_contacts' => ($statistic ? $statistic->number_of_contacts : 0),
+            'number_of_users' => ($statistic ? $statistic->number_of_users : 0),
+            'number_of_activities' => ($statistic ? $statistic->number_of_activities : 0),
+            'number_of_reminders' => ($statistic ? $statistic->number_of_reminders : 0),
+            'number_of_new_users_last_week' => $numberNewUsers,
         ]);
+
+        return $statistics;
     }
 }
