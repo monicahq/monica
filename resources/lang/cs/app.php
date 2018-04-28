7717b53751d0dc098fdf43d6eb9f145129e016da
Régis Freyd <djaiss@users.noreply.github.com>
2018-04-28 06:30:44 -0400

    New translations app.php (Czech)

--- resources/lang/cs/app.php
+++ resources/lang/cs/app.php
@@ -20,8 +20,7 @@ return [
     'load_more' => 'Načíst další',
     'loading' => 'Načítá se...',
     'with' => 's',
-    'day' => 'day',
-    'days' => 'days',
+    'days' => 'day|days',
 
     'application_title' => 'Monica – personal relationship manager',
     'application_description' => 'Monica is a tool to manage your interactions with your loved ones, friends and family.',
