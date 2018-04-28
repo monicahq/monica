48370c8c33b766a8079f5b26a3845662a45ad71a
Régis Freyd <djaiss@users.noreply.github.com>
2018-04-28 06:30:56 -0400

    New translations people.php (Russian)

--- resources/lang/ru/people.php
+++ resources/lang/ru/people.php
@@ -61,7 +61,7 @@ return [
 
     // Stay in touch
     'stay_in_touch' => 'Stay in touch',
-    'stay_in_touch_frequency' => 'Stay in touch every {count} days',
+    'stay_in_touch_frequency' => 'Stay in touch every day|Stay in touch every {count} days',
     'stay_in_touch_invalid' => 'The frequency must be a number greater than 0.',
     'stay_in_touch_modal_title' => 'Stay in touch',
     'stay_in_touch_modal_desc' => 'We can remind you by email to keep in touch with {firstname} at a regular interval.',
