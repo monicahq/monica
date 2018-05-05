30acc724ec57ada93b1992e6c35c9799d2bc49c9
Regis Freyd <djaiss@users.noreply.github.com>
2018-05-04 23:30:25 -0400

    toujours un fils de pute

--- database/migrations/2018_05_04_231017_update_changelog_with_removal_automatic_birthday.php
+++ database/migrations/2018_05_04_231017_update_changelog_with_removal_automatic_birthday.php
@@ -1,8 +1,6 @@
 <?php
 
-use DB;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Migrations\Migration;
 
 class UpdateChangelogWithRemovalAutomaticBirthday extends Migration
