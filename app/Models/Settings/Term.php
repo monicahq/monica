6c40aa0b3e527bfebd85b7fbdfb71b0d61e9ed68
Regis Freyd <djaiss@users.noreply.github.com>
2018-05-07 20:30:21 -0400

    wip

---
+++ app/Models/Settings/Term.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models\Settings;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Term extends Model
+{
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = ['id'];
+
+    /**
+     * Get the users record associated with the term.
+     */
+    public function users()
+    {
+        return $this->belongsToMany('App\User')->withPivot('user_id')->withTimestamps();
+    }
+}
---
+++ database/migrations/2018_05_07_070458_create_terms_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTermsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('terms', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('term_version');
+            $table->mediumText('term_content');
+            $table->string('privacy_version');
+            $table->mediumText('privacy_content');
+            $table->timestamps();
+        });
+
+        Schema::create('term_user', function (Blueprint $table) {
+            $table->unsignedInteger('account_id');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('term_id');
+            $table->boolean('read')->default(0);
+            $table->dateTime('accepted_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('terms');
+        Schema::dropIfExists('term_user');
+    }
+}
