53de0b3527514b9f045261ea63ab49ed71c6166f
Alexis Saettler <alexis@saettler.org>
2018-05-10 18:08:06 +0200

    Add a APP_SIGNUP_DOUBLE_OPTIN environment variable

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -100,4 +100,25 @@ class RegisterController extends Controller
 
         return $user;
     }
+
+    /**
+     * The user has been registered.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $user
+     * @return mixed
+     */
+    protected function registered(Request $request, $user)
+    {
+        if (! config('monica.signup_double_optin')) {
+            // if signup_double_optin is disabled, skip the confirm email part
+            $user->confirmation_code = null;
+            $user->confirmed = true;
+            $user->save();
+
+            $this->guard()->login($user);
+
+            return redirect(route('login'));
+        }
+    }
 }
