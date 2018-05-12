31de6de135bfea200736cb48f3ca8ff177eec43a
Danny Waldron <jenosdeaddog@gmail.com>
2018-05-12 18:19:45 -0500

    Style

--- app/Contact.php
+++ app/Contact.php
@@ -333,7 +333,6 @@ class Contact extends Model
      */
     public function scopeSortedBy(Builder $builder, $criteria)
     {
-
         switch ($criteria) {
             case 'firstnameAZ':
                 return $builder->orderBy('first_name', 'asc');
@@ -348,12 +347,14 @@ class Contact extends Model
                 $builder->leftJoin('activities', 'activity_contact.activity_id', '=', 'activities.id');
                 $builder->orderBy('activities.date_it_happened', 'desc');
                 $builder->select('*', 'contacts.id as id');
+
                 return $builder;
             case 'lastactivitydateOldtoNew':
                 $builder->leftJoin('activity_contact', 'contacts.id', '=', 'activity_contact.contact_id');
                 $builder->leftJoin('activities', 'activity_contact.activity_id', '=', 'activities.id');
                 $builder->orderBy('activities.date_it_happened', 'asc');
                 $builder->select('*', 'contacts.id as id');
+
                 return $builder;
             default:
                 return $builder->orderBy('first_name', 'asc');
