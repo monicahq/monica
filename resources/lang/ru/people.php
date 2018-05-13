88ec74f6343cfc35e5794d34c383db4f9ef9f19e
Régis Freyd <djaiss@users.noreply.github.com>
2018-05-13 09:11:07 -0400

    New translations people.php (Russian)

--- resources/lang/ru/people.php
+++ resources/lang/ru/people.php
@@ -24,6 +24,8 @@ return [
     'people_list_account_usage' => 'Лимиты контактов: :current/:limit',
     'people_list_account_upgrade_title' => 'Перейдите на другой план чтобы получить больше возможностей.',
     'people_list_account_upgrade_cta' => 'Upgrade now',
+    'people_list_untagged' => 'View untagged contacts',
+    'people_list_filter_untag' => 'Showing all untagged contacts ',
 
     // people add
     'people_add_title' => 'Добавить человека',
