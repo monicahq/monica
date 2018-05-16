11493ace5c7d98a31092378e57b43eb223cee2c4
Régis Freyd <djaiss@users.noreply.github.com>
2018-05-16 19:20:41 -0400

    New translations app.php (Czech)

--- resources/lang/cs/app.php
+++ resources/lang/cs/app.php
@@ -216,4 +216,9 @@ return [
     'relationship_type_protege_female' => 'protege',
     'relationship_type_protege_with_name' => ':name’s protege',
     'relationship_type_protege_female_with_name' => ':name’s protege',
+
+    'relationship_type_ex_husband' => 'ex husband',
+    'relationship_type_ex_husband_female' => 'ex wife',
+    'relationship_type_ex_husband_with_name' => ':name’s ex husband',
+    'relationship_type_ex_husband_female_with_name' => ':name’s ex wife',
 ];
