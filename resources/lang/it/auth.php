7784e845185c74f855cfc86b938b4fe3d0be36b7
Régis Freyd <djaiss@users.noreply.github.com>
2018-05-20 16:00:53 -0400

    New translations auth.php (Italian)

--- resources/lang/it/auth.php
+++ resources/lang/it/auth.php
@@ -56,7 +56,7 @@ return [
     'register_password_example' => 'Inserire una password sicura',
     'register_password_confirmation' => 'Conferma password',
     'register_action' => 'Registrati',
-    'register_policy' => 'Registrarsi significa hai letto e sei d\'accordo con la nostra</a> informativa sulla privacy<a href=":url" hreflang=":hreflang">.',
+    'register_policy' => 'Signing up signifies you’ve read and agree to our <a href=":url" hreflang=":hreflang">Privacy Policy</a> and <a href=":urlterm" hreflang=":hreflang">Terms of use</a>.',
     'register_invitation_email' => 'Per ragioni di sicurezza, inserisci l\'indirizzo email della persona che ti ha invitato. Trovi questa informazione nella mail di invito.',
 
 ];
