64d903a0cc8d57521c8e69dcbc0d5fa3b8f71815
Régis Freyd <djaiss@users.noreply.github.com>
2018-05-20 16:01:24 -0400

    New translations auth.php (Chinese Simplified)

--- resources/lang/zh/auth.php
+++ resources/lang/zh/auth.php
@@ -56,7 +56,7 @@ return [
     'register_password_example' => 'Enter a secure password',
     'register_password_confirmation' => 'Password confirmation',
     'register_action' => 'Register',
-    'register_policy' => 'Signing up signifies you’ve read and agree to our <a href=":url" hreflang=":hreflang">Privacy Policy</a>.',
+    'register_policy' => 'Signing up signifies you’ve read and agree to our <a href=":url" hreflang=":hreflang">Privacy Policy</a> and <a href=":urlterm" hreflang=":hreflang">Terms of use</a>.',
     'register_invitation_email' => 'For security purposes, please indicate the email of the person who’ve invited you to join this account. This information is provided in the invitation email.',
 
 ];
