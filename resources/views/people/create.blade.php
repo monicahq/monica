25218b2c14f46c58c81308cebd26836c09c9060a
Regis Freyd <djaiss@users.noreply.github.com>
2018-05-20 23:29:01 -0400

    wip

--- resources/views/people/create.blade.php
+++ resources/views/people/create.blade.php
@@ -26,7 +26,7 @@
 
       <div class="pa4-ns ph3 pv2 bb b--gray-monica">
         {{-- This check is for the cultures that are used to say the last name first --}}
-        @if (auth()->user()->name_order == 'firstname_first')
+        @if (auth()->user()->name_order == 'firstname_lastname')
 
         <div class="mb3">
           <form-input
