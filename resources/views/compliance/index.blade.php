2a932c8fb2c194be5fdb7993463aff2d41829a70
Alexis Saettler <alexis@saettler.org>
2018-05-21 09:21:10 +0200

    Remove url form text to translate

--- resources/views/compliance/index.blade.php
+++ resources/views/compliance/index.blade.php
@@ -5,7 +5,7 @@
 
     <div class="mw7 center br3 ba b--gray-monica bg-white mb5 pa5">
       <h2 class="mb4">{{ trans('app.compliance_title') }}</h2>
-      <p>{!! trans('app.compliance_desc') !!}</p>
+      <p>{!! trans('app.compliance_desc', ['url' => 'https://monicahq.com/privacy', 'urlterm' => 'https://monicahq.com/terms', 'hreflang' => 'en', ]) !!}</p>
       <p>{{ trans('app.compliance_desc_end') }}</p>
 
       <form action="/compliance/sign" method="POST" class="tc mt4">
