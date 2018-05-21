c07695ab3ba00d0f426c47c2fff91eaeef8307cc
Alexis Saettler <alexis@saettler.org>
2018-05-21 21:30:36 +0200

    Remove email confirmation for first account

--- app/Notifications/ConfirmEmail.php
+++ app/Notifications/ConfirmEmail.php
@@ -2,6 +2,7 @@
 
 namespace App\Notifications;
 
+use App\Account;
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -18,7 +19,8 @@ class ConfirmEmail extends Notification implements ShouldQueue
      */
     public function via()
     {
-        if (! config('monica.signup_double_optin')) {
+        $first = Account::count() == 1;
+        if (! config('monica.signup_double_optin') || $first) {
             return [];
         }
 
