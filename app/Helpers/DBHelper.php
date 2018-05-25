715eaf67848f757edf3d3b918eabf4b4dc3bafa5
Tom Rochette <roctom@gmail.com>
2018-05-25 19:45:25 -0400

    Use DB::connection()->getDatabaseName() instead of complicated logic to retrieve the database name of the default connection.

--- app/Helpers/DBHelper.php
+++ app/Helpers/DBHelper.php
@@ -6,19 +6,14 @@ use Illuminate\Support\Facades\DB;
 
 class DBHelper
 {
+    /**
+     * @return \Illuminate\Support\Collection
+     */
     public static function getTables()
     {
         return DB::table('information_schema.tables')
             ->select('table_name')
-            ->where('table_schema', '=', static::getDefaultConnectionConfiguration('database'))
+            ->where('table_schema', '=', DB::connection()->getDatabaseName())
             ->get();
-    }
-
-    public static function getDefaultConnectionConfiguration($key = null, $default = null)
-    {
-        $defaultConnection = config('database.default');
-        $configuration = config('database.connections.'.$defaultConnection, []);
-
-        return $key ? array_get($configuration, $key, $default) : $configuration;
     }
 }
