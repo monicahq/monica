53df14b273f32560f8880ba6dddeb6bb8411e1c1
Régis Freyd <djaiss@users.noreply.github.com>
2018-05-28 11:44:15 -0400

    New translations mail.php (Italian)

--- resources/lang/it/mail.php
+++ resources/lang/it/mail.php
@@ -13,4 +13,13 @@ return [
 
     'stay_in_touch_subject_line' => 'Stay in touch with :name',
     'stay_in_touch_subject_description' => 'You asked to be reminded to stay in touch with :name every :frequency days.',
+
+    'notifications_whoops' => 'Whoops!',
+    'notifications_hello' => 'Hello!',
+    'notifications_regards' => 'Regards',
+    'notifications_footer' => 'If you’re having trouble clicking the ":actionText" button, copy and paste the URL below into your web browser: [:actionURL](:actionURL)',
+
+    'confirmation_email_title' => 'Monica – Email verification',
+    'confirmation_email_intro'=> 'To validate your email click on the button below',
+    'confirmation_email_button' => 'Verify email address',
 ];
