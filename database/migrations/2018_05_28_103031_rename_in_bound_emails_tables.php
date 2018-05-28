4dcdaa56db10f1dc53e110cded6af7ff6f56c25a
Danny Waldron <jenosdeaddog@gmail.com>
2018-05-28 09:44:42 -0500

    Style

--- database/migrations/2018_05_28_103031_rename_in_bound_emails_tables.php
+++ database/migrations/2018_05_28_103031_rename_in_bound_emails_tables.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 class RenameInBoundEmailsTables extends Migration
@@ -13,9 +12,9 @@ class RenameInBoundEmailsTables extends Migration
      */
     public function up()
     {
-      Schema::rename('in_bound_emails', 'inbound_emails');
+        Schema::rename('in_bound_emails', 'inbound_emails');
 
-      Schema::rename('contact_in_bound_emails', 'contact_inbound_emails');
+        Schema::rename('contact_in_bound_emails', 'contact_inbound_emails');
     }
 
 }
