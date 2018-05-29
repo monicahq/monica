2361af33051ca5d38ceec1565d0f4a23767ee76d
Régis Freyd <djaiss@users.noreply.github.com>
2018-05-29 02:37:57 -0400

    New translations mail.php (Hebrew)

--- resources/lang/he/mail.php
+++ resources/lang/he/mail.php
@@ -14,12 +14,12 @@ return [
     'stay_in_touch_subject_line' => 'לשמור על קשר עם :name',
     'stay_in_touch_subject_description' => 'ביקשת לקבל תזכורת לשמור על קשר עם :name כל :frequency ימים.',
 
-    'notifications_whoops' => 'Whoops!',
-    'notifications_hello' => 'Hello!',
-    'notifications_regards' => 'Regards',
+    'notifications_whoops' => 'אופס!',
+    'notifications_hello' => 'שלום!',
+    'notifications_regards' => 'בברכה',
     'notifications_footer' => 'If you’re having trouble clicking the ":actionText" button, copy and paste the URL below into your web browser: [:actionURL](:actionURL)',
 
-    'confirmation_email_title' => 'Monica – Email verification',
+    'confirmation_email_title' => 'מוניקה – אימות דוא״ל',
     'confirmation_email_intro'=> 'To validate your email click on the button below',
     'confirmation_email_button' => 'Verify email address',
 ];
