3784544bf7a294703ceed344affb18fa1b40dcd9
Régis Freyd <djaiss@users.noreply.github.com>
2018-05-30 22:50:32 -0400

    Fix wrong url to send a confirmation email (#1414)

--- resources/lang/vendor/confirmation/en/confirmation.php
+++ resources/lang/vendor/confirmation/en/confirmation.php
@@ -5,6 +5,6 @@ return [
     'success' => 'You have successfully verified your account! You can now login.',
     'again' => 'You must verify your email before you can access the site.
                 <br/>If you have not received the confirmation email check your spam folder.
-                <br/>To get a new confirmation email please <a href=":url" class="alert-link">click here</a>.',
+                <br/>To get a new confirmation email please <a href="/confirmation/resend" class="alert-link">click here</a>.',
     'resend' => 'A confirmation message has been sent. Please check your mailbox.'
 ];
