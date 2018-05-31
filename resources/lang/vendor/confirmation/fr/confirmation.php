cdbae0e0b81ba9a37f75997d8d1ff7b9994f658a
Régis Freyd <djaiss@users.noreply.github.com>
2018-05-31 01:26:53 -0400

    New translations confirmation.php (French)

--- resources/lang/vendor/confirmation/fr/confirmation.php
+++ resources/lang/vendor/confirmation/fr/confirmation.php
@@ -5,6 +5,6 @@ return [
     'success' => 'Votre compte a été validé ! Vous pouvez maintenant vous connecter.',
     'again' => 'You must verify your email before you can access the site.
                 <br/>If you have not received the confirmation email check your spam folder.
-                <br/>To get a new confirmation email please <a href="/confirmation/resend" class="alert-link">click here</a>.',
+                <br/>To get a new confirmation email please <a href=":url" class="alert-link">click here</a>.',
     'resend' => 'Un message de confirmation a été envoyé. Merci de vérifier votre boîte aux lettres.'
 ];
