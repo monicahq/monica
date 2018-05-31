2fc5e2234a8fb1ba2654413c922470854629e6c4
Régis Freyd <djaiss@users.noreply.github.com>
2018-05-31 02:00:47 -0400

    New translations confirmation.php (Hebrew)

--- resources/lang/vendor/confirmation/he/confirmation.php
+++ resources/lang/vendor/confirmation/he/confirmation.php
@@ -3,8 +3,8 @@
 return [
     'message' => 'תודה לך על ההרשמה! נא לבדוק את תיבת הדוא״ל שלך כדי לאמת את אותה.',
     'success' => 'החשבון שלך אומת בהצלחה! כעת יתאפשר לך להיכנס אליו.',
-    'again' => 'You must verify your email before you can access the site.
-                <br/>If you have not received the confirmation email check your spam folder.
-                <br/>To get a new confirmation email please <a href=":url" class="alert-link">click here</a>.',
+    'again' => 'עליך לאמת את כתובת הדוא״ל שלך לפני שתהיה לך אפשרות לגשת לאתר.
+                <br/>אם לא קיבלת הודעת אימות בדוא״ל מוטב לחפש בתיקיית דואר הזבל.
+                <br/>כדי לקבל דוא״ל אימות מחדש <a href=":url" class="alert-link">נא ללחוץ כאן</a>.',
     'resend' => 'נשלחה הודעת אימות. נא לבדוק בתיבת הדוא״ל שלך.'
 ];
