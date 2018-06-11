d8ea850da71be201e184052028576748aed7adf7
Regis Freyd <djaiss@users.noreply.github.com>
2018-06-11 18:47:43 -0400

    fix migration

---
+++ database/migrations/2018_06_11_184017_change_default_user_table.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeDefaultUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function ($table) {
+            $table->string('name_order')->default('firstname_lastname_nickname')->change();
+        });
+    }
+}
