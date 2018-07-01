1cde7bb7962c597eb975bae6f5006da92132db11
djaiss <rfrgpg@sent.as>
2018-07-01 15:31:01 +0000

    Apply fixes from StyleCI

--- app/Models/User/User.php
+++ app/Models/User/User.php
@@ -9,7 +9,6 @@ use App\Models\Settings\Term;
 use App\Models\Account\Account;
 use App\Models\Contact\Reminder;
 use App\Models\Settings\Currency;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\DB;
 use Laravel\Passport\HasApiTokens;
 use Illuminate\Support\Facades\App;
