c858d9e79e302b067ef170ac4d3e21a6f0d74bdd
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2018-07-05 17:20:03 +0200

    New translations auth.php (Hebrew)

--- resources/lang/he/auth.php
+++ resources/lang/he/auth.php
@@ -19,7 +19,7 @@ return [
     'signup_disabled' => 'ההרשמה מושבתת כרגע',
     'back_homepage' => 'חזרה לדף הבית',
     'mfa_auth_otp' => 'אימות עם ההתקן שלך לאימות דו־שלבי',
-    'mfa_auth_u2f' => 'Authenticate with a U2F device',
+    'mfa_auth_u2f' => 'אימות עם התקן U2F',
     '2fa_title' => 'אימות דו־שלבי',
     '2fa_wrong_validation' => 'האימות הדו־שלבי נכשל.',
     '2fa_one_time_password' => 'קוד אימות דו־שלבי',
