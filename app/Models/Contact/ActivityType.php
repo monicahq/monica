dda4e67540bc19ced9b1b70684e39232ccbdf484
Regis Freyd <djaiss@users.noreply.github.com>
2018-07-12 07:53:52 -0400

    fix palsm and tests

--- app/Models/Contact/ActivityType.php
+++ app/Models/Contact/ActivityType.php
@@ -4,6 +4,7 @@ namespace App\Models\Contact;
 
 use App\Models\Account\Account;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ActivityType extends Model
 {
--- app/Models/Contact/ActivityTypeCategory.php
+++ app/Models/Contact/ActivityTypeCategory.php
@@ -4,6 +4,8 @@ namespace App\Models\Contact;
 
 use App\Models\Account\Account;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ActivityTypeCategory extends Model
 {
