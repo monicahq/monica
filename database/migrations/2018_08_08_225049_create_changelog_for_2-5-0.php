3b273762a1bfaf0dd9c53242df28fab7dcf4a8be
djaiss <rfrgpg@sent.as>
2018-08-08 23:22:39 +0000

    Apply fixes from StyleCI

--- database/migrations/2018_08_08_225049_create_changelog_for_2-5-0.php
+++ database/migrations/2018_08_08_225049_create_changelog_for_2-5-0.php
@@ -1,8 +1,6 @@
 <?php
 
 use App\Models\Instance\Instance;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 class CreateChangelogFor250 extends Migration
