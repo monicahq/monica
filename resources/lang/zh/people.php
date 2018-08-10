e3b35f569a7be04ea5ab9fd897e73f1e2403f89d
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2018-08-10 19:21:02 +0200

    New translations people.php (Chinese Simplified)

--- resources/lang/zh/people.php
+++ resources/lang/zh/people.php
@@ -113,7 +113,7 @@ return [
     'information_no_linkedin_defined' => '没有定义LinkedIn',
     'information_no_work_defined' => '未定义工作信息',
     'information_work_at' => '在 :company',
-    'linkedin_profile' => 'LinkedIn profile',
+    'linkedin_profile' => '领英®上的个人资料',
     'work_add_cta' => '更新工作信息',
     'work_edit_success' => '工作信息已成功更新',
     'work_edit_title' => '更新:name的工作信息',
