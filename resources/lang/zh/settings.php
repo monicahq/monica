fc8f50e1dc1a9603a11e2483904938712c0e9c04
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2018-08-11 05:50:50 +0200

    New translations settings.php (Chinese Simplified)

--- resources/lang/zh/settings.php
+++ resources/lang/zh/settings.php
@@ -60,7 +60,7 @@ return [
     'locale_pt' => '葡萄牙文',
     'locale_ru' => '俄文',
     'locale_zh' => 'Chinese Simplified',
-    'locale_tr' => 'Turkish',
+    'locale_tr' => '土耳其文',
 
     'security_title' => '安全',
     'security_help' => '更改您的帐户的安全选项。',
