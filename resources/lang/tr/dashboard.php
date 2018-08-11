b3a06d8924c365fa4328fa2fe4ce1de4b0711f8d
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2018-08-11 10:21:10 +0200

    New translations dashboard.php (Turkish)

--- resources/lang/tr/dashboard.php
+++ resources/lang/tr/dashboard.php
@@ -1,14 +1,14 @@
 <?php
 
 return [
-    'dashboard_blank_title' => 'Welcome to your account!',
+    'dashboard_blank_title' => 'Hesabınıza hoş geldiniz!',
     'dashboard_blank_description' => 'Monica is the place to organize all the interactions you have with the ones you care about.',
-    'dashboard_blank_cta' => 'Add your first contact',
+    'dashboard_blank_cta' => 'İlk bağlantınızı ekleyin',
     'dashboard_blank_illustration' => 'Illustration by <a href=":url">Freepik</a>',
 
     'notes_title' => 'You don’t have any starred notes yet.',
 
-    'tab_recent_calls' => 'Recent calls',
+    'tab_recent_calls' => 'Son çağrılar',
     'tab_favorite_notes' => 'Favorite notes',
     'tab_calls_blank' => 'You haven’t logged a call yet.',
     'tab_debts' => 'Debts',
