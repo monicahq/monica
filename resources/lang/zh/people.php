dd94ca1deda98ecd987bc61210c936da03e87d0b
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2018-08-13 03:11:54 +0200

    New translations people.php (Chinese Simplified)

--- resources/lang/zh/people.php
+++ resources/lang/zh/people.php
@@ -225,6 +225,13 @@ return [
     'activities_delete_success' => '活动已成功删除',
     'activities_who_was_involved' => '谁参与了？',
     'activities_activity' => '活动类别',
+    'activities_view_activities_report' => 'View activities report',
+    'activities_profile_title' => 'Activities report between :name and you',
+    'activities_profile_subtitle' => 'You’ve logged :total_activities activities with :name in total and :activities_last_twelve_months in the last 12 months so far.',
+    'activities_profile_year_summary_activity_types' => 'Here is a breakdown of the type of activities you’ve done together in :year',
+    'activities_profile_year_summary' => 'Here is what you two have done in :year',
+    'activities_profile_number_occurences' => '{0,1}:value activity|{2,*}:value activities',
+
     // notes
     'notes_create_success' => '注释已成功创建',
     'notes_update_success' => '便笺已成功保存',
