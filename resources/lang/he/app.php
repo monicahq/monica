48dd855b2488a84a58e9108ed8baf4929310f3d3
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2018-08-13 17:25:45 +0200

    New translations app.php (Hebrew)

--- resources/lang/he/app.php
+++ resources/lang/he/app.php
@@ -73,7 +73,7 @@ return [
     'breadcrumb_settings_personalization' => 'התאמה אישית',
     'breadcrumb_settings_security' => 'אבטחה',
     'breadcrumb_settings_security_2fa' => 'אימות דו־שלבי',
-    'breadcrumb_profile' => 'Profile of :name',
+    'breadcrumb_profile' => 'הפרופיל של :name',
 
     'gender_male' => 'גבר',
     'gender_female' => 'אישה',
