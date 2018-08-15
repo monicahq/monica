0503206c449f1866f9b58571f5e732c3587f57dd
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2018-08-15 21:51:02 +0200

    New translations app.php (French)

--- resources/lang/fr/app.php
+++ resources/lang/fr/app.php
@@ -73,7 +73,7 @@ return [
     'breadcrumb_settings_personalization' => 'Personnalisation',
     'breadcrumb_settings_security' => 'Sécurité',
     'breadcrumb_settings_security_2fa' => 'Authentification à deux facteurs',
-    'breadcrumb_profile' => 'Profile of :name',
+    'breadcrumb_profile' => 'Profil de :name',
 
     'gender_male' => 'Homme',
     'gender_female' => 'Femme',
