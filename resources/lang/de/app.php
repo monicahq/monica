2d7d421fd879e023a8b8a601fcf686aadfe86bb4
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2018-08-17 07:51:03 +0200

    New translations app.php (German)

--- resources/lang/de/app.php
+++ resources/lang/de/app.php
@@ -26,7 +26,7 @@ return [
     'application_description' => 'Monica ist ein Werkzeug, um Ihre Interaktionen mit Ihren Lieben, Freunden und Familie zu verwalten.',
     'application_og_title' => 'Haben Sie bessere Beziehungen zu Ihren Lieben. Kostenloses Online CRM für Freunde und Familie.',
 
-    'markdown_description' => 'Du möchtest deinen Test schöner formatieren? Monica unterstützt Markdown.',
+    'markdown_description' => 'Du möchtest deinen Text schöner formatieren? Monica unterstützt Markdown.',
     'markdown_link' => 'Öffne die Dokumentation',
 
     'header_settings_link' => 'Einstellungen',
