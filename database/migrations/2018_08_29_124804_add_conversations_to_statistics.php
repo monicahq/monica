19ba1b2764ac5e2e56f5193045c438ed5d0fc531
Regis Freyd <djaiss@users.noreply.github.com>
2018-08-29 16:31:42 -0400

    add calculate statistics

---
+++ database/migrations/2018_08_29_124804_add_conversations_to_statistics.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddConversationsToStatistics extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('statistics', function ($table) {
+            $table->integer('number_of_conversations')->after('number_of_import_jobs')->nullable();
+            $table->integer('number_of_messages')->after('number_of_conversations')->nullable();
+        });
+    }
+}
