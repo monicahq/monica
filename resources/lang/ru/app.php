dd01016ffb3bda88e0174f7ffad314f76f8dca89
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2018-08-31 13:20:54 +0200

    New translations app.php (Russian)

--- resources/lang/ru/app.php
+++ resources/lang/ru/app.php
@@ -21,6 +21,11 @@ return [
     'loading' => 'Loading...',
     'with' => 'with',
     'days' => 'day|days',
+    'today' => 'today',
+    'yesterday' => 'yesterday',
+    'another_day' => 'another day',
+    'date' => 'Date',
+    'type' => 'Type',
 
     'application_title' => 'Monica – personal relationship manager',
     'application_description' => 'Monica is a tool to manage your interactions with your loved ones, friends and family.',
