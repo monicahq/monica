4f4b7bcac7fcf630c4751f90d9d9026329c0dbfe
Regis Freyd <djaiss@users.noreply.github.com>
2018-09-04 21:51:52 -0400

    add changelog entry

---
+++ database/migrations/2018_09_05_013730_update_changelog_for_conversations.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateChangelogForConversations extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $description = '
+**New feature: conversations**
+
+You can now log conversations that you have on social media or SMS or else. While you could already do that using notes, conversations is an easier way to record them using a nice user interface.
+
+![image](/img/changelogs/2018-09-04-conversations.png)';
+
+        $id = DB::table('changelogs')->insertGetId([
+            'description' => $description,
+            'created_at' => '2018-09-04',
+        ]);
+
+        $instance = Instance::first();
+        $instance->addUnreadChangelogEntry($id);
+    }
+}
