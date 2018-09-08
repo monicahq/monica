173f5a166e8169f1f9969a0f158d524a5d02d050
Alexis Saettler <alexis@saettler.org>
2018-09-08 13:21:41 +0200

    Not send confirm email if signup_double_optin is false

---
+++ app/Services/User/EmailChange.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Services\User;
+
+use App\Services\BaseService;
+use App\Notifications\ConfirmEmail;
+use App\Exceptions\MissingParameterException;
+
+class EmailChange extends BaseService
+{
+    /**
+     * The structure that the method expects to receive as parameter.
+     *
+     * @var array
+     */
+    private $structure = [
+        'email',
+    ];
+
+    /**
+     * Update email of the user.
+     *
+     * @param array $data
+     */
+    public function execute(array $data)
+    {
+        if (! $this->validateDataStructure($data, $this->structure)) {
+            throw new MissingParameterException('Missing parameters');
+        }
+
+        $user = auth()->user();
+
+        // Change email of the user
+        $user->email = $data['email'];
+
+        if (config('monica.signup_double_optin')) {
+            // Resend validation token
+            $user->confirmation_code = str_random(30);
+            $user->confirmed = false;
+            $user->save();
+
+            $user->notify(new ConfirmEmail);
+            return;
+        }
+
+        $user->save();
+    }
+}
