870b27686022396edaa15a78f0d39538658010ad
Alexis Saettler <alexis@saettler.org>
2018-09-10 00:20:19 +0200

    fix: fix move avatars command

--- app/Console/Commands/OneTime/MoveAvatars.php
+++ app/Console/Commands/OneTime/MoveAvatars.php
@@ -18,7 +18,7 @@ class MoveAvatars extends Command
      *
      * @var string
      */
-    protected $signature = 'monica:moveavatars {--force : Force the operation to run when in production.} {--dryrun : Simulate the execution but not write anything.}';
+    protected $signature = 'monica:moveavatars {--force : Force the operation to run when in production.} {--dryrun : Simulate the execution but not write anything.} {--storage : new storage to move avatars to}';
 
     /**
      * The console command description.
@@ -41,21 +41,19 @@ class MoveAvatars extends Command
         Contact::where('has_avatar', true)
             ->chunk(200, function ($contacts) {
                 foreach ($contacts as $contact) {
-                    if ($contact->avatar_location == config('filesystems.default')) {
-                        return;
+                    if ($contact->avatar_location == $this->newStorage()) {
+                        continue;
                     }
 
                     try {
                         // move avatars to new location
-                        $this->moveAvatarSize($contact);
-                        $this->moveAvatarSize($contact, 110);
-                        $this->moveAvatarSize($contact, 174);
+                        $this->moveContactAvatars($contact);
 
                         if (! $this->option('dryrun')) {
                             $contact->deleteAvatars();
 
                             // Update location. The filename has not changed.
-                            $contact->avatar_location = config('filesystems.default');
+                            $contact->avatar_location = $this->newStorage();
                             $contact->save();
                         }
                     } catch (FileNotFoundException $e) {
@@ -65,7 +63,34 @@ class MoveAvatars extends Command
             });
     }
 
-    private function moveAvatarSize($contact, $size = null)
+    private function moveContactAvatars($contact)
+    {
+        $this->line('Contact id:'.$contact->id);
+        $this->line(' > Avatar location:'.$contact->avatar_location);
+        $this->line(' > File name:'.$contact->avatar_file_name);
+
+        $avatars = [];
+        array_push($avatars, $this->getFileName($contact));
+        array_push($avatars, $this->getFileName($contact, 110));
+        array_push($avatars, $this->getFileName($contact, 174));
+
+        $storage = Storage::disk($contact->avatar_location);
+        $newStorage = Storage::disk($this->newStorage());
+
+        foreach ($avatars as $avatar) {
+            $avatarFile = $storage->get($avatar);
+
+            if (! $this->option('dryrun')) {
+                $newStorage->put($avatar, $avatarFile, 'public');
+            }
+
+            if ($this->getOutput()->getVerbosity() >= OutputInterface::VERBOSITY_VERBOSE) {
+                $this->line('  Moved file '.$avatar);
+            }
+        }
+    }
+
+    private function getFileName($contact, $size = null)
     {
         $filename = pathinfo($contact->avatar_file_name, PATHINFO_FILENAME);
         $extension = pathinfo($contact->avatar_file_name, PATHINFO_EXTENSION);
@@ -75,23 +100,27 @@ class MoveAvatars extends Command
             $avatarFileName = 'avatars/'.$filename.'_'.$size.'.'.$extension;
         }
 
-        $storage = Storage::disk($contact->avatar_location);
+        if ($this->fileExists($contact->avatar_location, $avatarFileName)) {
+            return $avatarFileName;
+        }
+    }
+
+    private function fileExists($storage, $avatarFileName) : bool
+    {
+        $storage = Storage::disk($storage);
+
         if (! $storage->exists($avatarFileName)) {
             if ($this->getOutput()->getVerbosity() >= OutputInterface::VERBOSITY_VERBOSE) {
-                $this->line('File not found: '.$avatarFileName);
+                $this->line('  ! File not found: '.$avatarFileName);
             }
-
-            return;
-        }
-        $avatarFile = $storage->get($avatarFileName);
-
-        $newStorage = Storage::disk(config('filesystems.default'));
-        if (! $this->option('dryrun')) {
-            $newStorage->put($avatarFileName, $avatarFile, 'public');
+            throw new FileNotFoundException();
         }
 
-        if ($this->getOutput()->getVerbosity() >= OutputInterface::VERBOSITY_VERBOSE) {
-            $this->line('Moved file '.$avatarFileName);
-        }
+        return true;
+    }
+
+    private function newStorage()
+    {
+        return $this->option('storage') ?? config('filesystems.default');
     }
 }
