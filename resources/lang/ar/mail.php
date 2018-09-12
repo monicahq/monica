173a51e3a35aaf9b4912b7d2eb7e60df056b108a
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2018-09-12 14:51:32 +0200

    New translations mail.php (Arabic)

--- resources/lang/ar/mail.php
+++ resources/lang/ar/mail.php
@@ -8,18 +8,18 @@ return [
     'for' => 'من أجل:',
     'footer_contact_info' => 'أضف، عرض، أكمل، و قم بتغيير معلومات عن جهة الإتصال هذه:',
 
-    'notification_subject_line' => 'You have an upcoming event',
+    'notification_subject_line' => 'لديك حدث قادم',
     'notification_description' => 'In :count days (on :date), the following event will happen:',
 
-    'stay_in_touch_subject_line' => 'Stay in touch with :name',
+    'stay_in_touch_subject_line' => 'ابقى على إتصال مع :اسم',
     'stay_in_touch_subject_description' => 'You asked to be reminded to stay in touch with :name every :frequency days.',
 
-    'notifications_whoops' => 'Whoops!',
-    'notifications_hello' => 'Hello!',
-    'notifications_regards' => 'Regards',
+    'notifications_whoops' => 'المعذرة!',
+    'notifications_hello' => 'مرحباً!',
+    'notifications_regards' => 'مع خالص التحيات',
     'notifications_footer' => 'If you’re having trouble clicking the ":actionText" button, copy and paste the URL below into your web browser: [:actionURL](:actionURL)',
 
-    'confirmation_email_title' => 'Monica – Email verification',
-    'confirmation_email_intro'=> 'To validate your email click on the button below',
+    'confirmation_email_title' => 'Monica - تأكيد البريد الإلكتروني',
+    'confirmation_email_intro'=> 'لتأكيد بريدك اضغط على الزر أدناه',
     'confirmation_email_button' => 'Verify email address',
 ];
