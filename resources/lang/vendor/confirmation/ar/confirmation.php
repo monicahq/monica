51c600210a1782790c61be1e58e2389a48752ff0
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2018-09-12 13:53:02 +0200

    New translations confirmation.php (Arabic)

--- resources/lang/vendor/confirmation/ar/confirmation.php
+++ resources/lang/vendor/confirmation/ar/confirmation.php
@@ -3,8 +3,8 @@
 return [
     'message' => 'شكراً لتسجيل إشتراكك! الرجاء التحقق من بريدك الإلكتروني لتأكيد بريدك.',
     'success' => 'لقد تم تأكيد حسابك بنجاح! يمكنك تسجيل الدخول الآن.',
-    'again' => 'You must verify your email before you can access the site.
-                <br/>If you have not received the confirmation email check your spam folder.
-                <br/>To get a new confirmation email please <a href=":url" class="alert-link">click here</a>.',
-    'resend' => 'A confirmation message has been sent. Please check your mailbox.'
+    'again' => 'يجب أن تقوم بتأكيد بريدك الإلكتروني قبل أن يمكنك دخول الموقع.
+                <br/>إذا لم تتلقى رسالة التأكيد فتحقق من مجلد البريد الغير هام.
+                <br/>للحصول على رسألة تأكيد جديدة، الرجاء <a href=":url" class="alert-link">الضغط هنا</a>.',
+    'resend' => 'تم إرسال رسألة التأكيد. الرجاء التحقق من صندوق بريدك.'
 ];
