98eb4497b9dcd675bef74e9e26de086f8ee72475
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2018-09-19 00:21:42 +0200

    New translations mail.php (Hebrew)

--- resources/lang/he/mail.php
+++ resources/lang/he/mail.php
@@ -4,20 +4,23 @@ return [
 
     'subject_line' => 'תזכורת עבור :contact',
     'greetings' => 'היי :username',
-    'want_reminded_of' => 'רצית שנזכיר לך בנושא',
-    'for' => 'עבור:',
+    'want_reminded_of' => 'You wanted to be reminded of :reason',
+    'for' => 'For: :name',
+    'comment' => 'Comment: :comment',
     'footer_contact_info' => 'הוספה, צפייה, השלמה ושינוי מידע על איש הקשר הזה:',
+    'footer_contact_info2' => 'See :name’s profile',
 
     'notification_subject_line' => 'יש לך אירוע קרב',
     'notification_description' => 'בעוד :count ימים (ב־:date), יתרחש האירוע הבא:',
 
     'stay_in_touch_subject_line' => 'לשמור על קשר עם :name',
-    'stay_in_touch_subject_description' => 'ביקשת לקבל תזכורת לשמור על קשר עם :name כל :frequency ימים.',
+    'stay_in_touch_subject_description' => 'You asked to be reminded to stay in touch with :name every :frequency day.|You asked to be reminded to stay in touch with :name every :frequency days.',
 
     'notifications_whoops' => 'אופס!',
     'notifications_hello' => 'שלום!',
     'notifications_regards' => 'בברכה',
     'notifications_footer' => 'אם נתקלת בבעיה בלחיצה על הכפתור „:actionText”, יש להעתיק ולהדביק את הכתובת שלהלן לתוך הדפדפן שלך: [:actionURL](:actionURL)',
+    'notifications_rights' => 'All rights reserved',
 
     'confirmation_email_title' => 'מוניקה – אימות דוא״ל',
     'confirmation_email_intro'=> 'כדי לאמת את כתובת הדוא״ל שלך נא ללחוץ על הכפתור שלהלן',
