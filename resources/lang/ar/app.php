01878fa05e1a2ee24df3ac2fd9aa5c352203878b
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2018-09-18 23:51:15 +0200

    New translations app.php (Arabic)

--- resources/lang/ar/app.php
+++ resources/lang/ar/app.php
@@ -88,6 +88,11 @@ return [
     'error_unauthorized' => 'ليس لديك الصلاحية لتحرير هذا المصدر.',
     'error_save' => 'لقد حصل خطأ بينما كنا نحاول حفظ البيانات.',
     'error_try_again' => 'حدث خطأ ما. الرجاء المحاولة مرة أخرى.',
+    'error_id' => 'Error ID: :id',
+    'error_unavailable' => 'Service Unavailable',
+    'error_maintenance' => 'Maintenance in progress. Be right back.',
+    'error_help' => 'We’ll be right back.',
+    'error_twitter' => 'Follow <a href="https://twitter.com/:twitter">our Twitter account</a> to be alerted when it’s up again.',
 
     'default_save_success' => 'تم حفظ البيانات.',
 
