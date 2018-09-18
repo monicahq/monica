090118f375aff7a4248a4babeaf4b166f9ea3176
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2018-09-18 23:51:24 +0200

    New translations app.php (French)

--- resources/lang/fr/app.php
+++ resources/lang/fr/app.php
@@ -88,6 +88,11 @@ return [
     'error_unauthorized' => 'Vous n’avez pas le droit de modifier cette ressource.',
     'error_save' => 'Une erreur est intervenue pendant la sauvegarde des données.',
     'error_try_again' => 'Une erreur s’est produite. Merci d’essayer à nouveau.',
+    'error_id' => 'Error ID: :id',
+    'error_unavailable' => 'Service Unavailable',
+    'error_maintenance' => 'Maintenance in progress. Be right back.',
+    'error_help' => 'We’ll be right back.',
+    'error_twitter' => 'Follow <a href="https://twitter.com/:twitter">our Twitter account</a> to be alerted when it’s up again.',
 
     'default_save_success' => 'Les modifications ont été enregistrées.',
 
