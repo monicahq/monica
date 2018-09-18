37533a47766cb15977ed2ad82e0d1dfb136cf2df
Regis Freyd <djaiss@users.noreply.github.com>
2018-09-18 15:41:56 -0400

    wip

---
+++ database/migrations/2018_09_18_142844_remove_events.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RemoveEvents extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::drop('events');
+    }
+}
