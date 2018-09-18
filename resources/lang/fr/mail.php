639a78475c09a732e3c8dc967319fc671f4221bf
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2018-09-19 00:21:47 +0200

    New translations mail.php (French)

--- resources/lang/fr/mail.php
+++ resources/lang/fr/mail.php
@@ -4,20 +4,23 @@ return [
 
     'subject_line' => 'Rappel pour :contact',
     'greetings' => 'Bonjour :username',
-    'want_reminded_of' => 'VOUS VOULIEZ ÊTRE RAPPELÉ DE',
-    'for' => 'POUR :',
+    'want_reminded_of' => 'You wanted to be reminded of :reason',
+    'for' => 'For: :name',
+    'comment' => 'Comment: :comment',
     'footer_contact_info' => 'Ajouter, afficher, compléter et modifier les informations sur ce contact :',
+    'footer_contact_info2' => 'See :name’s profile',
 
     'notification_subject_line' => 'Vous avez un événement à venir',
     'notification_description' => 'Dans :count jours (le :date), l’événement suivant se produira :',
 
     'stay_in_touch_subject_line' => 'Rester en contact avec :name',
-    'stay_in_touch_subject_description' => 'Vous avez demandé à être rappelé de rester en contact avec :name tous les :frequency jours.',
+    'stay_in_touch_subject_description' => 'You asked to be reminded to stay in touch with :name every :frequency day.|You asked to be reminded to stay in touch with :name every :frequency days.',
 
     'notifications_whoops' => 'Oups !',
     'notifications_hello' => 'Bonjour !',
     'notifications_regards' => 'Cordialement',
     'notifications_footer' => 'Si vous rencontrez des problèmes en cliquant sur le bouton « :actionText », copiez et collez l’URL ci-dessous dans votre navigateur web : [:actionURL](:actionURL)',
+    'notifications_rights' => 'All rights reserved',
 
     'confirmation_email_title' => 'Monica – vérification d’adresse courriel',
     'confirmation_email_intro'=> 'Pour valider votre adresse courriel, cliquez sur le bouton ci-dessous',
