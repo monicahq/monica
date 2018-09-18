e7ba2be46a1c26a835d77aa02b8e030a936fcf0e
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2018-09-19 00:21:25 +0200

    New translations mail.php (Arabic)

--- resources/lang/ar/mail.php
+++ resources/lang/ar/mail.php
@@ -4,20 +4,23 @@ return [
 
     'subject_line' => 'تذكير إلى :contact',
     'greetings' => 'مرحباً :username',
-    'want_reminded_of' => 'لقد أردتَ أن يتم تذكيرك بـ',
-    'for' => 'من أجل:',
+    'want_reminded_of' => 'You wanted to be reminded of :reason',
+    'for' => 'For: :name',
+    'comment' => 'Comment: :comment',
     'footer_contact_info' => 'أضف، عرض، أكمل، و قم بتغيير معلومات عن جهة الإتصال هذه:',
+    'footer_contact_info2' => 'See :name’s profile',
 
     'notification_subject_line' => 'لديك حدث قادم',
     'notification_description' => 'في :count أيام ( في :date)، سوف يحصل الحدث التالي:',
 
     'stay_in_touch_subject_line' => 'ابقى على إتصال مع :name',
-    'stay_in_touch_subject_description' => 'لقد طلبتَ أن يتم تذكيرك بالبقاء على اتصال مع :name كل :frequency أيام.',
+    'stay_in_touch_subject_description' => 'You asked to be reminded to stay in touch with :name every :frequency day.|You asked to be reminded to stay in touch with :name every :frequency days.',
 
     'notifications_whoops' => 'المعذرة!',
     'notifications_hello' => 'مرحباً!',
     'notifications_regards' => 'مع خالص التحيات',
     'notifications_footer' => 'إذا كنتَ تواجه مشكلة في الضغط على زر ":actionText"، انسخ والصق العنوان في متصفح الويب الخاص بك: [:actionURL](:actionURL)',
+    'notifications_rights' => 'All rights reserved',
 
     'confirmation_email_title' => 'Monica - تأكيد البريد الإلكتروني',
     'confirmation_email_intro'=> 'لتأكيد بريدك اضغط على الزر أدناه',
