ac9addeb8a62eb6053063fb76d47d569388ce400
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2018-09-22 17:31:42 +0200

    New translations settings.php (Russian)

--- resources/lang/ru/settings.php
+++ resources/lang/ru/settings.php
@@ -56,6 +56,7 @@ return [
     'locale_es' => 'Spanish',
     'locale_fr' => 'Французкий',
     'locale_he' => 'Hebrew',
+    'locale_hr' => 'Croatian',
     'locale_it' => 'итальянский',
     'locale_nl' => 'Dutch',
     'locale_pt' => 'Portuguese',
