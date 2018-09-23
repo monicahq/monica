ba7a571ad7ff18f5ff00e639e51205ae66c167bd
Alexis Saettler <alexis@saettler.org>
2018-09-23 15:05:46 +0200

    use sentry:release

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,8 +1,9 @@
 @servers(['local' => 'localhost', 'web' => '{{ $site }}@deploy.{{ $region }}.frbit.com'])
 
 @setup
-    $commit = exec('git log --pretty="%h" -n1 HEAD');
-    $version = exec('php artisan monica:getversion');
+    $commit = exec('git log --pretty="%H" -n1 HEAD');
+    //$version = exec('php artisan monica:getversion');
+    $release = exec('git log --pretty="%h" -n1 HEAD');
 @endsetup
 
 @story('deploy')
@@ -16,7 +17,10 @@
 @endtask
 
 @task('git', ['on' => 'local'])
-    git push -u fortrabbit {{ $branch ?: 'master' }}
+    git fetch origin
+    git checkout {{ $branch ?? 'master' }}
+    git pull
+    git push -u fortrabbit {{ $branch ?? 'master' }}
 @endtask
 
 @task('composer', ['on' => 'web'])
@@ -28,28 +32,5 @@
 @endtask
 
 @task('sentry', ['on' => 'web'])
-    
-    source .env 2>/dev/null
-    if [ "$SENTRY_SUPPORT" == "true" ]; then
-  
-        if [ ! -x ~/.local/bin/sentry-cli ]; then
-            mkdir -p ~/.local/bin
-            curl -sL https://sentry.io/get-cli/ | INSTALL_DIR=~/.local/bin bash
-        fi
-
-        if [ -x ~/.local/bin/sentry-cli ]; then
-            export SENTRY_AUTH_TOKEN
-            export SENTRY_ORG
-
-            # Create a release
-            ~/.local/bin/sentry-cli releases new -p $SENTRY_PROJECT --finalize {{ $commit }}
-
-            # Associate commits with the release
-            ~/.local/bin/sentry-cli releases set-commits --auto {{ $commit }}
-
-            # Create a deploy
-            ~/.local/bin/sentry-cli releases deploys {{ $commit }} new -e $SENTRY_ENV -n {{ $version }}
-        fi
-    fi
-
+    php artisan sentry:release --force -vvv --release={{ $release }} --environment=production --commit={{ $commit }}
 @endtask
