bc35d18b37e5f22a62361a22272750f886342356
Alexis Saettler <alexis@saettler.org>
2018-09-28 21:48:21 +0000

    Apply fixes from StyleCI

--- config/passport.php
+++ config/passport.php
@@ -20,8 +20,8 @@ $passports = [
 ];
 
 // Use fortrabbit secrets
-if (isset($_SERVER["APP_SECRETS"])) {
-    $secrets = json_decode(file_get_contents($_SERVER["APP_SECRETS"]), true);
+if (isset($_SERVER['APP_SECRETS'])) {
+    $secrets = json_decode(file_get_contents($_SERVER['APP_SECRETS']), true);
 
     if (isset($secrets['CUSTOM']['PASSPORT_PRIVATE_KEY'])) {
         $passports['private_key'] = str_replace('\\\\n', '\\n', $secrets['CUSTOM']['PASSPORT_PRIVATE_KEY']);
