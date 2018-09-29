113226e38b8d120e4acef1c9a287660da3bec435
Régis Freyd <djaiss@users.noreply.github.com>
2018-09-28 21:12:52 -0400

    chore: update i18n (#1853)

--- resources/lang/fr/mail.php
+++ resources/lang/fr/mail.php
@@ -4,20 +4,23 @@ return [
 
     'subject_line' => 'Rappel pour :contact',
     'greetings' => 'Bonjour :username',
-    'want_reminded_of' => 'VOUS VOULIEZ ÊTRE RAPPELÉ DE',
-    'for' => 'POUR :',
+    'want_reminded_of' => 'Vous souhaitez être rappelé de :reason',
+    'for' => 'Pour : :name',
+    'comment' => 'Commentaire : :comment',
     'footer_contact_info' => 'Ajouter, afficher, compléter et modifier les informations sur ce contact :',
+    'footer_contact_info2' => 'Voir le profil de :name',
 
     'notification_subject_line' => 'Vous avez un événement à venir',
     'notification_description' => 'Dans :count jours (le :date), l’événement suivant se produira :',
 
     'stay_in_touch_subject_line' => 'Rester en contact avec :name',
-    'stay_in_touch_subject_description' => 'Vous avez demandé à être rappelé de rester en contact avec :name tous les :frequency jours.',
+    'stay_in_touch_subject_description' => 'Vous avez demandé à être rappelé de rester en contact avec :name tous les :frequency jour.|Vous avez demandé à être rappelé de rester en contact avec :name tous les :frequency jours.',
 
     'notifications_whoops' => 'Oups !',
     'notifications_hello' => 'Bonjour !',
-    'notifications_regards' => 'Cordialement,',
+    'notifications_regards' => 'Cordialement',
     'notifications_footer' => 'Si vous rencontrez des problèmes en cliquant sur le bouton « :actionText », copiez et collez l’URL ci-dessous dans votre navigateur web : [:actionURL](:actionURL)',
+    'notifications_rights' => 'Tous droits réservés',
 
     'confirmation_email_title' => 'Monica – vérification d’adresse courriel',
     'confirmation_email_intro'=> 'Pour valider votre adresse courriel, cliquez sur le bouton ci-dessous',
