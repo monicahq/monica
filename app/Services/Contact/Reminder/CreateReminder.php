fcb2c37fa6767cce060aa384474b13076174c16e
Regis Freyd <djaiss@users.noreply.github.com>
2018-09-30 09:43:47 -0400

    fix failing tests

--- app/Services/Contact/Reminder/CreateReminder.php
+++ app/Services/Contact/Reminder/CreateReminder.php
@@ -2,7 +2,7 @@
 
 namespace App\Services\Contact\Reminder;
 
-use Illuminate\Validation\Validator;
+use Illuminate\Support\Facades\Validator;
 use App\Helpers\DateHelper;
 use App\Services\BaseService;
 use App\Models\Contact\Contact;
