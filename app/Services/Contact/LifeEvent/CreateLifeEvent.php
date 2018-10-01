21c9e9f61c3e01f96af0bee00adde03ae955e8c9
Régis Freyd <djaiss@users.noreply.github.com>
2018-09-30 21:59:21 -0400

    feat: add ability to set a reminder for a life event (#1855)

--- app/Services/Contact/LifeEvent/CreateLifeEvent.php
+++ app/Services/Contact/LifeEvent/CreateLifeEvent.php
@@ -22,6 +22,9 @@ class CreateLifeEvent extends BaseService
         'happened_at',
         'name',
         'note',
+        'has_reminder',
+        'happened_at_month_unknown',
+        'happened_at_day_unknown',
     ];
 
     /**
@@ -42,6 +45,45 @@ class CreateLifeEvent extends BaseService
         LifeEventType::where('account_id', $data['account_id'])
             ->findOrFail($data['life_event_type_id']);
 
-        return LifeEvent::create($data);
+        $lifeEvent = new LifeEvent;
+        $lifeEvent->account_id = $data['account_id'];
+        $lifeEvent->contact_id = $data['contact_id'];
+        $lifeEvent->life_event_type_id = $data['life_event_type_id'];
+        $lifeEvent->happened_at = $data['happened_at'];
+        $lifeEvent->name = $data['name'];
+        $lifeEvent->note = $data['note'];
+        $lifeEvent->happened_at_month_unknown = $data['happened_at_month_unknown'];
+        $lifeEvent->happened_at_day_unknown = $data['happened_at_day_unknown'];
+        $lifeEvent->save();
+
+        $this->addYearlyReminder($data, $lifeEvent);
+
+        // Get the newly created object as the Create method doesn't return all
+        // fields by default
+        return LifeEvent::find($lifeEvent->id);
+    }
+
+    /**
+     * Add yearly reminder if necessary.
+     *
+     * @param array $data
+     * @param LifeEvent $lifeEvent
+     */
+    private function addYearlyReminder($data, $lifeEvent)
+    {
+        if ($data['has_reminder']) {
+            $array = [
+                'account_id' => $data['account_id'],
+                'life_event_id' => $lifeEvent->id,
+                'date' => $data['happened_at'],
+                'frequency_type' => 'year',
+                'frequency_number' => 1,
+            ];
+
+            $reminder = (new AddReminderToLifeEvent)->execute($array);
+
+            $lifeEvent->reminder_id = $reminder->id;
+            $lifeEvent->save();
+        }
     }
 }
