d60b76a20f67d787d797e075f5a4eefccd25fd84
Alexis Saettler <alexis@saettler.org>
2018-10-04 22:20:16 +0200

    fix: fix vehicle misspell

---
+++ database/migrations/2018_10_04_181116_life_event_vehicle.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class LifeEventVehicle extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('default_life_event_types')
+            ->where('translation_key', 'new_vehicule')
+            ->update(['translation_key' => 'new_vehicle']);
+    }
+}
