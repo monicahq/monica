a8f8c0fa6c410f95f89742ed8528647bebe16291
Alexis Saettler <alexis@saettler.org>
2018-10-07 12:52:24 +0000

    Apply fixes from StyleCI

--- database/migrations/2018_10_07_120133_fix_json_column.php
+++ database/migrations/2018_10_07_120133_fix_json_column.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 class FixJsonColumn extends Migration
@@ -26,15 +24,15 @@ class FixJsonColumn extends Migration
                             ->select('table_name', 'column_name')
                             ->where([
                                 ['table_schema', '=', $databasename],
-                                ['data_type', '=', 'json']
+                                ['data_type', '=', 'json'],
                             ])
                             ->whereIn('table_name', [
                                 'default_life_event_types',
                                 'life_event_types',
-                                'life_events'
+                                'life_events',
                             ])
                             ->get();
-        
+
         foreach ($columns as $column) {
             DB::statement('ALTER TABLE `'.$databasename.'`.`'.$column->table_name.'` MODIFY `'.$column->column_name.'` text;');
         }
