4ee023b1daeab1fb8f7b3fe974102314ed1dafd5
Alexis Saettler <alexis@saettler.org>
2018-10-07 22:28:40 +0000

    Apply fixes from StyleCI

--- database/migrations/2018_10_04_181116_life_event_vehicle.php
+++ database/migrations/2018_10_04_181116_life_event_vehicle.php
@@ -15,14 +15,14 @@ class LifeEventVehicle extends Migration
         DB::table('default_life_event_types')
             ->where('translation_key', 'new_vehicule')
             ->update([
-                'translation_key' => 'new_vehicle'
+                'translation_key' => 'new_vehicle',
             ]);
 
         DB::table('life_event_types')
             ->where('default_life_event_type_key', 'new_vehicule')
             ->update([
                 'default_life_event_type_key' => 'new_vehicle',
-                'name' => trans('settings.personalization_life_event_type_new_vehicle', [], 'en')
+                'name' => trans('settings.personalization_life_event_type_new_vehicle', [], 'en'),
             ]);
     }
 }
