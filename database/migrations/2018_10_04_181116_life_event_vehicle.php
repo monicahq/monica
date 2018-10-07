c57bce499b76b0e9f1cdfeb21e6d8fefac7c4438
Alexis Saettler <alexis@saettler.org>
2018-10-08 00:28:22 +0200

    update

--- database/migrations/2018_10_04_181116_life_event_vehicle.php
+++ database/migrations/2018_10_04_181116_life_event_vehicle.php
@@ -14,6 +14,15 @@ class LifeEventVehicle extends Migration
     {
         DB::table('default_life_event_types')
             ->where('translation_key', 'new_vehicule')
-            ->update(['translation_key' => 'new_vehicle']);
+            ->update([
+                'translation_key' => 'new_vehicle'
+            ]);
+
+        DB::table('life_event_types')
+            ->where('default_life_event_type_key', 'new_vehicule')
+            ->update([
+                'default_life_event_type_key' => 'new_vehicle',
+                'name' => trans('settings.personalization_life_event_type_new_vehicle', [], 'en')
+            ]);
     }
 }
