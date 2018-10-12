03ed4339bd76d93f07d14458885c33231ceef5d1
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2018-10-12 04:03:23 +0200

    New translations dashboard.php (French)

--- resources/lang/fr/dashboard.php
+++ resources/lang/fr/dashboard.php
@@ -13,6 +13,8 @@ return [
     'tab_calls_blank' => 'Vous n’avez encore enregistré aucun appel.',
     'tab_debts' => 'Dettes',
     'tab_debts_blank' => 'Vous n’avez encore enregistré aucune dette.',
+    'tab_tasks' => 'Tasks',
+    'tab_tasks_blank' => 'You haven’t any task yet.',
 
     'debts_you_owe' => 'Vous devez',
 
