4a1252d1ce7cb9af3cfb094816b50601b8f2aefc
Régis Freyd <djaiss@users.noreply.github.com>
2018-10-11 21:59:15 -0400

    feat: display all pending tasks on the dashboard (#1909)

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,6 +10,7 @@ use App\Models\Contact\Contact;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Resources\Debt\Debt as DebtResource;
+use App\Http\Resources\Task\Task as TaskResource;
 
 class DashboardController extends Controller
 {
@@ -140,6 +141,23 @@ class DashboardController extends Controller
         }
 
         return $debtsCollection;
+    }
+
+    /**
+     * Get tasks for the dashboard.
+     *
+     * @return Collection
+     */
+    public function tasks()
+    {
+        $tasksCollection = collect([]);
+        $tasks = auth()->user()->account->tasks()->where('completed', 0)->get();
+
+        foreach ($tasks as $task) {
+            $tasksCollection->push(new TaskResource($task));
+        }
+
+        return $tasksCollection;
     }
 
     /**
