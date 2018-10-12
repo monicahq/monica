3cef372738a755407b4a0cd4edc906b84507ffbb
Regis Freyd <djaiss@users.noreply.github.com>
2018-10-11 22:41:27 -0400

    feat: add description for a contact

--- resources/lang/en/people.php
+++ resources/lang/en/people.php
@@ -112,12 +112,6 @@ return [
     'age_exact_in_years' => ':age years old',
     'age_exact_birthdate' => 'born :date',
 
-    // Last called
-    'last_called' => 'Last called: :date',
-    'last_called_empty' => 'Last called: unknown',
-    'last_activity_date' => 'Last activity together: :date',
-    'last_activity_date_empty' => 'Last activity together: unknown',
-
     // additional information
     'information_edit_success' => 'The profile has been updated successfully',
     'information_edit_title' => 'Edit :name’s personal information',
@@ -125,6 +119,8 @@ return [
     'information_edit_max_size' => 'Max :size Mb.',
     'information_edit_firstname' => 'First name',
     'information_edit_lastname' => 'Last name (Optional)',
+    'information_edit_description' => 'Description (Optional)',
+    'information_edit_description_help' => 'Used on the contact list to add some context, if necessary.',
     'information_edit_linkedin' => 'LinkedIn profile (optional)',
     'information_edit_unknown' => 'I do not know this person’s age',
     'information_edit_probably' => 'This person is probably...',
