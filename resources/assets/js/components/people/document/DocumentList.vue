b327645fc74de30855ed049cbaf8b62f16f4ccd6
Regis Freyd <djaiss@users.noreply.github.com>
2018-10-17 22:18:48 -0400

    wip

--- resources/assets/js/components/people/document/DocumentList.vue
+++ resources/assets/js/components/people/document/DocumentList.vue
@@ -47,6 +47,10 @@
         color: #CB4066;
     }
 
+    .document-action-menu-item.delete:hover {
+        color: #fff;
+    }
+
     progress {
         -webkit-appearance: none;
         border: none;
@@ -185,29 +189,14 @@
                             <circle cx="18.5" cy="2.5" r="2.5" fill="#505473" fill-opacity="0.86"/>
                         </svg>
                     </div>
-                    <ul class="absolute bg-white z-max document-action-menu" v-if="modalToDisplay == document.id">
+                    <ul class="absolute bg-white z-max pv1 document-action-menu" v-if="modalToDisplay == document.id">
                         <li class="tc">
-                            <a class="pv2 ph3 inline-flex items-center no-underline">
-                                <span class="dib">
-                                    <svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <g clip-path="url(#clip0)">
-                                            <path d="M8.45 5.20005V1.30005H4.55V5.20005H1.3L6.5 10.4L11.7 5.20005H8.45ZM0 11.7H13V13H0V11.7Z" fill="#5A6F84"/>
-                                        </g>
-                                        <defs>
-                                            <clipPath id="clip0">
-                                                <rect width="13" height="13" fill="white"/>
-                                            </clipPath>
-                                        </defs>
-                                    </svg>
-                                </span>
+                            <a class="pv2 pointer ph3 inline-flex items-center w-100 no-underline document-action-menu-item" href="">
                                 Download
                             </a>
                         </li>
                         <li class="tc">
-                            <a class="pv2 ph3 document-action-menu-item delete">
-                                <svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M3.8998 1.3L5.1998 0H7.7998L9.0998 1.3H11.6998V2.6H1.2998V1.3H3.8998ZM1.9498 3.9H11.0498L10.3998 13H2.5998L1.9498 3.9ZM5.1998 5.2V11.7H5.8498V5.2H5.1998ZM7.1498 5.2V11.7H7.7998V5.2H7.1498Z" fill="#CB4066"/>
-                                </svg>
+                            <a class="pv2 pointer ph3 inline-flex items-center no-underline w-100 document-action-menu-item delete" @click="deleteDocument(document.id)">
                                 Delete
                             </a>
                         </li>
@@ -304,6 +293,14 @@
                     this.file = null
                     this.displayUploadError = true
                 });
+            },
+
+            downloadDocument(id) {
+
+            },
+
+            deleteDocument(id) {
+
             },
         }
     }
