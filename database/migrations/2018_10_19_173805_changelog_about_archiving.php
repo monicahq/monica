6991d6718a10613b9f98895985844fdf8a55f36f
Alexis Saettler <alexis@saettler.org>
2018-10-19 19:46:40 +0200

    Add changelog notification

---
+++ database/migrations/2018_10_19_173805_changelog_about_archiving.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangelogAboutArchiving extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $description = '
+**New feature: archiving contact**
+
+You can now archive a contact to no longer see it in the Dashboard, or in the searchs. Archived contacts can still be found on the Contacts board.
+
+![image](/img/changelogs/2018-10-19-archiving.png)';
+
+        $id = DB::table('changelogs')->insertGetId([
+            'description' => $description,
+            'created_at' => '2018-10-19',
+        ]);
+
+        $instance = Instance::first();
+        $instance->addUnreadChangelogEntry($id);
+    }
+}
