bbb01934fab7c95888e16f8861deec2c81cf30ac
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2018-10-29 10:02:33 +0100

    New translations auth.php (Hebrew)

--- resources/lang/he/auth.php
+++ resources/lang/he/auth.php
@@ -66,10 +66,10 @@ return [
     'register_policy' => 'הרשמה מאמתת שקראת והסכמת ל<a href=":url" hreflang=":hreflang">מדיניות הפרטיות</a> ול<a href=":urlterm" hreflang=":hreflang">תנאי השימוש</a> שלנו.',
     'register_invitation_email' => 'מטעמי אבטחה, נא לציין את כתובת הדוא״ל של מי שהזמין אותך להצטרף לחשבון הזה. המידע הזה מופיע בהודעת ההזמנה.',
 
-    'confirmation_title' => 'Verify Your Email Address',
-    'confirmation_fresh' => 'A fresh verification link has been sent to your email address.',
-    'confirmation_check' => 'Before proceeding, please check your email for a verification link.',
-    'confirmation_request_another' => 'If you did not receive the email <a href=":url">click here to request another</a>.',
+    'confirmation_title' => 'אימות כתובת הדוא״ל שלך',
+    'confirmation_fresh' => 'נשלח קישור אימות טרי לכתובת הדוא״ל שלך.',
+    'confirmation_check' => 'בטרם המשך התהליך, נא לחפש את קישור האימות בתיבת הדוא״ל שלך.',
+    'confirmation_request_another' => 'אם לא קיבלת את ההודעה <a href=":url">נא ללחוץ כאן כדי לבקש אחת נוספת</a>.',
 
     'confirmation_again' => 'כדי לשנות את כתובת הדוא״ל שלך נא <a href=":url" class="alert-link">ללחוץ כאן</a>.',
     'email_change_current_email' => 'כתובת הדוא״ל הנוכחית:',
