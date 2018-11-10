c52ea693391c0c709858e87d3903bb172c6af142
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2018-11-10 02:52:24 +0100

    New translations people.php (Italian)

--- resources/lang/it/people.php
+++ resources/lang/it/people.php
@@ -117,6 +117,12 @@ return [
     'age_exact_in_years' => ':age anni',
     'age_exact_birthdate' => 'nato :date',
 
+    // Last called
+    'last_called' => 'Last called: :date',
+    'last_called_empty' => 'Last called: unknown',
+    'last_activity_date' => 'Last activity together: :date',
+    'last_activity_date_empty' => 'Last activity together: unknown',
+
     // additional information
     'information_edit_success' => 'Il profilo è stato aggiornato',
     'information_edit_title' => 'Modifica le informazioni personali di :name',
@@ -309,7 +315,9 @@ return [
 
     // tags
     'tag_edit' => 'Modifica etichetta',
-    'tag_add' => 'aggiungi un’etichetta',
+    'tag_add' => 'Add tags',
+    'tag_add_search' => 'Add or search tags',
+    'tag_no_tags' => 'No tags yet',
 
     // Introductions
     'introductions_sidebar_title' => 'Come vi siete conosciuti',
