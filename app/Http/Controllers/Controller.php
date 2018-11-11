dacef7c6479f383cc89081e2bbf0ee12d90fc482
Alexis Saettler <alexis@saettler.org>
2018-11-11 13:23:34 +0100

    update

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,33 +11,4 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    /**
-     * Handle the ModelNotFoundException.
-     *
-     * @param ModelNotFoundException $e
-     * @return \Illuminate\Http\Response
-     */
-    protected function handleModelNotFound(ModelNotFoundException $e)
-    {
-        return $this->respondUnauthorized($e->getMessage());
-    }
-
-    /**
-     * Respond with an Unauthorized message.
-     *
-     * @param string $errorMessage
-     * @return \Illuminate\Http\Response
-     */
-    protected function respondUnauthorized($errorMessage = null)
-    {
-        $data = [];
-        if ($errorMessage) {
-            $data = ['errors' => $errorMessage];
-        }
-
-        return response()->json([
-            'message' => trans('app.error_unauthorized'),
-        ] + $data, 400);
-    }
 }
