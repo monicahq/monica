071676b64be394188d99160de4a4a1f79d8efe20
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2018-11-11 16:53:09 +0100

    New translations dashboard.php (Arabic)

--- resources/lang/ar/dashboard.php
+++ resources/lang/ar/dashboard.php
@@ -26,4 +26,7 @@ return [
     'reminders_none' => 'لا يوجد تذكير لهذا الشهر',
 
     'linkedin_profile' => 'LinkedIn',
+
+    'product_changes' => 'Product changes',
+    'product_view_details' => 'View details',
 ];
