c1f1d0140492a3385a69fbda122540f861301485
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2018-11-21 23:31:49 +0100

    New translations dashboard.php (Chinese Simplified)

--- resources/lang/zh/dashboard.php
+++ resources/lang/zh/dashboard.php
@@ -16,11 +16,11 @@ return [
     'tab_tasks' => '任务',
     'tab_tasks_blank' => '你还没有任何任务',
 
-    'tasks_add_task_placeholder' => 'What is this task about?',
-    'tasks_tab_your_contacts' => 'Tasks related to your contacts',
-    'tasks_tab_your_tasks' => 'Your tasks',
-    'tasks_add_note' => 'Press <kbd>Enter</kbd> to add the task.',
-    'task_add_cta' => 'Add a task',
+    'tasks_add_task_placeholder' => '这个任务是关于什么的？',
+    'tasks_tab_your_contacts' => '与任务相关的联系人',
+    'tasks_tab_your_tasks' => '您的任务',
+    'tasks_add_note' => '按<kbd>回车</kbd>来添加任务',
+    'task_add_cta' => '添加任务',
 
     'debts_you_owe' => '待还金额',
 
