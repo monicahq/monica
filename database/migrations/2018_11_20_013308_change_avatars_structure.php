cf37b2dcb4530b04f13430308c3f1e817971c5b1
Regis Freyd <djaiss@users.noreply.github.com>
2018-11-23 06:53:46 -0500

    fix

--- database/migrations/2018_11_20_013308_change_avatars_structure.php
+++ database/migrations/2018_11_20_013308_change_avatars_structure.php
@@ -16,9 +16,9 @@ class ChangeAvatarsStructure extends Migration
         Schema::table('contacts', function (Blueprint $table) {
             $table->string('avatar_source')->after('food_preferencies')->default('adorable');
             $table->string('avatar_gravatar_url', 250)->after('avatar_source')->nullable();
-            $table->string('avatar_adorable_uuid')->after('avatar_adorable_url')->nullable();
-            $table->string('avatar_adorable_url', 250)->after('avatar_gravatar_url')->nullable();
-            $table->integer('avatar_photo_id')->after('avatar_adorable_uuid')->nullable();
+            $table->string('avatar_adorable_uuid')->after('avatar_gravatar_url')->nullable();
+            $table->string('avatar_adorable_url', 250)->after('avatar_adorable_uuid')->nullable();
+            $table->integer('avatar_photo_id')->after('avatar_adorable_url')->nullable();
         });
     }
 }
