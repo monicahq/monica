614336357d2a83d6676f87319ef9a55892a036fb
Adrien Poupa <adrien@poupa.fr>
2018-11-26 23:16:33 -0500

    camelCase

--- database/seeds/FakeContentTableSeeder.php
+++ database/seeds/FakeContentTableSeeder.php
@@ -38,7 +38,7 @@ class FakeContentTableSeeder extends Seeder
         // Get or create the first account
         if (User::where('email', 'admin@admin.com')->exists()) {
             $userId = User::where('email', 'admin@admin.com')->value('id');
-            $this->account = Account::where('id', $user_id)->first();
+            $this->account = Account::where('id', $userId)->first();
         } else {
             $this->account = Account::createDefault('John', 'Doe', 'admin@admin.com', 'admin');
 
