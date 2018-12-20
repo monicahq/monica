42e5824dd29c1e42f6e60c3dd8dbc05261c266b2
Regis Freyd <djaiss@users.noreply.github.com>
2018-12-19 21:36:54 -0500

    rewrite api for addresses

--- app/Models/Contact/Contact.php
+++ app/Models/Contact/Contact.php
@@ -26,7 +26,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Contracts\Filesystem\FileNotFoundException;
-use App\Http\Resources\Address\AddressShort as AddressShortResource;
+use App\Http\Resources\Address\Address as AddressResource;
 use App\Http\Resources\Contact\ContactShort as ContactShortResource;
 use App\Http\Resources\ContactField\ContactField as ContactFieldResource;
 
@@ -1141,7 +1141,7 @@ class Contact extends Model
      */
     public function getAddressesForAPI()
     {
-        return AddressShortResource::collection($this->addresses);
+        return AddressResource::collection($this->addresses);
     }
 
     /**
