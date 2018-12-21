362146e4dc6bbb4b29bc27929959f35fd2f951e4
Alexis Saettler <alexis@saettler.org>
2018-12-21 17:49:06 +0100

    Update changelog & doc

--- config/auth.php
+++ config/auth.php
@@ -109,9 +109,13 @@ return [
     | Recovery codes generation
     |--------------------------------------------------------------------------
     |
-    | count: number of recovery codes to generate
-    | blocks: number of blocks in one code
-    | chars: number of characters per block
+    | Recovery codes can be used to access your account in the event you lost
+    | access to the device used to generate or receive two-factor
+    | authentication codes. 
+    |
+    | * count: number of recovery codes to generate
+    | * blocks: number of blocks in one code
+    | * chars: number of characters per block
     |
     */
 
