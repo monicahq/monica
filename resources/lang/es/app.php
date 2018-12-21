3072f1887e9f2823cf475cafe359aa09a6bab779
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2018-12-21 22:42:15 +0100

    New translations app.php (Spanish)

--- resources/lang/es/app.php
+++ resources/lang/es/app.php
@@ -12,6 +12,7 @@ return [
     'download' => 'Download',
     'save_close' => 'Save and close',
     'close' => 'Cerrar',
+    'copy' => 'Copy',
     'create' => 'Crear',
     'remove' => 'Remover',
     'revoke' => 'Revocar',
