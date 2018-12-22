7e55756040fc82704d9ab41bbc905963b4432660
Regis Freyd <djaiss@users.noreply.github.com>
2018-12-22 16:25:32 -0500

    wip

--- database/migrations/2018_12_22_200413_add_reminder_initial_date_to_reminders.php
+++ database/migrations/2018_12_22_200413_add_reminder_initial_date_to_reminders.php
@@ -16,6 +16,8 @@ class AddReminderInitialDateToReminders extends Migration
     {
         Schema::table('reminders', function (Blueprint $table) {
             $table->date('reminder_intial_date')->after('description');
+            $table->boolean('enable_notification_seven_days_prior')->default(true)->after('frequency_number');
+            $table->boolean('enable_notification_thirty_days_prior')->default(true)->after('frequency_number');
         });
 
         // we need to migrate old data. Since we don't know what was the initial
@@ -26,6 +28,22 @@ class AddReminderInitialDateToReminders extends Migration
                 $reminder->reminder_initial_date = $reminder->last_triggered;
                 $reminder->save();
             }
+        });
+
+        Schema::create('reminder_outbox', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('account_id');
+            $table->unsignedInteger('reminder_id');
+            $table->datetime('trigger_date');
+            $table->string('city')->nullable();
+            $table->string('province')->nullable();
+            $table->string('postal_code')->nullable();
+            $table->char('country', 3)->nullable();
+            $table->double('latitude')->nullable();
+            $table->double('longitude')->nullable();
+            $table->timestamps();
+            $table->foreign('account_id')->references('id')->on('accounts')->onDelete('cascade');
+            $table->foreign('reminder_id')->references('id')->on('reminders')->onDelete('cascade');
         });
     }
 }
