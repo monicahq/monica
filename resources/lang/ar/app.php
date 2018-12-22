acc43dfd656ba7312561390e031b37a184a1e556
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2018-12-22 22:31:30 +0100

    New translations app.php (Arabic)

--- resources/lang/ar/app.php
+++ resources/lang/ar/app.php
@@ -261,6 +261,7 @@ return [
     'emotion_secondary_cheerfulness' => 'Cheerfulness',
     'emotion_secondary_zest' => 'Zest',
     'emotion_secondary_contentment' => 'Contentment',
+    'emotion_secondary_pride' => 'Pride',
     'emotion_secondary_optimism' => 'Optimism',
     'emotion_secondary_enthrallment' => 'Enthrallment',
     'emotion_secondary_relief' => 'Relief',
