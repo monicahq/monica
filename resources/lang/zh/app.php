99ec9214eee7c3e000aad65bb93ace8ef89b1e17
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2018-12-22 10:55:40 +0100

    New translations app.php (Chinese Simplified)

--- resources/lang/zh/app.php
+++ resources/lang/zh/app.php
@@ -12,7 +12,7 @@ return [
     'download' => '下载',
     'save_close' => '保存并关闭',
     'close' => '关闭',
-    'copy' => 'Copy',
+    'copy' => '复制',
     'create' => '创建',
     'remove' => '删除',
     'revoke' => '撤销',
