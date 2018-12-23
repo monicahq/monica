d8dbda838bfe6fc3881664a39bacac7a6c56a38b
Alexis Saettler <alexis@saettler.org>
2018-12-23 20:19:55 +0100

    update

--- tests/Api/Auth/AuthControllerTest.php
+++ tests/Api/Auth/AuthControllerTest.php
@@ -42,19 +42,22 @@ class AuthControllerTest extends ApiTestCase
     public function test_oauth_login()
     {
         $client = (new ClientRepository())->createPasswordGrantClient(
-            null, config('app.name'), 'http://localhost'
+            null, config('app.name'), config('app.url')
         );
-        $x = (new ClientRepository())->find($client->id);
-        config(['monica.mobile_client_id' => $client->id]);
-        config(['monica.mobile_client_secret' => $client->secret]);
 
+        config([
+            'monica.mobile_client_id' => $client->id,
+            'monica.mobile_client_secret' => $client->secret
+        ]);
+
+        $userPassword = 'password';
         $user = factory(User::class)->create([
-            'password' => bcrypt('password'),
+            'password' => bcrypt($userPassword),
         ]);
 
         $response = $this->json('POST', '/oauth/login', [
             'email' => $user->email,
-            'password' => 'password',
+            'password' => $userPassword,
         ]);
 
         $response->assertStatus(200);
