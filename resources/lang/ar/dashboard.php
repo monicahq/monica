c42637dc0c1d5cac87cfeed7a32eddde0c3d7502
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2018-12-25 03:32:19 +0100

    New translations dashboard.php (Arabic)

--- resources/lang/ar/dashboard.php
+++ resources/lang/ar/dashboard.php
@@ -31,8 +31,6 @@ return [
     'reminders_next_months' => 'الأحداث التي ستقام في الأشهر الثلاث المقبلة',
     'reminders_none' => 'لا يوجد تذكير لهذا الشهر',
 
-    'linkedin_profile' => 'LinkedIn',
-
     'product_changes' => 'Product changes',
     'product_view_details' => 'View details',
 ];
