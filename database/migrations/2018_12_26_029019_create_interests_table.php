1f906351ce67f3a1c5c67808f760a2dd6a968d6b
Gareth Edwards <garrethedwards@gmail.com>
2018-12-27 15:02:17 +0200

    --

--- database/migrations/2018_12_26_029019_create_interests_table.php
+++ database/migrations/2018_12_26_029019_create_interests_table.php
@@ -1,7 +1,7 @@
 <?php
 
-use DB;
 use App\Models\Account\Account;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
