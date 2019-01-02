8e0436bdfd3d7d20c978d8883510a7bcc88532b4
Regis Freyd <djaiss@users.noreply.github.com>
2019-01-01 20:39:54 -0500

    fixes

--- app/Models/Contact/Activity.php
+++ app/Models/Contact/Activity.php
@@ -102,17 +102,6 @@ class Activity extends Model implements IsJournalableInterface
     }
 
     /**
-     * Get the date_it_happened field.
-     *
-     * @param string $value
-     * @return string
-     */
-    public function getDateItHappenedAttribute($value)
-    {
-        return $value;
-    }
-
-    /**
      * Get the summary for this activity.
      *
      * @return string or null
