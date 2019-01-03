ccd1b3e0d20c37ee434cc62ae49c06c5deeacbb5
Régis Freyd <djaiss@users.noreply.github.com>
2019-01-03 11:18:34 -0500

    chore: refactor api methods to manage contacts (#2251)

--- app/Services/Contact/Contact/CreateContact.php
+++ app/Services/Contact/Contact/CreateContact.php
@@ -26,6 +26,19 @@ class CreateContact extends BaseService
             'gender_id' => 'required|integer|exists:genders,id',
             'description' => 'nullable|string|max:255',
             'is_partial' => 'nullable|boolean',
+            'is_birthdate_known' => 'required|boolean',
+            'birthdate_day' => 'nullable|integer',
+            'birthdate_month' => 'nullable|integer',
+            'birthdate_year' => 'nullable|integer',
+            'birthdate_is_age_based' => 'nullable|boolean',
+            'birthdate_age' => 'nullable|integer',
+            'birthdate_add_reminder' => 'nullable|boolean',
+            'is_deceased' => 'required|boolean',
+            'is_deceased_date_known' => 'required|boolean',
+            'deceased_date_day' => 'nullable|integer',
+            'deceased_date_month' => 'nullable|integer',
+            'deceased_date_year' => 'nullable|integer',
+            'deceased_date_add_reminder' => 'nullable|boolean',
         ];
     }
 
@@ -39,7 +52,31 @@ class CreateContact extends BaseService
     {
         $this->validate($data);
 
-        $this->contact = Contact::create($data);
+        // filter out the data that shall not be updated here
+        $dataOnly = array_except(
+            $data,
+            [
+                'is_birthdate_known',
+                'birthdate_day',
+                'birthdate_month',
+                'birthdate_year',
+                'birthdate_is_age_based',
+                'birthdate_age',
+                'birthdate_add_reminder',
+                'is_deceased',
+                'is_deceased_date_known',
+                'deceased_date_day',
+                'deceased_date_month',
+                'deceased_date_year',
+                'deceased_date_add_reminder',
+            ]
+        );
+
+        $this->contact = Contact::create($dataOnly);
+
+        $this->updateBirthDayInformation($data);
+
+        $this->updateDeceasedInformation($data);
 
         $this->generateUUID();
 
@@ -47,7 +84,8 @@ class CreateContact extends BaseService
 
         $this->contact->save();
 
-        return $this->contact;
+        // we query the DB again to fill the object with all the new properties
+        return Contact::find($this->contact->id);
     }
 
     /**
@@ -59,5 +97,46 @@ class CreateContact extends BaseService
     {
         $this->contact->uuid = RandomHelper::uuid();
         $this->contact->save();
+    }
+
+    /**
+     * Update the information about the birthday.
+     *
+     * @param array $data
+     * @return void
+     */
+    private function updateBirthDayInformation(array $data)
+    {
+        (new UpdateBirthdayInformation)->execute([
+            'account_id' => $data['account_id'],
+            'contact_id' => $this->contact->id,
+            'is_date_known' => $data['is_birthdate_known'],
+            'day' => $this->nullOrvalue($data, 'birthdate_day'),
+            'month' => $this->nullOrvalue($data, 'birthdate_month'),
+            'year' => $this->nullOrvalue($data, 'birthdate_year'),
+            'is_age_based' => $this->nullOrvalue($data, 'birthdate_is_age_based'),
+            'age' => $this->nullOrvalue($data, 'birthdate_age'),
+            'add_reminder' => $this->nullOrvalue($data, 'birthdate_add_reminder'),
+        ]);
+    }
+
+    /**
+     * Update the information about the date of death.
+     *
+     * @param array $data
+     * @return void
+     */
+    private function updateDeceasedInformation(array $data)
+    {
+        (new UpdateDeceasedInformation)->execute([
+            'account_id' => $data['account_id'],
+            'contact_id' => $this->contact->id,
+            'is_deceased' => $data['is_deceased'],
+            'is_date_known' => $data['is_deceased_date_known'],
+            'day' => $this->nullOrValue($data, 'deceased_date_day'),
+            'month' => $this->nullOrValue($data, 'deceased_date_month'),
+            'year' => $this->nullOrValue($data, 'deceased_date_year'),
+            'add_reminder' => $this->nullOrValue($data, 'deceased_date_add_reminder'),
+        ]);
     }
 }
