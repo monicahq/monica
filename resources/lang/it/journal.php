6c53742e9a3bce00dd760e6bc1b1a64b38bad8af
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2019-01-08 01:23:07 +0100

    New translations journal.php (Italian)

--- resources/lang/it/journal.php
+++ resources/lang/it/journal.php
@@ -9,6 +9,7 @@ return [
     'journal_entry_type_journal' => 'Journal entry',
     'journal_entry_type_activity' => 'Activity',
     'journal_entry_rate' => 'You rated your day.',
+    'journal_add_comment' => 'Care to add a comment (optional)?',
     'entry_delete_success' => 'La pagina del diario è stata rimossa.',
     'journal_add_title' => 'Titolo (facoltativo)',
     'journal_add_date' => 'Date',
