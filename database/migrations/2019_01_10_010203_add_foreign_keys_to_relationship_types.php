59eac6053c7cd2e89bc719e090e4a229077e45ac
djaiss <rfrgpg@sent.as>
2019-01-10 14:08:01 +0000

    Apply fixes from StyleCI

--- database/migrations/2019_01_10_010203_add_foreign_keys_to_relationship_types.php
+++ database/migrations/2019_01_10_010203_add_foreign_keys_to_relationship_types.php
@@ -3,8 +3,8 @@
 use App\Models\Account\Account;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Migrations\Migration;
 use App\Models\Relationship\RelationshipType;
+use Illuminate\Database\Migrations\Migration;
 use App\Models\Relationship\RelationshipTypeGroup;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
