e06fa9aa0d8c118271a9635cc412ffb09f92d754
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2019-01-12 16:22:39 +0100

    New translations settings.php (Chinese Simplified)

--- resources/lang/zh/settings.php
+++ resources/lang/zh/settings.php
@@ -106,7 +106,7 @@ return [
     'u2f_error_configuration_unsupported' => '客户端配置不支持',
     'u2f_error_device_ineligible' => '当前设备不支持此请求。',
     'u2f_error_timeout' => '请求超时',
-    'u2f_last_use' => 'Last use:',
+    'u2f_last_use' => 'Last use: {timestamp}',
     'u2f_delete_confirmation' => 'Are you sure you want to delete this key?',
     'u2f_delete_success' => 'Key deleted',
 
