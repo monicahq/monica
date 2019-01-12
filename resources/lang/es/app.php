cc831fc85d732c57ce0616785ed3b74410367075
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2019-01-12 12:13:30 +0100

    New translations app.php (Spanish)

--- resources/lang/es/app.php
+++ resources/lang/es/app.php
@@ -34,6 +34,9 @@ return [
     'zoom' => 'Zoom',
     'upgrade' => 'Upgrade to unlock',
     'percent_uploaded' => '{percent}% uploaded',
+    'next' => 'Next ❯',
+    'previous' => '❮ Previous',
+    'retry' => 'Retry',
 
     'application_title' => 'Monica – gestor de relaciones personales',
     'application_description' => 'Monica es una herramienta para gestionar las interacciones con sus seres queridos, amigos y familiares.',
