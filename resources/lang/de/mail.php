1072d982d0f3b048093ebd680d37c580d288c07d
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2019-01-21 10:41:33 +0100

    New translations mail.php (German)

--- resources/lang/de/mail.php
+++ resources/lang/de/mail.php
@@ -14,13 +14,14 @@ return [
     'notification_description' => 'In :count Tagen (am :date), findet folgendes Ereignis statt:',
 
     'stay_in_touch_subject_line' => 'Mit :name in Kontakt bleiben',
-    'stay_in_touch_subject_description' => 'You asked to be reminded to stay in touch with :name every :frequency day.|You asked to be reminded to stay in touch with :name every :frequency days.',
+    'stay_in_touch_subject_description' => 'Sie wollten jeden :frequency Tag erinnert werden, um in Kontakt zu bleiben mit :name .| 
+Sie wollten alle :frequency Tage erinnert werden um in Kontakt zu bleiben mit :name .',
 
     'notifications_whoops' => 'Hoppla!',
     'notifications_hello' => 'Hallo!',
     'notifications_regards' => 'Grüße',
     'notifications_footer' => 'Wenn Sie Probleme beim Klicken auf die Schaltfläche ":actionText" haben, kopieren Sie einfach die folgende URL in die Adresszeile Ihres Webbrowsers: [:actionURL](:actionURL)',
-    'notifications_rights' => 'All rights reserved',
+    'notifications_rights' => 'Alle Rechte vorbehalten',
 
     'confirmation_email_title' => 'Monica – E-Mail-Verifikation',
     'confirmation_email_intro'=> 'Um Ihre E-Mail-Adresse zu validieren, klicken Sie bitte auf den untenstehenden Button',
