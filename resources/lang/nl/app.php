a8b3740d78fe82e5cbe722f18e514688ff8c50ea
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2019-01-25 15:14:23 +0100

    New translations app.php (Dutch)

--- resources/lang/nl/app.php
+++ resources/lang/nl/app.php
@@ -86,6 +86,7 @@ return [
     'breadcrumb_add_note' => 'Notitie toevoegen',
     'breadcrumb_edit_note' => 'Notitie bewerken',
     'breadcrumb_api' => 'API',
+    'breadcrumb_api' => 'DAV Resources',
     'breadcrumb_edit_introductions' => 'Hoe hebben jullie elkaar ontmoet',
     'breadcrumb_settings_personalization' => 'Personalisatie',
     'breadcrumb_settings_security' => 'Beveiliging',
