d82210c102ca7d70b2eb32b1ecae701c89072a16
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2019-01-27 13:45:24 +0100

    New translations app.php (Spanish)

--- resources/lang/es/app.php
+++ resources/lang/es/app.php
@@ -433,4 +433,12 @@ return [
     'weather_current_temperature_celsius' => ':temperatura ºC',
     'weather_current_temperature_fahrenheit' => ':temperatura ºF',
     'weather_current_title' => 'Clima actual',
+
+    // dav
+    'dav_contacts' => 'Contacts',
+    'dav_contacts_description' => ':name’s contacts',
+    'dav_birthdays' => 'Birthdays',
+    'dav_birthdays_description' => ':name’s contact’s birthdays',
+    'dav_tasks' => 'Tasks',
+    'dav_tasks_description' => ':name’s tasks',
 ];
