225a28d90d730d6ce295696088fbdaad71912937
Alexis Saettler <alexis@saettler.org>
2019-01-29 14:14:21 +0100

    add config file

---
+++ config/gunther.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    /*
+     * Available languages of the application.
+     * List of all languages, separated by comas.
+     */
+    'languages' => env('LANGUAGES', 'ar,cs,de,es,fr,he,hr,it,nl,pt,pt-BR,ru,tr,zh'),
+
+    /*
+     * Source language of the application.
+     */
+    'source_language' => env('SOURCE_LANGUAGE', 'en'),
+
+    /*
+     * Crowdin api key.
+     * See https://crowdin.com/project/{project_name}/settings#api
+     */
+    'apikey' => env('CROWDIN_APIKEY'),
+
+    /*
+     * Crowdin project.
+     * See https://crowdin.com/project/{project_name}/settings#api
+     */
+    'project' => env('CROWIND_PROJECT'),
+
+    /*
+     * Resulting file format.
+     * @see https://support.crowdin.com/files-management/#file-export-settings
+     */
+    'resulting_file' => '/master/resources/lang/%two_letters_code%/%original_file_name%',
+
+];
