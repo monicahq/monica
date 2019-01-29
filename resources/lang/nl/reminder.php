765ed92081cc19aecb9b853d524eb2498019404e
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2019-01-29 12:23:33 +0100

    New translations reminder.php (Dutch)

--- resources/lang/nl/reminder.php
+++ resources/lang/nl/reminder.php
@@ -4,8 +4,8 @@ return [
     'type_birthday' => 'Wens een gelukkige verjaardag aan',
     'type_phone_call' => 'Bel',
     'type_lunch' => 'Luchen met',
-    'type_hangout' => 'Hangout with',
-    'type_email' => 'Email',
+    'type_hangout' => 'Ontmoeten met',
+    'type_email' => 'E-mail',
     'type_birthday_kid' => 'Feliciteer het kind van',
 
 ];
