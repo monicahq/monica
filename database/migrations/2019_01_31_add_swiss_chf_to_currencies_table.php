64b6ce8cd1093cef84718b19d05579504de30d89
Alexis Saettler <alexis@saettler.org>
2019-02-07 18:42:17 +0100

    Apply StyleCI

--- database/migrations/2019_01_31_add_swiss_chf_to_currencies_table.php
+++ database/migrations/2019_01_31_add_swiss_chf_to_currencies_table.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 class AddSwissCHFToCurrenciesTable extends Migration
@@ -13,7 +11,7 @@ class AddSwissCHFToCurrenciesTable extends Migration
      */
     public function up()
     {
-        DB::table('currencies')->insert([ 'iso' => 'CHF', 'name' => 'Swiss CHF', 'symbol' => 'CHF']);
+        DB::table('currencies')->insert(['iso' => 'CHF', 'name' => 'Swiss CHF', 'symbol' => 'CHF']);
     }
 }
 
