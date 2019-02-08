30b722a11c1f65b5ba8f96f28378e5ad9961f24a
bu4ak <igorbu4@gmail.com>
2019-02-08 10:29:32 +0700

    Refactor form requests

--- app/Http/Requests/Settings/GendersRequest.php
+++ app/Http/Requests/Settings/GendersRequest.php
@@ -2,20 +2,10 @@
 
 namespace App\Http\Requests\Settings;
 
-use Illuminate\Foundation\Http\FormRequest;
+use App\Http\Requests\AuthorizedRequest;
 
-class GendersRequest extends FormRequest
+class GendersRequest extends AuthorizedRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
     /**
      * Get the validation rules that apply to the request.
      *
