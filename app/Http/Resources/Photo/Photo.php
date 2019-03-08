8390cbbb1dacc340720d1dc4c0c617bfd65745d6
Alexis Saettler <alexis@saettler.org>
2019-03-08 09:04:00 +0100

    fixes

--- app/Http/Resources/Photo/Photo.php
+++ app/Http/Resources/Photo/Photo.php
@@ -27,7 +27,7 @@ class Photo extends Resource
             'account' => [
                 'id' => $this->account->id,
             ],
-            'contact' => new ContactShortResource($this->contact()),
+            'contact' => new ContactShortResource($this->contact),
             'created_at' => DateHelper::getTimestamp($this->created_at),
             'updated_at' => DateHelper::getTimestamp($this->updated_at),
         ];
