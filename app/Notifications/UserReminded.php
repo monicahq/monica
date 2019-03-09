f43402a412b76613de4c69f1544c657c62175fee
Regis Freyd <djaiss@users.noreply.github.com>
2019-03-08 23:21:54 -0500

    fix: fix reminders not being sent

--- app/Notifications/UserReminded.php
+++ app/Notifications/UserReminded.php
@@ -8,6 +8,7 @@ use App\Models\Contact\Contact;
 use Illuminate\Support\Facades\App;
 use App\Interfaces\MailNotification;
 use App\Models\Contact\ReminderOutbox;
+use App\Models\Contact\Reminder;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -20,18 +21,18 @@ class UserReminded extends LaravelNotification implements ShouldQueue, MailNotif
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     /**
-     * @var ReminderOutbox
+     * @var Reminder
      */
-    public $reminderOutbox;
+    public $reminder;
 
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct(ReminderOutbox $reminderOutbox)
+    public function __construct(Reminder $reminder)
     {
-        $this->reminderOutbox = $reminderOutbox;
+        $this->reminder = $reminder;
     }
 
     /**
@@ -55,18 +56,18 @@ class UserReminded extends LaravelNotification implements ShouldQueue, MailNotif
         App::setLocale($user->locale);
 
         $contact = Contact::where('account_id', $user->account_id)
-            ->findOrFail($this->reminderOutbox->reminder->contact_id);
+            ->findOrFail($this->reminder->contact_id);
 
         $message = (new MailMessage)
             ->subject(trans('mail.subject_line', ['contact' => $contact->name]))
             ->greeting(trans('mail.greetings', ['username' => $user->first_name]))
-            ->line(trans('mail.want_reminded_of', ['reason' => $this->reminderOutbox->reminder->title]))
+            ->line(trans('mail.want_reminded_of', ['reason' => $this->reminder->title]))
             ->line(trans('mail.for', ['name' => $contact->name]))
             ->action(trans('mail.footer_contact_info2', ['name' => $contact->name]), $contact->getLink());
 
-        if (! is_null($this->reminderOutbox->reminder->description)) {
+        if (! is_null($this->reminder->description)) {
             $message = $message
-                ->line(trans('mail.comment', ['comment' => $this->reminderOutbox->reminder->description]));
+                ->line(trans('mail.comment', ['comment' => $this->reminder->description]));
         }
 
         return $message;
