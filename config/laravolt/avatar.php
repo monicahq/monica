f6af4976ea1d8ec8a0bbc2fe676aeacd32a87702
Alexis Saettler <alexis@saettler.org>
2019-03-09 14:08:19 +0100

    Fix config

--- config/laravolt/avatar.php
+++ config/laravolt/avatar.php
@@ -44,7 +44,7 @@ return [
 
     // Fonts used to render text.
     // If contains more than one fonts, randomly selected based on name supplied
-    'fonts'    => [__DIR__.'/../fonts/OpenSans-Bold.ttf', __DIR__.'/../fonts/rockwell.ttf'],
+    'fonts'    => [__DIR__.'/../../vendor/laravolt/avatar/fonts/OpenSans-Bold.ttf'],
 
     // List of foreground colors to be used, randomly selected based on name supplied
     'foregrounds'   => [
