41c67a97c9f9505b3075fa6fbf6a8b55cc0ce47d
Alexis Saettler <alexis@saettler.org>
2019-03-19 17:36:37 +0100

    test: add a test in MoneyHelper with locale formatting (#2542)

--- tests/Unit/Helpers/MoneyHelperTest.php
+++ tests/Unit/Helpers/MoneyHelperTest.php
@@ -6,6 +6,7 @@ use Tests\TestCase;
 use App\Models\User\User;
 use App\Helpers\MoneyHelper;
 use App\Models\Settings\Currency;
+use Illuminate\Support\Facades\App;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class MoneyHelperTest extends TestCase
@@ -19,6 +20,17 @@ class MoneyHelperTest extends TestCase
 
         $this->assertEquals('€500.00', MoneyHelper::format(500, $currency));
         $this->assertEquals('€5,038.29', MoneyHelper::format(5038.29, $currency));
+    }
+
+    public function testFormatReturnsAmountWithLocale()
+    {
+        App::setLocale('fr');
+
+        $currency = new Currency();
+        $currency->iso = 'EUR';
+
+        $this->assertEquals('500,00 €', MoneyHelper::format(500, $currency));
+        $this->assertEquals('5 038,29 €', MoneyHelper::format(5038.29, $currency));
     }
 
     public function testFormatReturnsAmountWithCurrencySymbolOfZeroMinorUnitCurrency()
