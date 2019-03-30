15d2a0820929a82da02633c1bf5e4ff1d887ba0a
Alexis Saettler <alexis@saettler.org>
2019-03-30 23:53:32 +0100

    Fix tests

--- tests/Browser/Auth/AuthControllerTest.php
+++ tests/Browser/Auth/AuthControllerTest.php
@@ -8,7 +8,7 @@ use Laravel\Passport\ClientRepository;
 
 class AuthControllerTest extends ApiTestCase
 {
-    public function setUp()
+    public function setUp(): void
     {
         parent::setUp();
 
