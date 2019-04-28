d5f08955fbbff0517c6151b30922a30ce0c8eac2
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2019-04-28 14:54:33 +0200

    New translations people.php (Turkish)

--- resources/lang/tr/people.php
+++ resources/lang/tr/people.php
@@ -3,6 +3,7 @@
 return [
 
     //index
+    'people_not_found' => 'Contact not found',
     'people_list_number_kids' => '1 çocuk|:count çocuklar',
     'people_list_last_updated' => 'Son görüşme:',
     'people_list_number_reminders' => '1 hatırlatıcı|:count hatırlatıcı',
