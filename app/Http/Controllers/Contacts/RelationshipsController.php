4f641046a979b60ac4b62f9bf4d37a24f16dccd2
Alexis Saettler <alexis@saettler.org>
2019-04-29 21:32:24 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Contacts/RelationshipsController.php
+++ app/Http/Controllers/Contacts/RelationshipsController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Contacts;
 
 use App\Helpers\DateHelper;
 use Illuminate\Http\Request;
-use App\Helpers\SearchHelper;
 use App\Helpers\GendersHelper;
 use App\Models\Contact\Contact;
 use Illuminate\Support\Collection;
