d29d55050ed593c63b8e2b7d7a2c483953f0b687
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2019-05-16 19:43:28 +0200

    New translations app.php (Turkish)

--- resources/lang/tr/app.php
+++ resources/lang/tr/app.php
@@ -86,7 +86,7 @@ return [
     'breadcrumb_add_note' => 'Add a note',
     'breadcrumb_edit_note' => 'Edit a note',
     'breadcrumb_api' => 'API',
-    'breadcrumb_api' => 'DAV Resources',
+    'breadcrumb_dav' => 'DAV Resources',
     'breadcrumb_edit_introductions' => 'How did you meet',
     'breadcrumb_settings_personalization' => 'Personalization',
     'breadcrumb_settings_security' => 'Security',
