50052021c5079d6b6fa82456d64911bb2a39b101
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2019-05-18 16:52:19 +0200

    New translations app.php (Chinese Simplified)

--- resources/lang/zh/app.php
+++ resources/lang/zh/app.php
@@ -86,7 +86,7 @@ return [
     'breadcrumb_add_note' => '添加注释',
     'breadcrumb_edit_note' => '编辑注释',
     'breadcrumb_api' => 'API',
-    'breadcrumb_dav' => 'DAV Resources',
+    'breadcrumb_dav' => 'DAV 资源',
     'breadcrumb_edit_introductions' => '你是怎么知道的',
     'breadcrumb_settings_personalization' => '个性化',
     'breadcrumb_settings_security' => '安全',
