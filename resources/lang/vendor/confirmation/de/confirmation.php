733115e655c4c31158e92b0a77fb385dfc55347b
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2019-05-19 22:02:48 +0200

    New translations confirmation.php (German)

--- resources/lang/vendor/confirmation/de/confirmation.php
+++ resources/lang/vendor/confirmation/de/confirmation.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'message' => 'Thanks for signing up! Please check your emails to confirm your email address.',
-    'success' => 'You have successfully verified your account! You can now login.',
+    'message' => 'Vielen Dank für deine Anmeldung! Bitte überprüfe deine E-Mail-Postfach, um deine E-Mail-Adresse zu bestätigen.',
+    'success' => 'Du hast dein Konto erfolgreich verifiziert. Du kannst dich ab sofort anmelden.',
     'again' => 'You must verify your email before you can access the site.
                 <br/>If you have not received the confirmation email check your spam folder.
                 <br/>To get a new confirmation email please <a href=":url" class="alert-link">click here</a>.',
-    'resend' => 'A confirmation message has been sent. Please check your mailbox.'
+    'resend' => 'Wir haben dir einen Link zur Verifikation gesendet. Bitte überprüfe dein E-Mail-Postfach.'
 ];
