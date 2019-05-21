7a00b0fddd5dbd1f3c89aa30d8b29039e5d3db69
MonicaBot <40141970+MonicaBot@users.noreply.github.com>
2019-05-21 17:33:31 +0200

    New translations confirmation.php (Portuguese, Brazilian)

--- resources/lang/vendor/confirmation/pt-BR/confirmation.php
+++ resources/lang/vendor/confirmation/pt-BR/confirmation.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'message' => 'Thanks for signing up! Please check your emails to confirm your email address.',
-    'success' => 'You have successfully verified your account! You can now login.',
+    'message' => 'Obrigado por se cadastrar! Por favor, verifique seus e-mails para confirmar seu endereço de e-mail.',
+    'success' => 'Você verificou com sucesso sua conta! Agora você pode entrar.',
     'again' => 'You must verify your email before you can access the site.
                 <br/>If you have not received the confirmation email check your spam folder.
                 <br/>To get a new confirmation email please <a href=":url" class="alert-link">click here</a>.',
-    'resend' => 'A confirmation message has been sent. Please check your mailbox.'
+    'resend' => 'Um e-mail de confirmação foi enviado. Por favor, verifique seu e-mail.'
 ];
