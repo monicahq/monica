f53c0da6b323f314d427ccff829b4ae11ad61115
Alexis Saettler <alexis@saettler.org>
2019-05-27 22:46:44 +0200

    reduce size

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -40,7 +40,6 @@ Vue.use(vSelectMenu);
 
 // Tables
 import VueGoodTablePlugin from 'vue-good-table';
-import 'vue-good-table/dist/vue-good-table.css';
 Vue.use(VueGoodTablePlugin);
 
 import VueClipboard from 'vue-clipboard2';
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,41 +1,38 @@
 let mix = require('laravel-mix');
 require('laravel-mix-purgecss');
 
-let purgeCssOptions = {
-    whitelistPatterns: [/^vdp-datepicker/, /^StripeElement/],
-    whitelistPatternsChildren: [/^vdp-datepicker/]
-};
-
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
 mix.webpackConfig({
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/,
+            options: {
+                plugins: ['lodash'],
+                presets: [['@babel/preset-env', { 'modules': false, 'targets': { 'node': 6 } }]]
+          }
+        }],
+    },
     plugins: [
+        new LodashModuleReplacementPlugin,
         new MomentLocalesPlugin({
-            localesToKeep: [
-                'en',
-                'ar',
-                'cs',
-                'de',
-                'es',
-                'fr',
-                'he',
-                'hr',
-                'it',
-                'nl',
-                'pt',
-                'pt-BR',
-                'ru',
-                'tr',
-                'zh-cn',
-            ],
+            localesToKeep: ['en', 'ar', 'cs', 'de', 'es', 'fr', 'he', 'hr', 'it', 'nl', 'pt', 'pt-BR', 'ru', 'tr', 'zh-cn'],
         }),
     ],
 });
+
+let purgeCssOptions = {
+    whitelistPatterns: [/^vdp-datepicker/, /^StripeElement/, /^vgt/],
+    whitelistPatternsChildren: [/^vdp-datepicker/, /^vgt/]
+};
 
 mix.js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app-ltr.scss', 'public/css')
     .sass('resources/assets/sass/app-rtl.scss', 'public/css')
     .purgeCss(purgeCssOptions)
-    .extract(['vue'])
+    .extract(['vue', 'jquery', 'moment', 'moment-timezone'])
     .setResourceRoot('../')
     .sourceMaps(false)
     .version();
