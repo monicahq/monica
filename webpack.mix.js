3baacd421b04b9770a4671b1ebbdf5baf277371a
Alexis Saettler <alexis@saettler.org>
2019-05-30 07:48:54 +0200

    try again

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,22 +6,30 @@ const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
 mix.webpackConfig({
     module: {
         rules: [{
-            loader: 'babel-loader',
             test: /\.js$/,
             exclude: /node_modules/,
-            options: {
-                plugins: ['lodash'],
-                presets: [['@babel/preset-env', { 'modules': false, 'targets': { 'node': 6 } }]]
-          }
-        }],
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    plugins: ['lodash'],
+                    presets: [['@babel/preset-env', { 'targets': { 'node': 6 } }]]
+                }
+            }
+        }]
     },
     plugins: [
-        new LodashModuleReplacementPlugin,
+        new LodashModuleReplacementPlugin(/*{
+            'collections': true
+        }*/),
         new MomentLocalesPlugin({
             localesToKeep: ['en', 'ar', 'cs', 'de', 'es', 'fr', 'he', 'hr', 'it', 'nl', 'pt', 'pt-BR', 'ru', 'tr', 'zh-cn'],
         }),
     ],
 });
+mix.babelConfig({
+    plugins: ['lodash'],
+    presets: [['@babel/preset-env', { 'targets': { 'node': 6 } }]]
+})
 
 let purgeCssOptions = {
     whitelistPatterns: [/^vdp-datepicker/, /^StripeElement/, /^vgt/],
@@ -31,18 +39,20 @@ let purgeCssOptions = {
 mix.js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app-ltr.scss', 'public/css')
     .sass('resources/assets/sass/app-rtl.scss', 'public/css')
-    .purgeCss(purgeCssOptions)
-    .extract(['vue', 'jquery', 'moment', 'moment-timezone'])
-    .setResourceRoot('../')
-    .sourceMaps(false)
-    .version();
 
-mix.js('resources/assets/js/stripe.js', 'public/js')
+    // stripe
+    .js('resources/assets/js/stripe.js', 'public/js')
     .sass('resources/assets/sass/stripe.scss', 'public/css')
+
+    // u2f
+    .scripts(['resources/assets/js/vendor/u2f/u2f-api.js'], 'public/js/u2f-api.js')
+
+    // global commands
     .purgeCss(purgeCssOptions)
+    .extract(/*['vue', 'jquery', 'moment', 'moment-timezone']*/)
     .setResourceRoot('../')
     .sourceMaps(false)
     .version();
 
-mix.scripts(['resources/assets/js/vendor/u2f/u2f-api.js'], 'public/js/u2f-api.js')
-    .setResourceRoot('../');
+mix.babel(['public/js/app.js'], 'public/js/app.js');
+mix.babel(['public/js/vendor.js'], 'public/js/vendor.js');
