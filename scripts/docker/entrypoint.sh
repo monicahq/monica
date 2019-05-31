a8d4a160fcd2844866274449085bf89ef8dee533
Alexis Saettler <alexis@saettler.org>
2019-05-31 13:00:18 +0200

    Optimize dockerfile

--- scripts/docker/entrypoint.sh
+++ scripts/docker/entrypoint.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 
 waitfordb() {
-    echo "Connecting to ${DB_HOST}:${DB_PORT}"
+    HOST=${DB_HOST:-mysql}
+    PORT=${DB_PORT:-3306}
+    echo "Connecting to ${HOST}:${PORT}"
 
     attempts=0
     max_attempts=30
     while [ $attempts -lt $max_attempts ]; do
-        nc -z "${DB_HOST}" "${DB_PORT}" && break
-        echo "Waiting for ${DB_HOST}:${DB_PORT} ..."
+        nc -z "${HOST}" "${PORT}" && break
+        echo "Waiting for ${HOST}:${PORT} ..."
         sleep 1
         let "attempts=attempts+1"
     done
 
     if [ $attempts -eq $max_attempts ]; then
-        echo "Unable to contact your database at ${DB_HOST}:${DB_PORT}"
+        echo "Unable to contact your database at ${HOST}:${PORT}"
         exit 1
     fi
 
