33fab0d576481c6561232287eb4ca1d65de6928d
Alexis Saettler <alexis@saettler.org>
2019-06-16 13:59:57 +0200

    chore(ci): fix ci run for forked repos (#2777)

---
+++ scripts/ci/fixsecrets.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "$SONAR_TOKEN" == "\$(SONAR_TOKEN)" ]; then
+    echo -e "\033[0;36mFix SONAR_TOKEN\033[0;37m"
+    export SONAR_TOKEN=
+fi
+if [ "$GITHUB_TOKEN" == "\$(GITHUB_TOKEN)" ]; then
+    echo -e "\033[0;36mFix GITHUB_TOKEN\033[0;37m"
+    export GITHUB_TOKEN=
+fi
+if [ "$GH_TOKEN" == "\$(GH_TOKEN)" ]; then
+    echo -e "\033[0;36mFix GH_TOKEN\033[0;37m"
+    export GH_TOKEN=
+fi
+if [ "$ASSETS_GITHUB_TOKEN" == "\$(ASSETS_GITHUB_TOKEN)" ]; then
+    echo -e "\033[0;36mFix ASSETS_GITHUB_TOKEN\033[0;37m"
+    export ASSETS_GITHUB_TOKEN=
+fi
