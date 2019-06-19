cc010e5e1daf3bcaeb1b6980609670db35c952f7
Alexis Saettler <alexis@saettler.org>
2019-06-19 14:54:57 +0200

    update

---
+++ scripts/docker/ubuntu/install-yarn.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -v
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+
+apt-get update
+apt-get -y install yarn
