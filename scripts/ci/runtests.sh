960e7622d9ccc41a4051caa92225a855614cd0c4
Alexis Saettler <alexis@saettler.org>
2019-06-19 23:47:53 +0200

    update

--- scripts/ci/runtests.sh
+++ scripts/ci/runtests.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+SYSTEM_JOBPOSITIONINPHASE=$1
+TESTSUITE=$2
+
 set -exvo pipefail
 
-mkdir -p results/coverage
 sed 's/DB_TEST_PASSWORD=/DB_TEST_PASSWORD=root/' scripts/tests/.env.mysql > .env
-touch .sentry-release
 
 phpdbg -dmemory_limit=4G -qrr vendor/bin/phpunit -c phpunit.xml --log-junit ./results/junit/unit/results${SYSTEM_JOBPOSITIONINPHASE}.xml --coverage-clover ./results/coverage${SYSTEM_JOBPOSITIONINPHASE}.xml --testsuite $TESTSUITE
