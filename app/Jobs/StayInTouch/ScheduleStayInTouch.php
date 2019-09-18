20835f4557a9f2cf594499e4e1019b8274eb0868
Alexis Saettler <alexis@saettler.org>
2019-09-18 08:53:18 +0000

    Apply fixes from StyleCI

--- app/Jobs/StayInTouch/ScheduleStayInTouch.php
+++ app/Jobs/StayInTouch/ScheduleStayInTouch.php
@@ -47,6 +47,7 @@ class ScheduleStayInTouch implements ShouldQueue
         if (count($users) > 0) {
             NotificationFacade::send($users, new StayInTouchEmail($this->contact));
             $this->contact->setStayInTouchTriggerDate($this->contact->stay_in_touch_frequency);
+
             return;
         }
 
