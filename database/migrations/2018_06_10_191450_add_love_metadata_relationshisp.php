cd50e158479fec72f33857cf52f9947756a7d5ee
George Gerges <george.gerges@sap.com>
2019-11-30 15:19:32 -0500

    fix db migration

--- database/migrations/2018_06_10_191450_add_love_metadata_relationshisp.php
+++ database/migrations/2018_06_10_191450_add_love_metadata_relationshisp.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddchurchMetadataRelationshisp extends Migration
+class AddLoveMetadataRelationshisp extends Migration
 {
     /**
      * Run the migrations.
