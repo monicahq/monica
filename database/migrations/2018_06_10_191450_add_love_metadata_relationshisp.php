1b55dd49b24e4c968329e84d01c5eece2bcd8255
George Gerges <george.gerges@sap.com>
2019-11-30 15:40:40 -0500

    .

--- database/migrations/2018_06_10_191450_add_love_metadata_relationshisp.php
+++ database/migrations/2018_06_10_191450_add_love_metadata_relationshisp.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddLoveMetadataRelationshisp extends Migration
+class AddChurchMetadataRelationshisp extends Migration
 {
     /**
      * Run the migrations.
