9f39219bd5b7560a9f81bbc4152a33b96c8f0c90
Alexis Saettler <alexis@saettler.org>
2019-12-02 12:58:58 +0100

    chore: clean envoy file

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,33 +1,18 @@
-@servers(['local' => 'localhost', 'web' => '{{ $site }}@deploy.{{ $region }}.frbit.com'])
+// See Envoy doc: https://laravel.com/docs/5.8/envoy
+// 
+// Configure with: `envoy run git-config --host=my-host --site=site-conf`
+// Deploy with: `envoy run deploy`
 
-@setup
-    $commit = exec('git log --pretty="%H" -n1 HEAD');
-    //$version = exec('php artisan monica:getversion');
-    $release = exec('git log --pretty="%h" -n1 HEAD');
-@endsetup
+@servers(['local' => 'localhost', 'web' => '{{ $site }}@{{ $host }}'])
 
-@story('deploy')
-    sentry-set
-    git
-    sentry
-@endstory
-
-@task('git-config', ['on' => 'local'])
-    git remote add fortrabbit {{ $site }}@deploy.{{ $region }}.frbit.com:{{ $site }}.git
-@endtask
-
-@task('git', ['on' => 'local'])
+@task('deploy', ['on' => 'local'])
     git fetch origin
     git checkout {{ $branch ?? 'master' }}
     git pull
-    git push -u fortrabbit {{ $branch ?? 'master' }}
+    git push -u deploy {{ $branch ?? 'master' }}
 @endtask
 
-@task('sentry-set', ['on' => 'web'])
-    echo {{ $release }} > .sentry-release
-@endtask
-
-@task('sentry', ['on' => 'web'])
-    php artisan sentry:release --force -vvv --release={{ $release }} --environment=production --commit={{ $commit }}
-    php artisan config:cache
+@task('git-config', ['on' => 'local'])
+    git remote remove deploy || true
+    git remote add deploy {{ $site.'@'.$host.':'.$site.'.git' }}
 @endtask
