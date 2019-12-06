26a0c2304ae0bda2d90c78be64869ef1f317c73f
Alexis Saettler <alexis@saettler.org>
2019-12-06 09:12:12 +0100

    update

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -3,13 +3,45 @@
 // Configure with: `envoy run git-config --host=my-host --site=site-conf`
 // Deploy with: `envoy run deploy`
 
-@servers(['local' => 'localhost', 'web' => '{{ $site }}@{{ $host }}'])
+@servers(['local' => 'localhost', 'web' => $web ])
 
-@task('deploy', ['on' => 'local'])
+@setup
+    $commit = exec('git log --pretty="%H" -n1 HEAD');
+    //$version = exec('php artisan monica:getversion');
+    $release = exec('git log --pretty="%h" -n1 HEAD');
+@endsetup
+
+@story('deploy')
+    pull
+    sentry-file
+    sentry-release
+    push
+@endstory
+
+@task('push', ['on' => 'local'])
+    git push -u deploy {{ $branch ?? 'master' }} --force
+@endtask
+
+@task('pull', ['on' => 'local'])
     git fetch origin
     git checkout {{ $branch ?? 'master' }}
     git pull
-    git push -u deploy {{ $branch ?? 'master' }}
+@endtask
+
+@task('sentry-file', ['on' => 'local'])
+    echo {{ $release }} > .sentry-release
+    git add .sentry-release
+    git commit -m 'add sentry file'
+@endtask
+
+@task('sentry-release', ['on' => 'local'])
+    php artisan sentry:release --force -vvv --release={{ $release }} --environment={{ $environment ?? 'production' }} --commit={{ $commit }}
+@endtask
+
+@task('sentry-local', ['on' => 'local'])
+    test -x ~/.local/bin/sentry-cli || curl -sL https://sentry.io/get-cli/ | INSTALL_DIR=~/.local/bin bash
+    source .env
+    php artisan sentry:release --force -vvv --release={{ $release }} --environment={{ $environment ?? 'production' }} --commit={{ $commit }}
 @endtask
 
 @task('git-config', ['on' => 'local'])
