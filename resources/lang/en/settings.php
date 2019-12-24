26fc761caf8871e6c44bbc558aa9bb3ec141de14
Alexis Saettler <alexis@saettler.org>
2019-12-24 16:37:58 +0100

    Language fix

--- resources/lang/en/settings.php
+++ resources/lang/en/settings.php
@@ -309,7 +309,7 @@ return [
 
     'api_oauth_clients' => 'Your OAuth clients',
     'api_oauth_clients_desc' => 'This section lets you register your own OAuth clients.',
-    'api_oauth_clients_desc2' => 'Use this client id to request a new token, and convert authorization codes to access tokens. See <a href="{url}">Laravel Passport documentation</a> for more explanations.',
+    'api_oauth_clients_desc2' => 'Use this client id to request a new token, and convert authorization codes to access tokens. See <a href="{url}">Laravel Passport documentation</a> for more information.',
     'api_oauth_title' => 'OAuth Clients',
     'api_oauth_create_new' => 'Create New Client',
     'api_oauth_edit' => 'Edit Client',
@@ -323,7 +323,7 @@ return [
     'api_oauth_redirecturl_help' => 'Your application’s authorization callback URL.',
 
     'api_authorized_clients' => 'List of authorized clients',
-    'api_authorized_clients_desc' => 'This section lists all the clients you’ve authorized to access your application datas. You can revoke this authorization at anytime.',
+    'api_authorized_clients_desc' => 'This section lists all the clients you’ve authorized to access your application data. You can revoke this authorization at anytime.',
     'api_authorized_clients_title' => 'Authorized Applications',
     'api_authorized_clients_none' => 'There is no authorized client yet.',
     'api_authorized_clients_name' => 'Name',
