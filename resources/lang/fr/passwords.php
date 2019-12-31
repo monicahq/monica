a2ac3ccb160965306e3ad6033bd7126a85b7562a
Regis Freyd <djaiss@users.noreply.github.com>
2019-12-31 12:58:59 -0500

    chore: update i18n (#3411)

--- resources/lang/fr/passwords.php
+++ resources/lang/fr/passwords.php
@@ -13,12 +13,12 @@ return [
     |
     */
 
-    'password' => 'Les mots de passe doivent contenir au moins six caractères et doivent être identiques.',
     'reset' => 'Votre mot de passe a été réinitialisé !',
     'sent' => 'Nous vous avons envoyé par courriel le lien de réinitialisation du mot de passe.',
     'token' => 'Ce jeton de réinitialisation du mot de passe n’est pas valide.',
     'user' => 'Si l’email que vous avez entré correspond chez nous, un email de réinitialisation du mot de passe vous a été envoyé.',
     'changed' => 'Mot de passe changé avec succès.',
     'invalid' => 'Le mot de passe que vous avez saisi est incorrect.',
+    'throttled' => 'Merci de patienter avant de réessayer.',
 
 ];
