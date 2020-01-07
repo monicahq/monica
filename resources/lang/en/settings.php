177da4ad7384940099d688d27eaec9ff16503294
Jamie Schouten <j.d.g.a.schouten@outlook.com>
2020-01-07 23:18:52 +0100

    add name_order_nickname_firstname_lastname to localization

--- resources/lang/en/settings.php
+++ resources/lang/en/settings.php
@@ -29,6 +29,7 @@ return [
     'name_order_firstname_nickname_lastname' => '<First name> (<Nickname>) <Last name> - John (Rambo) Doe',
     'name_order_lastname_firstname_nickname' => '<Last name> <First name> (<Nickname>) - Doe John (Rambo)',
     'name_order_lastname_nickname_firstname' => '<Last name> (<Nickname>) <First name> - Doe (Rambo) John',
+    'name_order_nickname_firstname_lastname' => '<Nickname> (<First name> <Last name>) - Rambo (John Doe)',
     'name_order_nickname' => '<Nickname> - Rambo',
     'currency' => 'Currency',
     'name' => 'Your name: :name',
