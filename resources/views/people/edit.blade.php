6f90bcf3d6c8c956b90c64fc9aac8afe99198f64
Alexis Saettler <alexis@saettler.org>
2020-01-18 12:24:30 +0100

    remove unused attribute

--- resources/views/people/edit.blade.php
+++ resources/views/people/edit.blade.php
@@ -129,7 +129,6 @@
           :birthdate="'{{ $birthdate }}'"
           :reminder="{{ \Safe\json_encode($hasBirthdayReminder) }}"
           :value="'{{ $birthdayState }}'"
-          :name="'Birthdate'"
         ></form-specialdate>
 
         {{-- Is the contact deceased? --}}
