2fba7eb24ed9da6453858dcf5e313a89cac5b591
Regis Freyd <djaiss@users.noreply.github.com>
2020-01-31 22:47:26 -0500

    chore: remove old method to delete all information about a contact

--- app/Models/Contact/Contact.php
+++ app/Models/Contact/Contact.php
@@ -484,8 +484,8 @@ class Contact extends Model
      * Scope a query to only include contacts who are not only a kid or a
      * significant other without being a contact.
      *
-     * @param  \Illuminate\Database\Eloquent\Builder $query
-     * @return \Illuminate\Database\Eloquent\Builder
+     * @param Builder $query
+     * @return Builder
      */
     public function scopeReal($query)
     {
@@ -495,8 +495,8 @@ class Contact extends Model
     /**
      * Scope a query to only include contacts who are active.
      *
-     * @param  \Illuminate\Database\Eloquent\Builder $query
-     * @return \Illuminate\Database\Eloquent\Builder
+     * @param Builder $query
+     * @return Builder
      */
     public function scopeActive($query)
     {
@@ -506,8 +506,8 @@ class Contact extends Model
     /**
      * Scope a query to only include contacts who are alive.
      *
-     * @param  \Illuminate\Database\Eloquent\Builder $query
-     * @return \Illuminate\Database\Eloquent\Builder
+     * @param Builder $query
+     * @return Builder
      */
     public function scopeAlive($query)
     {
@@ -517,8 +517,8 @@ class Contact extends Model
     /**
      * Scope a query to only include contacts who are dead.
      *
-     * @param  \Illuminate\Database\Eloquent\Builder $query
-     * @return \Illuminate\Database\Eloquent\Builder
+     * @param Builder $query
+     * @return Builder
      */
     public function scopeDead($query)
     {
@@ -528,8 +528,8 @@ class Contact extends Model
     /**
      * Scope a query to only include contacts who are not active.
      *
-     * @param  \Illuminate\Database\Eloquent\Builder $query
-     * @return \Illuminate\Database\Eloquent\Builder
+     * @param Builder $query
+     * @return Builder
      */
     public function scopeNotActive($query)
     {
@@ -1206,34 +1206,6 @@ class Contact extends Model
     }
 
     /**
-     * Delete all related objects.
-     *
-     * @return bool
-     */
-    public function deleteEverything()
-    {
-        // I know: this is a really brutal way of deleting objects. I'm doing
-        // this because I'll add more objects related to contacts in the future
-        // and I don't want to have to think of deleting a row that matches a
-        // contact.
-        //
-        $tables = DBHelper::getTables();
-        foreach ($tables as $table) {
-            $tableName = $table->table_name;
-
-            try {
-                DB::table($tableName)->where('contact_id', $this->id)->delete();
-            } catch (QueryException $e) {
-                continue;
-            }
-        }
-
-        $this->delete();
-
-        return true;
-    }
-
-    /**
      * Get all the reminders regarding the birthdays of the contacts who have a
      * relationships with the current contact.
      *
@@ -1299,9 +1271,9 @@ class Contact extends Model
     /**
      * Get the contacts that have all the provided $tags
      * or if $tags is NONE get contacts that have no tags.
-     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param Builder $query
      * @param mixed $tags string or Tag
-     * @return \Illuminate\Database\Eloquent\Builder $query
+     * @return Builder $query
      */
     public function scopeTags($query, $tags)
     {
