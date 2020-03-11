3e4f991fa32b968ce3430601e16be0b6c87326d1
Alexis Saettler <alexis@saettler.org>
2020-03-11 23:33:45 +0100

    chore: add webpack-shell-plugin

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,19 +1,27 @@
 let mix = require('laravel-mix');
 require('laravel-mix-purgecss');
-
+const WebpackShellPlugin = require('webpack-shell-plugin');
 const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
-mix.webpackConfig({
-    plugins: [
-        new MomentLocalesPlugin({
-            localesToKeep: ['en', 'ar', 'cs', 'de', 'en-GB', 'es', 'fr', 'he', 'hr', 'it', 'nl', 'pt', 'pt-BR', 'ru', 'tr', 'zh-cn'],
-        }),
-    ],
-});
+
+var plugins = [];
+
+plugins.push(new MomentLocalesPlugin({
+  localesToKeep: ['en', 'ar', 'cs', 'de', 'en-GB', 'es', 'fr', 'he', 'hr', 'it', 'nl', 'pt', 'pt-BR', 'ru', 'tr', 'zh-cn'],
+}));
+
+plugins.push(new WebpackShellPlugin({
+  onBuildStart:['php artisan lang:generate --quiet'],
+  onBuildEnd:[]
+}));
 
 let purgeCssOptions = {
     whitelistPatterns: [/^fa-/, /^vdp-datepicker/, /^StripeElement/, /^vgt/, /^vue-tooltip/, /^pretty/],
     whitelistPatternsChildren: [/^vdp-datepicker/, /^vgt/, /^vue-tooltip/, /^pretty/]
 };
+
+mix.webpackConfig({
+  plugins: plugins,
+});
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app-ltr.scss', 'public/css')
