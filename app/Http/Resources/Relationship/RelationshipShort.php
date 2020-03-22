bb515c7ae0588e50d0f05118004c7300cfa08c26
Alexis Saettler <alexis@saettler.org>
2020-03-22 15:39:46 +0100

    fix: fix contact resources (#3583)

---
+++ app/Http/Resources/Relationship/RelationshipShort.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\Relationship;
+
+use Illuminate\Http\Resources\Json\Resource;
+use App\Http\Resources\Contact\ContactShort as ContactShortResource;
+
+class RelationshipShort extends Resource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'relationship' => [
+                'id' => $this->id,
+                'name' => $this->relationshipType->name,
+            ],
+            'contact' => new ContactShortResource($this->ofContact),
+        ];
+    }
+}
