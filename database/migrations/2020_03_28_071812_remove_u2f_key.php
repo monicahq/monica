c60396b9ee3ec09a7afc5eafeb32669852dde2a7
Alexis Saettler <alexis@saettler.org>
2020-03-28 07:26:16 +0000

    Apply fixes from StyleCI

--- database/migrations/2020_03_28_071812_remove_u2f_key.php
+++ database/migrations/2020_03_28_071812_remove_u2f_key.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Migrations\Migration;
 
 class RemoveU2fKey extends Migration
 {
