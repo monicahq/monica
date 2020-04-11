97dabe3e84dfc1a15fe82c6e64662f35d4eb5b36
Alexis Saettler <alexis@saettler.org>
2020-04-11 17:52:48 +0200

    add test

--- tests/Commands/PassportCommandTest.php
+++ tests/Commands/PassportCommandTest.php
@@ -12,7 +12,7 @@ class PassportCommandTest extends TestCase
     use DatabaseTransactions;
 
     /** @test */
-    public function passport_command()
+    public function passport_command_create()
     {
         $app = $this->createApplication();
         $app->make('config')->set(['passport.private_key' => '', 'passport.public_key' => '']);
@@ -29,6 +29,23 @@ class PassportCommandTest extends TestCase
 
         $this->assertCount(1, $commandExecutor->buffer);
         $this->assertCommandContains($commandExecutor->buffer[0], '✓ Creating personal access client', 'php artisan passport:client');
+    }
+
+    /** @test */
+    public function passport_command_alreday_created()
+    {
+        $app = $this->createApplication();
+        $app->make('config')->set(['passport.private_key' => '', 'passport.public_key' => '']);
+        PersonalAccessClient::create();
+
+        $commandExecutor = new CommandExecutorTester();
+        $command = new Passport();
+        $command->commandExecutor = $commandExecutor;
+        $command->setLaravel($app);
+
+        $command->run(new \Symfony\Component\Console\Input\ArrayInput([]), new \Symfony\Component\Console\Output\NullOutput());
+
+        $this->assertCount(0, $commandExecutor->buffer);
     }
 
     private function assertCommandContains($array, $message, $command)
