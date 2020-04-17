monicahq/monica

commit c5ecf3421456a0bb832ccb7f1b85ba08762595d7

chore: fix search scopes (#3852)

app/Http/Controllers/ContactsController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Helpers\DBHelper;
 use Illuminate\View\View;
 use App\Helpers\DateHelper;
 use App\Helpers\FormHelper;
@@ -81,9 +80,9 @@ class ContactsController extends Controller
 
         $contacts = $user->account->contacts()->real();
         if ($active) {
-            $nbArchived = $contacts->count();
-            $contacts = $contacts->active();
-            $nbArchived = $nbArchived - $contacts->count();
+            $archived = (clone $contacts)->notActive();
+            $contacts = (clone $contacts)->active();
+            $nbArchived = $archived->count();
         } else {
             $contacts = $contacts->notActive();
             $nbArchived = $contacts->count();
@@ -99,8 +98,10 @@ class ContactsController extends Controller
             $tags = collect();
 
             while ($request->input('tag'.$count)) {
-                $tag = Tag::where('account_id', auth()->user()->account_id)
-                            ->where('name_slug', $request->input('tag'.$count));
+                $tag = Tag::where([
+                    'account_id' => auth()->user()->account_id,
+                    'name_slug' => $request->input('tag'.$count),
+                ]);
                 if ($tag->count() > 0) {
                     $tag = $tag->get();
 
@@ -550,9 +551,10 @@ class ContactsController extends Controller
             return;
         }
 
-        $results = SearchHelper::searchContacts($needle, 20, DBHelper::getTable('contacts').'.`created_at`');
+        $results = SearchHelper::searchContacts($needle, 'created_at')
+            ->paginate(20);
 
-        if (count($results) !== 0) {
+        if ($results->total() > 0) {
             return ContactResource::collection($results);
         } else {
             return ['noResults' => trans('people.people_search_no_results')];
@@ -694,9 +696,10 @@ class ContactsController extends Controller
             $tags = collect();
 
             while ($request->input('tag'.$count)) {
-                $tag = Tag::where('account_id', $accountId)
-                    ->where('name_slug', $request->input('tag'.$count))
-                    ->get();
+                $tag = Tag::where([
+                    'account_id' => $accountId,
+                    'name_slug' => $request->input('tag'.$count),
+                ])->get();
 
                 if (! ($tags->contains($tag[0]))) {
                     $tags = $tags->concat($tag);
@@ -715,7 +718,8 @@ class ContactsController extends Controller
         $perPage = $request->has('perPage') ? $request->input('perPage') : config('monica.number_of_contacts_pagination');
 
         // search contacts
-        $contacts = $contacts->search($request->input('search') ? $request->input('search') : '', $accountId, $perPage, DBHelper::getTable('contacts').'.`is_starred` desc', null, $sort);
+        $contacts = $contacts->search($request->has('search') ? $request->input('search') : '', $accountId, 'is_starred', 'desc', $sort)
+            ->paginate($perPage);
 
         return [
             'totalRecords' => $contacts->total(),

app/Traits/Searchable.php
@@ -3,9 +3,7 @@
 namespace App\Traits;
 
 use App\Helpers\DBHelper;
-use App\Helpers\StringHelper;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
 trait Searchable
 {
@@ -15,13 +13,12 @@ trait Searchable
      * @param  Builder $builder query builder
      * @param  string $needle
      * @param  int  $accountId
-     * @param  int $limitPerPage
+     * @param  string $orderByColumn
+     * @param  string $orderByDirection
      * @param  string $sortOrder
-     * @param  string $whereCondition
-     *
-     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator|null
+     * @return Builder|null
      */
-    public function scopeSearch(Builder $builder, $needle, $accountId, $limitPerPage, $orderBy, $whereCondition = null, $sortOrder = null): ?LengthAwarePaginator
+    public function scopeSearch(Builder $builder, string $needle, int $accountId, string $orderByColumn, string $orderByDirection = 'asc', string $sortOrder = null): ?Builder
     {
         if ($this->searchable_columns == null) {
             return null;
@@ -31,10 +28,10 @@ trait Searchable
             return DBHelper::getTable($this->getTable()).".`$column`";
         }, $this->searchable_columns);
 
-        $queryString = StringHelper::buildQuery($searchableColumns, $needle);
+        $queryString = $this->buildQuery($searchableColumns, $needle);
 
-        $builder->whereRaw(DBHelper::getTable($this->getTable()).".`account_id` = $accountId AND ($queryString) $whereCondition");
-        $builder->orderByRaw($orderBy);
+        $builder->whereRaw('('.$queryString.')')
+            ->orderBy($orderByColumn, $orderByDirection);
 
         if ($sortOrder) {
             $builder->sortedBy($sortOrder);
@@ -44,6 +41,35 @@ trait Searchable
             return "{$this->getTable()}.$column";
         }, $this->return_from_search));
 
-        return $builder->paginate($limitPerPage);
+        return $builder;
+    }
+
+    /**
+     * Build a query based on the array that contains column names.
+     *
+     * @param  array  $array
+     * @param  string $searchTerm
+     * @return string
+     */
+    private function buildQuery(array $array, string $searchTerm): string
+    {
+        $first = true;
+        $queryString = '';
+        $searchTerms = explode(' ', $searchTerm);
+
+        foreach ($searchTerms as $searchTerm) {
+            $searchTerm = DBHelper::connection()->getPdo()->quote('%'.$searchTerm.'%');
+
+            foreach ($array as $column) {
+                if ($first) {
+                    $first = false;
+                } else {
+                    $queryString .= ' OR ';
+                }
+                $queryString .= $column.' LIKE '.$searchTerm;
+            }
+        }
+
+        return $queryString;
     }
 }

app/Helpers/SearchHelper.php
@@ -6,19 +6,20 @@ use function Safe\preg_match;
 use App\Models\Contact\Contact;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Contact\ContactFieldType;
+use Illuminate\Database\Eloquent\Builder;
 
 class SearchHelper
 {
     /**
      * Search contacts by the given query.
      *
-     * @param  string $query
-     * @param  int $limitPerPage
-     * @return mixed
+     * @param  string $needle
+     * @param  string $orderByColumn
+     * @param  string $orderByDirection
+     * @return Builder
      */
-    public static function searchContacts($query, $limitPerPage, $order)
+    public static function searchContacts(string $needle, string $orderByColumn, string $orderByDirection = 'asc'): Builder
     {
-        $needle = $query;
         $accountId = Auth::user()->account_id;
 
         if (preg_match('/(.{1,})[:](.{1,})/', $needle, $matches)) {
@@ -31,17 +32,16 @@ class SearchHelper
 
             $field_id = is_null($field) ? 0 : $field->id;
 
-            $results = Contact::whereHas('contactFields', function ($query) use ($accountId, $field_id, $search_term) {
+            return Contact::whereHas('contactFields', function ($query) use ($accountId, $field_id, $search_term) {
                 $query->where([
                     ['account_id', $accountId],
                     ['data', 'like', "$search_term%"],
                     ['contact_field_type_id', $field_id],
                 ]);
-            })->paginate($limitPerPage);
-        } else {
-            $results = Contact::search($needle, $accountId, $limitPerPage, $order, 'AND '.DBHelper::getTable('contacts').'.`is_partial` = FALSE');
+            })
+                ->orderBy($orderByColumn, $orderByDirection);
         }
 
-        return $results;
+        return Contact::search($needle, $accountId, $orderByColumn, $orderByDirection);
     }
 }

app/Helpers/StringHelper.php
@@ -5,34 +5,6 @@ namespace App\Helpers;
 class StringHelper
 {
     /**
-     * Build a query based on the array that contains column names.
-     *
-     * @param  array  $array
-     * @return string
-     */
-    public static function buildQuery(array $array, string $searchTerm)
-    {
-        $first = true;
-        $queryString = '';
-        $searchTerms = explode(' ', $searchTerm);
-
-        foreach ($searchTerms as $searchTerm) {
-            $searchTerm = DBHelper::connection()->getPdo()->quote('%'.$searchTerm.'%');
-
-            foreach ($array as $column) {
-                if ($first) {
-                    $first = false;
-                } else {
-                    $queryString .= ' or ';
-                }
-                $queryString .= $column.' LIKE '.$searchTerm;
-            }
-        }
-
-        return $queryString;
-    }
-
-    /**
      * Test if string is null or whitespace.
      *
      * @param  mixed $text

app/Http/Controllers/Api/ApiContactController.php
@@ -50,9 +50,11 @@ class ApiContactController extends ApiController
             try {
                 $contacts = SearchHelper::searchContacts(
                     $needle,
-                    $this->getLimitPerPage(),
-                    $this->sort.' '.$this->sortDirection
-                );
+                    $this->sort,
+                    $this->sortDirection
+                )
+                    ->real()
+                    ->paginate($this->getLimitPerPage());
             } catch (QueryException $e) {
                 return $this->respondInvalidQuery();
             }

app/Http/Controllers/Contacts/IntroductionsController.php
@@ -21,8 +21,14 @@ class IntroductionsController extends Controller
      */
     public function edit(Contact $contact)
     {
+        $contacts = auth()->user()->account->contacts()
+                        ->real()
+                        ->active()
+                        ->get();
+
         return view('people.introductions.edit')
-            ->withContact($contact);
+            ->withContact($contact)
+            ->withContacts($contacts);
     }
 
     /**

app/Http/Controllers/Contacts/RelationshipsController.php
@@ -32,7 +32,9 @@ class RelationshipsController extends Controller
      */
     public function create(Request $request, Contact $contact)
     {
-        $existingContacts = Contact::search('', Auth::user()->account_id, 20, 'updated_at', 'AND `id` != '.$contact->id);
+        $existingContacts = Contact::search('', Auth::user()->account_id, 'updated_at')
+            ->whereNotIn('id', [$contact->id])
+            ->paginate(20);
 
         return view('people.relationship.new')
             ->withContact($contact)

app/Http/Controllers/DashboardController.php
@@ -28,7 +28,12 @@ class DashboardController extends Controller
             )->with('debts.contact')
             ->first();
 
-        if ($account->contacts()->real()->active()->count() === 0) {
+        $numberOfContacts = $account->contacts()
+                                    ->real()
+                                    ->active()
+                                    ->count();
+
+        if ($numberOfContacts === 0) {
             return view('dashboard.blank');
         }
 
@@ -53,14 +58,14 @@ class DashboardController extends Controller
             $lastUpdatedContactsCollection->push(json_encode($data));
         }
 
-        $debt = $account->debts->where('status', 'inprogress');
+        $debts = $account->debts()->inProgress();
 
-        $debt_due = $debt->where('in_debt', 'yes')
+        $debt_due = $debts->due()->get()
             ->reduce(function ($totalDueDebt, Debt $debt) {
                 return $totalDueDebt + $debt->amount;
             }, 0);
 
-        $debt_owed = $debt->where('in_debt', 'no')
+        $debt_owed = $debts->owed()->get()
             ->reduce(function ($totalOwedDebt, Debt $debt) {
                 return $totalOwedDebt + $debt->amount;
             }, 0);
@@ -77,7 +82,7 @@ class DashboardController extends Controller
 
         $data = [
             'lastUpdatedContacts' => $lastUpdatedContactsCollection,
-            'number_of_contacts' => $account->contacts()->real()->active()->count(),
+            'number_of_contacts' => $numberOfContacts,
             'number_of_reminders' => $account->reminders_count,
             'number_of_notes' => $account->notes_count,
             'number_of_activities' => $account->activities_count,
@@ -85,7 +90,7 @@ class DashboardController extends Controller
             'number_of_tasks' => $account->tasks_count,
             'debt_due' => $debt_due,
             'debt_owed' => $debt_owed,
-            'debts' => $debt,
+            'debts' => $debts,
             'user' => auth()->user(),
             'changelogs' => $changelogs,
             'reminderOutboxes' => $reminderOutboxes,

commit e986085da4d9428526b2e8a819a48f9ff15be54e

chore: fix search broken in #3852

app/Http/Controllers/ContactsController.php
@@ -718,7 +718,7 @@ class ContactsController extends Controller
         $perPage = $request->has('perPage') ? $request->input('perPage') : config('monica.number_of_contacts_pagination');
 
         // search contacts
-        $contacts = $contacts->search($request->has('search') ? $request->input('search') : '', $accountId, 'is_starred', 'desc', $sort)
+        $contacts = $contacts->search($request->input('search') ?? '', $accountId, 'is_starred', 'desc', $sort)
             ->paginate($perPage);
 
         return [

app/Traits/Searchable.php
@@ -30,7 +30,8 @@ trait Searchable
 
         $queryString = $this->buildQuery($searchableColumns, $needle);
 
-        $builder->whereRaw('('.$queryString.')')
+        $builder->whereRaw(DBHelper::getTable($this->getTable()).".`account_id` = $accountId")
+            ->whereRaw("( $queryString )")
             ->orderBy($orderByColumn, $orderByDirection);
 
         if ($sortOrder) {