2dca313822823a3b2a47f90dc35102aedcbc2d68
mechanarchy <1166756+mechanarchy@users.noreply.github.com>
2020-04-26 15:23:36 +1000

    Add additional tests for SendTestEmail

--- tests/Commands/SendTestEmailTest.php
+++ tests/Commands/SendTestEmailTest.php
@@ -25,6 +25,42 @@ class SendTestEmailTest extends TestCase
         $this->assertEquals(-1, $exitCode);
     }
 
+    /** @test */
+    public function command_prompts_for_email()
+    {
+        $this->withoutMockingConsoleOutput();
 
+        $command = m::mock('\App\Console\Commands\SendTestEmail[ask]');
 
+        $command
+            ->shouldReceive('ask')
+            ->once()
+            ->with('What email address should I send the test email to?');
+
+        $this->app['Illuminate\Contracts\Console\Kernel']->registerCommand($command);
+
+        $exitCode = $this->artisan('monica:test-email');
+        $this->assertEquals(-1, $exitCode);
+    }
+
+    /** @test */
+    public function command_attempts_to_send_email()
+    {
+        $this->withoutMockingConsoleOutput();
+
+        $exampleEmail = 'test@example.org';
+
+        $externalMock = m::mock('overload:\Illuminate\Support\Facades\Mail');
+        $externalMock
+            ->shouldReceive('raw')
+            ->once()
+            ->with('Hi '.$exampleEmail.', you requested a test email from Monica.', m::any());
+
+        $command = m::mock('\App\Console\Commands\SendTestEmail[Mail::raw]');
+
+        $this->app['Illuminate\Contracts\Console\Kernel']->registerCommand($command);
+
+        $exitCode = $this->artisan('monica:test-email --email '.$exampleEmail);
+        $this->assertEquals(0, $exitCode);
+    }
 }
