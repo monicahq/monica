a1d9a4e31bf72bf1c2eb17e557141b1942a2c3c2
Alexis Saettler <alexis@saettler.org>
2020-04-29 22:15:52 +0200

    fix oauth test

--- tests/ApiTestCase.php
+++ tests/ApiTestCase.php
@@ -2,82 +2,13 @@
 
 namespace Tests;
 
+use Tests\Traits\Asserts;
 use Tests\Traits\ApiSignIn;
-use Illuminate\Testing\TestResponse;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ApiTestCase extends TestCase
 {
     use ApiSignIn,
+        Asserts,
         DatabaseTransactions;
-
-    /**
-     * Test that the response contains a not found notification.
-     *
-     * @param TestResponse $response
-     */
-    public function expectNotFound(TestResponse $response)
-    {
-        $response->assertStatus(404);
-
-        $response->assertJson([
-            'error' => [
-                'message' => 'The resource has not been found',
-                'error_code' => 31,
-            ],
-        ]);
-    }
-
-    /**
-     * Test that the response contains a not authorized notification.
-     *
-     * @param TestResponse $response
-     */
-    public function expectNotAuthorized(TestResponse $response)
-    {
-        $response->assertStatus(401);
-
-        $response->assertJson([
-            'error' => [
-                'message' => 'Not authorized',
-                'error_code' => 42,
-            ],
-        ]);
-    }
-
-    /**
-     * Test that the response contains a data error notification.
-     *
-     * @param TestResponse $response
-     * @param string|array $message
-     */
-    public function expectDataError(TestResponse $response, $message = '')
-    {
-        $response->assertStatus(422);
-
-        $response->assertJson([
-            'error' => [
-                'message' => $message,
-                'error_code' => 32,
-            ],
-        ]);
-    }
-
-    /**
-     * Test that the response contains an invalid parameter notification.
-     *
-     * @param TestResponse $response
-     * @param string|array $message
-     */
-    public function expectInvalidParameter(TestResponse $response, $message = '')
-    {
-        $response->assertStatus(422);
-
-        $response->assertJson([
-            'error' => [
-                'message' => $message,
-                'error_code' => 41,
-            ],
-        ]);
-    }
 }
--- tests/FeatureTestCase.php
+++ tests/FeatureTestCase.php
@@ -3,28 +3,12 @@
 namespace Tests;
 
 use Tests\Traits\SignIn;
-use Illuminate\Testing\TestResponse;
+use Tests\Traits\Asserts;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class FeatureTestCase extends TestCase
 {
     use SignIn,
+        Asserts,
         DatabaseTransactions;
-
-    /**
-     * Test that the response contains a not found notification.
-     *
-     * @param TestResponse $response
-     */
-    public function expectNotFound(TestResponse $response)
-    {
-        $response->assertStatus(404);
-
-        $response->assertJson([
-            'error' => [
-                'message' => 'The resource has not been found',
-                'error_code' => 31,
-            ],
-        ]);
-    }
 }
