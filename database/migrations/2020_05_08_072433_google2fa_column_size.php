a10c338bb02cc75c2868c81032dd0b8db6ed8da5
Alexis Saettler <alexis@saettler.org>
2020-05-08 07:28:24 +0000

    Apply fixes from StyleCI

--- database/migrations/2020_05_08_072433_google2fa_column_size.php
+++ database/migrations/2020_05_08_072433_google2fa_column_size.php
@@ -1,8 +1,7 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Migrations\Migration;
 
 class Google2faColumnSize extends Migration
 {
