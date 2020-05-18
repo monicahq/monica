1d0cfcfccb1ea8fd3d59bd6140c922878fd37337
Alexis Saettler <alexis@saettler.org>
2020-05-18 23:22:43 +0200

    chore(i18n): update i18n (#4037)

--- resources/lang/vendor/webauthn/zh/errors.php
+++ resources/lang/vendor/webauthn/zh/errors.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'user_unauthenticated' => 'You need to log in before doing a Webauthn authentication',
-    'auth_data_not_found' => 'Authentication data not found',
-    'create_data_not_found' => 'Register data not found',
-    'object_not_found' => 'Object not found',
+    'user_unauthenticated' => ' Webauthn 认证之前您需要先登录',
+    'auth_data_not_found' => '找不到身份验证数据',
+    'create_data_not_found' => '未找到注册数据',
+    'object_not_found' => '未找到对象',
 
 ];
