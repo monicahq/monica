954552e0e6743e617138b2b2297dce3ddf400d0c
P-Shakibafar <mehrdad.shakibafar@gmail.com>
2020-06-14 15:18:15 +0430

    refactor get signed policies method in ApiUserController.php

--- app/Http/Controllers/Api/Account/ApiUserController.php
+++ app/Http/Controllers/Api/Account/ApiUserController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Api\Account;
 
-use App\Models\User\User;
 use App\Helpers\DateHelper;
 use Illuminate\Http\Request;
 use App\Models\Settings\Term;
@@ -15,138 +14,124 @@ use App\Http\Controllers\Api\ApiController;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use App\Http\Resources\Account\User\User as UserResource;
 use App\Http\Resources\Settings\Compliance\Compliance as ComplianceResource;
+use function auth;
 
-class ApiUserController extends ApiController
-{
+class ApiUserController extends ApiController {
+
     /**
      * Get the detail of the authenticated user.
      *
      * @param Request $request
-     *
      * @return UserResource
      */
-    public function show(Request $request): UserResource
+    public function show( Request $request ) : UserResource
     {
-        return new UserResource(auth()->user());
+        return new UserResource( auth() -> user() );
     }
 
     /**
      * Get the state of a specific term for the user.
      *
      * @param Request $request
-     * @param int $termId
-     *
+     * @param int     $termId
      * @return JsonResponse
      */
-    public function get(Request $request, $termId)
+    public function get( Request $request, $termId )
     {
         try {
-            $term = Term::findOrFail($termId);
-        } catch (ModelNotFoundException $e) {
-            return $this->respondNotFound();
+            $term = Term ::findOrFail( $termId );
+        } catch( ModelNotFoundException $e ) {
+            return $this -> respondNotFound();
         }
-
-        $termUser = DB::table('term_user')->where('user_id', auth()->user()->id)
-            ->where('account_id', auth()->user()->account_id)
-            ->where('term_id', $term->id)
-            ->first();
-
-        if ($termUser) {
+        $termUser = DB ::table( 'term_user' ) -> where( 'user_id', auth() -> id() )
+                       -> where( 'account_id', auth() -> user() -> account_id )
+                       -> where( 'term_id', $term -> id )
+                       -> first();
+        if( $termUser ) {
             $data = [
-                'signed' => true,
-                'signed_date' => DateHelper::getTimestamp($termUser->created_at),
-                'ip_address' => $termUser->ip_address,
-                'user' => new UserResource(auth()->user()),
-                'term' => new ComplianceResource($term),
+                'signed'      => TRUE,
+                'signed_date' => DateHelper ::getTimestamp( $termUser -> created_at ),
+                'ip_address'  => $termUser -> ip_address,
+                'user'        => new UserResource( auth() -> user() ),
+                'term'        => new ComplianceResource( $term ),
             ];
         } else {
-            return $this->respondNotFound();
+            return $this -> respondNotFound();
         }
 
-        return $this->respond([
+        return $this -> respond( [
             'data' => $data,
-        ]);
+        ] );
     }
 
     /**
      * Get all the policies ever signed by the authenticated user.
      *
      * @param Request $request
-     *
      * @return JsonResponse
      */
-    public function getSignedPolicies(Request $request)
+    public function getSignedPolicies( Request $request )
     {
-        $terms = collect();
-        $termsForUser = DB::table('term_user')
-            ->where('user_id', auth()->user()->id)
-            ->get();
-
-        if ($termsForUser->count() == 0) {
-            return $this->respondNotFound();
+        $terms        = collect();
+        $termsForUser = auth() -> user() -> terms;
+        if( $termsForUser -> count() == 0 ) {
+            return $this -> respondNotFound();
         }
+        $termsForUser -> flatten() -> map( function ( $term ) use ( $terms ) {
+            $terms -> push( [
+                'signed'      => TRUE,
+                'signed_date' => DateHelper ::getTimestamp( $term -> created_at ),
+                'ip_address'  => $term -> pivot -> ip_address,
+                'user'        => new UserResource( auth() -> user() ),
+                'term'        => new ComplianceResource( $term ),
+            ] );
+        } );
 
-        foreach ($termsForUser as $termUser) {
-            $term = Term::findOrFail($termUser->term_id);
-
-            $terms->push([
-                'signed' => true,
-                'signed_date' => DateHelper::getTimestamp($termUser->created_at),
-                'ip_address' => $termUser->ip_address,
-                'user' => new UserResource(auth()->user()),
-                'term' => new ComplianceResource($term),
-            ]);
-        }
-
-        return $this->respond([
+        return $this -> respond( [
             'data' => $terms,
-        ]);
+        ] );
     }
 
     /**
      * Sign the latest policy for the authenticated user.
      *
      * @param Request $request
-     *
      * @return JsonResponse
      */
-    public function set(Request $request)
+    public function set( Request $request )
     {
-        $validator = Validator::make($request->all(), [
+        $validator = Validator ::make( $request -> all(), [
             'ip_address' => 'required',
-        ]);
-
-        if ($validator->fails()) {
-            return $this->respondValidatorFailed($validator);
+        ] );
+        if( $validator -> fails() ) {
+            return $this -> respondValidatorFailed( $validator );
         }
-
         try {
-            $term = app(AcceptPolicy::class)->execute([
-                'account_id' => auth()->user()->account_id,
-                'user_id' => auth()->user()->id,
-                'ip_address' => $request->input('ip_address'),
-            ]);
-        } catch (QueryException $e) {
-            return $this->respondInvalidQuery();
+            $term = app( AcceptPolicy::class ) -> execute( [
+                'account_id' => auth() -> user() -> account_id,
+                'user_id'    => auth() -> id(),
+                'ip_address' => $request -> input( 'ip_address' ),
+            ] );
+        } catch( QueryException $e ) {
+            return $this -> respondInvalidQuery();
         }
-
         try {
-            $termUser = DB::table('term_user')->where('user_id', auth()->user()->id)
-                ->where('account_id', auth()->user()->account_id)
-                ->where('term_id', $term->id)
-                ->first();
-        } catch (ModelNotFoundException $e) {
-            return $this->respondInvalidQuery();
+            $termUser = DB ::table( 'term_user' ) -> where( 'user_id', auth() -> id() )
+                           -> where( 'account_id', auth() -> user() -> account_id )
+                           -> where( 'term_id', $term -> id )
+                           -> first();
+        } catch( ModelNotFoundException $e ) {
+            return $this -> respondInvalidQuery();
         }
 
-        return $this->respond([
+        return $this -> respond( [
             'data' => [
-                'signed' => true,
-                'signed_date' => DateHelper::getTimestamp($termUser->created_at),
-                'ip_address' => $termUser->ip_address,
-                'user' => new UserResource(auth()->user()),
-                'term' => new ComplianceResource($term),
+                'signed'      => TRUE,
+                'signed_date' => DateHelper ::getTimestamp( $termUser -> created_at ),
+                'ip_address'  => $termUser -> ip_address,
+                'user'        => new UserResource( auth() -> user() ),
+                'term'        => new ComplianceResource( $term ),
             ],
-        ]);
+        ] );
     }
 }
