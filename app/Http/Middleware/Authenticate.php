e9a69f93a6485cd0fcc35e5c4a99bd1c05176ce6
Alexis Saettler <alexis@saettler.org>
2020-09-07 17:59:15 +0200

    add static analysis

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,10 +12,11 @@ class Authenticate extends Middleware
      * @param  \Illuminate\Http\Request  $request
      * @return string|null
      */
-    protected function redirectTo($request)
+    protected function redirectTo($request): ?string
     {
         if (! $request->expectsJson()) {
             return route('login');
         }
+        return null;
     }
 }
