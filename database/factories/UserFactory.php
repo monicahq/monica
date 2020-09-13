0d927e9424f2e663627eab8089d9c61bf2e4f406
Alexis Saettler <asbiin@users.noreply.github.com>
2020-09-13 20:36:07 +0000

    Apply fixes from StyleCI

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,8 +2,8 @@
 
 namespace Database\Factories;
 
-use App\Models\User;
 use App\Models\Account;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
