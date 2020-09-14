619ce5dcd91169c1a28423d76e2f7b475e1b2d51
Alexis Saettler <alexis@saettler.org>
2020-09-14 09:15:03 +0200

    change title and add more tests

--- config/database.php
+++ config/database.php
@@ -101,7 +101,7 @@ return [
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'strict' => false,
+            'strict' => true,
         ],
 
     ],
