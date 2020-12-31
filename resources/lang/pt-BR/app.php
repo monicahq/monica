ffdcdb454ade521f5d72ffd63dc9d8d8ddfca6be
Mike Graham <mike@mgrah.am>
2020-12-31 18:51:17 +0000

    fix: Corrected "Edition not allowed" to "Editing not allowed" globally The only file I didn't correct was `resources/lang/en/dashboard.php` as this is already being fixed by my earlier pull request #4741.

--- resources/lang/pt-BR/app.php
+++ resources/lang/pt-BR/app.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * ⚠️ Edition not allowed except for 'en' language.
+ * ⚠️ Editing not allowed except for 'en' language.
  *
  * @see https://github.com/monicahq/monica/blob/master/docs/contribute/translate.md for translations.
  */
