d05dce816880f7fe31a955f5ba08ff89e6bb0d48
Mike Graham <mike@mgrah.am>
2021-01-02 12:32:55 +0000

    Revert "fix: Corrected "Edition not allowed" to "Editing not allowed" globally"
    This reverts commit ffdcdb454ade521f5d72ffd63dc9d8d8ddfca6be.

--- resources/lang/de/logs.php
+++ resources/lang/de/logs.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * ⚠️ Editing not allowed except for 'en' language.
+ * ⚠️ Edition not allowed except for 'en' language.
  *
  * @see https://github.com/monicahq/monica/blob/master/docs/contribute/translate.md for translations.
  */
