a28adcdd7d13330271d198c470b6985eee39df11
Mike Graham <mike@mgrah.am>
2021-01-13 18:32:59 +0000

    feat: add Apple icons for use when bookmarking. (#4743)

--- resources/views/layouts/skeleton.blade.php
+++ resources/views/layouts/skeleton.blade.php
@@ -15,6 +15,14 @@
     @endif
 
     <link rel="shortcut icon" href="img/favicon.png">
+
+    <meta name="apple-mobile-web-app-title" content="Monica">
+    <link rel="apple-touch-icon" href="touch-icon-iphone.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="touch-icon-ipad.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="touch-icon-iphone-retina.png">
+    <link rel="apple-touch-icon" sizes="167x167" href="touch-icon-ipad-retina.png">
+
+
     <script>
       window.Laravel = {!! \Safe\json_encode([
           'locale' => \App::getLocale(),
