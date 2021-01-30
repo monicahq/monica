175b23130e6ce9c2bb305c9440fd319e1d2e181c
Alexis Saettler <alexis@saettler.org>
2021-01-30 20:43:24 +0100

    chore(i18n): update translations (#4829)

--- resources/lang/el/dashboard.php
+++ resources/lang/el/dashboard.php
@@ -9,34 +9,34 @@
 return [
     'dashboard_blank_title' => 'Welcome to your account!',
     'dashboard_blank_description' => 'Monica is the place to organize all the interactions you have with the people you care about.',
-    'dashboard_blank_cta' => 'Add your first contact',
+    'dashboard_blank_cta' => 'Προσθέστε την πρώτη σας επαφή',
     'dashboard_blank_illustration' => 'Illustration by <a href=":url">Freepik</a>',
 
     'notes_title' => 'You don’t have any starred notes yet.',
 
     'tab_recent_calls' => 'Recent calls',
-    'tab_favorite_notes' => 'Favorite notes',
-    'tab_calls_blank' => 'You haven’t logged any calls yet.',
-    'tab_debts' => 'Debts',
-    'tab_debts_blank' => 'You haven’t logged any debts yet.',
-    'tab_tasks' => 'Tasks',
-    'tab_tasks_blank' => 'You haven’t any tasks yet.',
+    'tab_favorite_notes' => 'Αγαπημένες σημειώσεις',
+    'tab_calls_blank' => 'Δεν έχετε καταγράψει καμία κλήση ακόμα.',
+    'tab_debts' => 'Χρέη',
+    'tab_debts_blank' => 'Δεν έχετε καταγράψει ακόμη χρέη.',
+    'tab_tasks' => 'Εργασίες',
+    'tab_tasks_blank' => 'Δεν έχετε ακόμα καμία εργασία.',
 
-    'tasks_add_task_placeholder' => 'What is this task about?',
-    'tasks_tab_your_contacts' => 'Tasks related to your contacts',
-    'tasks_tab_your_tasks' => 'Your tasks',
+    'tasks_add_task_placeholder' => 'Τι είναι αυτή η εργασία;',
+    'tasks_tab_your_contacts' => 'Εργασίες που σχετίζονται με τις επαφές σας',
+    'tasks_tab_your_tasks' => 'Οι εργασίες σας',
     'tasks_add_note' => 'Press <kbd>Enter</kbd> to add the task.',
     'task_add_cta' => 'Add a task',
 
-    'debts_you_owe' => 'You owe',
+    'debts_you_owe' => 'Χρωστάτε',
 
-    'statistics_contacts' => 'Contacts',
-    'statistics_activities' => 'Activities',
-    'statistics_gifts' => 'Gifts',
+    'statistics_contacts' => 'Επαφές',
+    'statistics_activities' => 'Δραστηριότητες',
+    'statistics_gifts' => 'Δώρα',
 
-    'reminders_next_months' => 'Events in the next 3 months',
-    'reminders_none' => 'No reminders for this month.',
+    'reminders_next_months' => 'Γεγονότα τους επόμενους 3 μήνες',
+    'reminders_none' => 'Δεν υπάρχουν υπενθυμίσεις για αυτό το μήνα.',
 
-    'product_changes' => 'Product changes',
-    'product_view_details' => 'View details',
+    'product_changes' => 'Αλλαγές προϊόντος',
+    'product_view_details' => 'Προβολή λεπτομερειών',
 ];
