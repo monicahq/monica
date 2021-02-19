c43eae59c0af5c27d5b3205b577257b1b8867f0b
Regis Freyd <djaiss@users.noreply.github.com>
2021-02-19 18:48:36 -0500

    Update resources/views/people/relationship/_relationship.blade.php
    Co-authored-by: Alexis Saettler <alexis@saettler.org>

--- resources/views/people/relationship/_relationship.blade.php
+++ resources/views/people/relationship/_relationship.blade.php
@@ -12,13 +12,13 @@
 
   {{-- AGE --}}
   @if ($relationship->ofContact->is_dead)
-  @if ($relationship->ofContact->deceasedDate)
-  <span class="{{ htmldir() == 'ltr' ? '' : 'fr' }}">({{ $relationship->ofContact->getAgeAtDeath() }})</span>
-  @endif
+    @if ($relationship->ofContact->deceasedDate)
+      <span class="{{ htmldir() == 'ltr' ? '' : 'fr' }}">({{ $relationship->ofContact->getAgeAtDeath() }})</span>
+    @endif
   @elseif ($relationship->ofContact->birthday_special_date_id)
-  @if ($relationship->ofContact->birthdate->getAge())
-  <span class="{{ htmldir() == 'ltr' ? '' : 'fr' }}">({{ $relationship->ofContact->birthdate->getAge() }})</span>
-  @endif
+    @if ($relationship->ofContact->birthdate->getAge())
+      <span class="{{ htmldir() == 'ltr' ? '' : 'fr' }}">({{ $relationship->ofContact->birthdate->getAge() }})</span>
+    @endif
   @endif
 
   <span class="silver fw3 ba br2 ph1 {{ htmldir() == 'ltr' ? '' : 'fr' }}">{{ $relationship->relationshipType->getLocalizedName(null, false, $relationship->ofContact->gender ? $relationship->ofContact->gender->type : null) }}</span>
