623f55d52f18349fafe3ea0e27971373cff127ec
Regis Freyd <djaiss@users.noreply.github.com>
2021-02-19 16:04:00 -0500

    style: adjust styling of relationships to make it clearer

--- resources/views/people/relationship/index.blade.php
+++ resources/views/people/relationship/index.blade.php
@@ -1,50 +1,54 @@
 @if ($modules->contains('key', 'love_relationships'))
-<div class="ba b--near-white br2 bg-gray-monica pa3 mb3 f6">
-  <div class="w-100 dt">
-    <div class="dtc">
-      <h3 class="f6 ttu normal">{{ trans('app.relationship_type_group_love') }}</h3>
+<div class="ba b--near-white br2 bg-gray-monica mb3 f6">
+  <div class="pa3">
+    <div class="w-100 dt">
+      <div class="dtc">
+        <h3 class="f6 ttu normal">{{ trans('app.relationship_type_group_love') }}</h3>
+      </div>
     </div>
+
+    @include('people.relationship._relationship', ['relationships' => $loveRelationships])
   </div>
 
-   @include('people.relationship._relationship', ['relationships' => $loveRelationships])
-
-
-  <p class="mb0">
+  <p class="mb0 pa2 pl3 bt b--near-white f6">
     <a href="{{ route('people.relationships.create', $contact) }}?type={{ $contact->account->getRelationshipTypeByType('partner')->id }}">{{ trans('app.add') }}</a>
   </p>
 </div>
 @endif
 
 @if ($modules->contains('key', 'family_relationships'))
-<div class="ba b--near-white br2 bg-gray-monica pa3 mb3 f6">
-  <div class="w-100 dt">
-    <div class="dtc">
-      <h3 class="f6 ttu normal">{{ trans('app.relationship_type_group_family') }}</h3>
+<div class="ba b--near-white br2 bg-gray-monica mb3 f6">
+  <div class="pa3">
+    <div class="w-100 dt">
+      <div class="dtc">
+        <h3 class="f6 ttu normal">{{ trans('app.relationship_type_group_family') }}</h3>
+      </div>
     </div>
+
+    @include('people.relationship._relationship', ['relationships' => $familyRelationships])
   </div>
 
-
-   @include('people.relationship._relationship', ['relationships' => $familyRelationships])
-
-   <p class="mb0">
+  <p class="mb0 pa2 pl3 bt b--near-white f6">
     <a href="{{ route('people.relationships.create', $contact) }}?type={{ $contact->account->getRelationshipTypeByType('child')->id }}">{{ trans('app.add') }}</a>
   </p>
 </div>
 @endif
 
 @if ($modules->contains('key', 'other_relationships'))
-<div class="ba b--near-white br2 bg-gray-monica pa3 mb3 f6">
-  <div class="w-100 dt">
-    <div class="dtc">
-      <h3 class="f6 ttu normal">{{ trans('app.relationship_type_group_other') }}</h3>
+<div class="ba b--near-white br2 bg-gray-monica mb3 f6">
+  <div class="pa3">
+    <div class="w-100 dt">
+      <div class="dtc">
+        <h3 class="f6 ttu normal">{{ trans('app.relationship_type_group_other') }}</h3>
+      </div>
     </div>
+
+    @include('people.relationship._relationship', ['relationships' => $friendRelationships])
+
+    @include('people.relationship._relationship', ['relationships' => $workRelationships])
   </div>
 
-   @include('people.relationship._relationship', ['relationships' => $friendRelationships])
-
-   @include('people.relationship._relationship', ['relationships' => $workRelationships])
-
-   <p class="mb0">
+  <p class="mb0 pa2 pl3 bt b--near-white f6">
     <a href="{{ route('people.relationships.create', $contact) }}?type={{ $contact->account->getRelationshipTypeByType('friend')->id }}">{{ trans('app.add') }}</a>
   </p>
 </div>
