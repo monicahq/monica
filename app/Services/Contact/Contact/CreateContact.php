2f943889fcb8143c23733f78b6bae120c31726f0
Michael Bianco <iloveitaly@gmail.com>
2021-04-24 09:12:53 -0600

    Create new contact field if email field exists when creating contact

--- app/Services/Contact/Contact/CreateContact.php
+++ app/Services/Contact/Contact/CreateContact.php
@@ -9,10 +9,12 @@ use App\Services\BaseService;
 use App\Helpers\AccountHelper;
 use function Safe\json_encode;
 use App\Models\Account\Account;
+use App\Models\Contact\ContactFieldType;
 use App\Models\Contact\Contact;
 use App\Jobs\AuditLog\LogAccountAudit;
 use App\Jobs\Avatars\GenerateDefaultAvatar;
 use App\Jobs\Avatars\GetAvatarsFromInternet;
+use App\Services\Contact\ContactField\CreateContactField;
 
 class CreateContact extends BaseService
 {
@@ -30,6 +32,7 @@ class CreateContact extends BaseService
             'middle_name' => 'nullable|string|max:255',
             'last_name' => 'nullable|string|max:255',
             'nickname' => 'nullable|string|max:255',
+            'email' => 'nullable|string|max:255',
             'gender_id' => 'nullable|integer|exists:genders,id',
             'description' => 'nullable|string|max:255',
             'is_partial' => 'nullable|boolean',
@@ -71,6 +74,7 @@ class CreateContact extends BaseService
             $data,
             [
                 'author_id',
+                'email',
                 'is_birthdate_known',
                 'birthdate_day',
                 'birthdate_month',
@@ -92,6 +96,8 @@ class CreateContact extends BaseService
         $this->updateBirthDayInformation($data, $contact);
 
         $this->updateDeceasedInformation($data, $contact);
+
+        $this->updateEmail($data, $contact);
 
         $this->generateUUID($contact);
 
@@ -157,6 +163,34 @@ class CreateContact extends BaseService
             'add_reminder' => $this->nullOrvalue($data, 'birthdate_add_reminder'),
             'is_deceased' => $data['is_deceased'],
         ]);
+    }
+
+    /**
+     * Adds a contact field containing the email address
+     *
+     * @param array $data
+     * @param Contact $contact
+     * @return void
+     */
+    private function updateEmail(array $data, Contact $contact)
+    {
+        $contactFieldType = ContactFieldType::where([
+            'account_id' => $data['account_id'],
+            'type' => ContactFieldType::EMAIL,
+        ])->first();
+
+        if(is_null($contactFieldType) || is_null($data['email'])) {
+            return;
+        }
+
+        $contactField = app(CreateContactField::class)->execute(
+                [
+                    'account_id' => $data['account_id'],
+                    'contact_id' => $contact->id,
+                    'contact_field_type_id' => $contactFieldType->id,
+                    'data' => $data['email']
+                ]
+        );
     }
 
     /**
