4d18fd1ff348f18c9e00c6078bb97a60fb556e08
Alexis Saettler <alexis@saettler.org>
2021-05-04 22:58:21 +0200

    style: fix email name signature (#5167)

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -2,7 +2,7 @@
 {{-- Header --}}
 @slot('header')
 @component('mail::header', ['url' => Str::of(config('app.url'))->ltrim('/')])
-{{ config('app.name') }}
+{{ config('app.display_name') }}
 @endcomponent
 @endslot
 
@@ -21,7 +21,7 @@
 {{-- Footer --}}
 @slot('footer')
 @component('mail::footer')
-© {{ date('Y') }} {{ config('app.name') }}. @lang('All rights reserved.')
+© {{ date('Y') }} {{ config('app.display_name') }}. @lang('All rights reserved.')
 @endcomponent
 @endslot
 @endcomponent
