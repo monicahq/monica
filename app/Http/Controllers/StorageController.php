f2b19a3ca2f2d33b27891d589876f8454657a71d
Alexis Saettler <alexis@saettler.org>
2021-05-18 23:53:57 +0200

    use etagconditionals

--- app/Http/Controllers/StorageController.php
+++ app/Http/Controllers/StorageController.php
@@ -9,12 +9,16 @@ use App\Helpers\StorageHelper;
 use Illuminate\Support\Carbon;
 use App\Models\Contact\Contact;
 use App\Models\Contact\Document;
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Response;
 use League\Flysystem\FileNotFoundException;
 
-class StorageController
+class StorageController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['setEtag', 'ifMatch', 'ifNoneMatch']);
+    }
+
     /**
      * Download file authorization.
      *
@@ -65,18 +69,13 @@ class StorageController
         try {
             $disk = StorageHelper::disk(config('filesystems.default'));
 
-            $etag = Cache::rememberForever('etag.'.$file, function () {
-                return '"'.Str::uuid().'"';
-            });
             $lastModified = Carbon::createFromTimestamp($disk->lastModified($file), 'UTC')->locale('en');
 
             $headers = [
-                'ETag' => $etag,
-                'Cache-Control' => config('filesystems.default_cache_control'),
                 'Last-Modified' => $lastModified->isoFormat('ddd\, DD MMM YYYY HH\:mm\:ss \G\M\T'),
             ];
 
-            if (! $this->checkConditions($request, $etag, $lastModified)) {
+            if (! $this->checkConditions($request, $lastModified)) {
                 return Response::noContent(304, $headers)->setNotModified();
             }
 
@@ -86,58 +85,9 @@ class StorageController
         }
     }
 
-    private function checkConditions(Request $request, string $etag, Carbon $lastModified): bool
+    private function checkConditions(Request $request, Carbon $lastModified): bool
     {
-        if ($ifMatch = $request->header('If-Match')) {
-            // Only need to check entity tags if they are not *
-            if ($ifMatch !== '*') {
-                // There can be multiple ETags
-                $ifMatch = explode(',', $ifMatch);
-                $haveMatch = false;
-
-                foreach ($ifMatch as $ifMatchItem) {
-                    // Stripping any extra spaces
-                    $ifMatchItem = trim($ifMatchItem, ' ');
-
-                    if ($ifMatchItem === $etag) {
-                        $haveMatch = true;
-                        break;
-                    }
-                }
-                if (! $haveMatch) {
-                    abort(403, 'An If-Match header was specified, but none of the specified ETags matched.');
-                }
-            }
-        }
-
-        if ($ifNoneMatch = $request->header('If-None-Match')) {
-            // The If-None-Match header contains an ETag.
-            // If the ETag does match the current ETag, we return a 304.
-            // The header can also contain *, in which case it's always true.
-            $haveMatch = false;
-            if ($ifNoneMatch === '*') {
-                $haveMatch = true;
-            } else {
-                // There might be multiple ETags
-                $ifNoneMatch = explode(',', $ifNoneMatch);
-
-                foreach ($ifNoneMatch as $ifNoneMatchItem) {
-                    // Stripping any extra spaces
-                    $ifNoneMatchItem = trim($ifNoneMatchItem, ' ');
-
-                    if ($ifNoneMatchItem === $etag) {
-                        $haveMatch = true;
-                        break;
-                    }
-                }
-            }
-
-            if ($haveMatch) {
-                return false;
-            }
-        }
-
-        if (! $ifNoneMatch && ($ifModifiedSince = $request->header('If-Modified-Since'))) {
+        if (! $request->header('If-None-Match') && ($ifModifiedSince = $request->header('If-Modified-Since'))) {
             // The If-Modified-Since header contains a date. We will only
             // return the entity if it has been changed since that date.
             $date = Carbon::parse($ifModifiedSince);
