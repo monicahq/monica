c95a0092857e2184cdc1cf291c0f6fdb2581ebaa
Mike Graham <mike@mgrah.am>
2021-10-04 20:46:02 +0100

    style: add theme-color meta tag. (#5550)

--- resources/views/layouts/skeleton.blade.php
+++ resources/views/layouts/skeleton.blade.php
@@ -2,10 +2,16 @@
 <html lang="{{ \App::getLocale() }}" dir="{{ htmldir() }}">
   <head>
     <base href="{{ url('/') }}/" />
+
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="apple-mobile-web-app-title" content="Monica">
+    <meta name="application-name" content="Monica">
+    <meta name="theme-color" content="#325776">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <title>@yield('title', trans('app.application_title'))</title>
+
     <link rel="manifest" href="manifest.webmanifest">
 
     <link rel="stylesheet" href="{{ asset(mix('css/app-'.htmldir().'.css')) }}">
@@ -16,13 +22,11 @@
 
     <link rel="shortcut icon" href="img/favicon.png">
 
-    <meta name="apple-mobile-web-app-title" content="Monica">
     <link rel="apple-touch-icon" href="img/icons/touch-icon-iphone.png">
     <link rel="apple-touch-icon" sizes="152x152" href="img/icons/touch-icon-ipad.png">
     <link rel="apple-touch-icon" sizes="180x180" href="img/icons/touch-icon-iphone-retina.png">
     <link rel="apple-touch-icon" sizes="167x167" href="img/icons/touch-icon-ipad-retina.png">
 
-    <meta name="application-name" content="Monica">
     <link rel="shortcut icon" sizes="196x196" href="img/icons/favicon-196.png">
 
     <script>
