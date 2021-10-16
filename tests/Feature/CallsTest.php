faee8c72390f19681e49c486620c160fae49cf3c
Samuel Felipe <samfelgar@gmail.com>
2021-10-16 20:18:28 -0300

    Implement tests for the created endpoint

--- tests/Feature/CallsTest.php
+++ tests/Feature/CallsTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Helpers\DateHelper;
+use App\Services\Contact\Call\CreateCall;
 use Tests\FeatureTestCase;
 use App\Models\Contact\Call;
 use App\Models\Contact\Contact;
@@ -55,6 +57,54 @@ class CallsTest extends FeatureTestCase
             10,
             $response->decodeResponseJson()['data']
         );
+    }
+
+    /** @test */
+    public function it_gets_last_talked_to()
+    {
+        $user = $this->signin();
+
+        $contact = factory(Contact::class)->create([
+            'account_id' => $user->account_id,
+        ]);
+
+        $referenceDate = now();
+
+        app(CreateCall::class)->execute([
+            'account_id' => $user->account_id,
+            'contact_id' => $contact->id,
+            'called_at' => $referenceDate->format('Y-m-d'),
+        ]);
+
+        $response = $this->json('GET', "/people/{$contact->hashId()}/calls/last");
+
+        $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            'last_talked_to'
+        ]);
+
+        $this->assertEquals($response->json('last_talked_to'), DateHelper::getShortDate($referenceDate));
+    }
+
+    /** @test */
+    public function it_gets_a_empty_last_talked_to()
+    {
+        $user = $this->signin();
+
+        $contact = factory(Contact::class)->create([
+            'account_id' => $user->account_id,
+        ]);
+
+        $response = $this->json('GET', "/people/{$contact->hashId()}/calls/last");
+
+        $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            'last_talked_to'
+        ]);
+
+        $this->assertNull($response->json('last_talked_to'));
     }
 
     public function test_dashboard_calls()
