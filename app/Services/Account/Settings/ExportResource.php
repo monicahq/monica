2e843a7cf7a37586d13b5be9c37c5dda0122b78b
Alexis Saettler <alexis@saettler.org>
2021-11-12 22:02:35 +0100

    update

--- app/Services/Account/Settings/ExportResource.php
+++ app/Services/Account/Settings/ExportResource.php
@@ -11,18 +11,10 @@ use Illuminate\Http\Resources\DelegatesToResource;
 use Illuminate\Database\Eloquent\JsonEncodingException;
 use Illuminate\Http\Resources\ConditionallyLoadsAttributes;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Resources\Json\JsonResource;
 
-class ExportResource implements ArrayAccess, JsonSerializable, UrlRoutable
+class ExportResource extends JsonResource
 {
-    use ConditionallyLoadsAttributes, DelegatesToResource;
-
-    /**
-     * The resource instance.
-     *
-     * @var mixed
-     */
-    public $resource;
-
     /**
      * The resource instance.
      *
@@ -35,14 +27,7 @@ class ExportResource implements ArrayAccess, JsonSerializable, UrlRoutable
      *
      * @var array
      */
-    protected $properties;
-
-    /**
-     * The "data" wrapper that should be applied.
-     *
-     * @var string|null
-     */
-    // public static $wrap = null;
+    protected $properties = null;
 
     /**
      * Create a new resource instance.
@@ -56,44 +41,12 @@ class ExportResource implements ArrayAccess, JsonSerializable, UrlRoutable
     }
 
     /**
-     * Create a new anonymous resource collection.
-     *
-     * @param  mixed  $resource
-     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
-     */
-    public static function collection($resource)
-    {
-        return tap(new AnonymousResourceCollection($resource, static::class), function ($collection) {
-            if (property_exists(static::class, 'preserveKeys')) {
-                $collection->preserveKeys = (new static([]))->preserveKeys === true;
-            }
-        });
-    }
-
-    /**
-     * Resolve the resource to an array.
-     *
-     * @return array
-     */
-    public function resolve()
-    {
-        $data = $this->toArray();
-
-        if ($data instanceof Arrayable) {
-            $data = $data->toArray();
-        } elseif ($data instanceof JsonSerializable) {
-            $data = $data->jsonSerialize();
-        }
-
-        return $this->filter((array) $data);
-    }
-
-    /**
      * Transform the resource into an array.
      *
+     * @param  \Illuminate\Http\Request  $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
-    public function toArray()
+    public function toArray($request)
     {
         if (is_null($this->resource)) {
             return [];
@@ -101,7 +54,7 @@ class ExportResource implements ArrayAccess, JsonSerializable, UrlRoutable
 
         return is_array($this->resource)
             ? $this->resource
-            : $this->transform($this->columns, $this->properties, $this->data());
+            : $this->export($this->columns, $this->properties, $this->data());
     }
 
     /**
@@ -113,36 +66,6 @@ class ExportResource implements ArrayAccess, JsonSerializable, UrlRoutable
     }
 
     /**
-     * Convert the model instance to JSON.
-     *
-     * @param  int  $options
-     * @return string
-     *
-     * @throws \Illuminate\Database\Eloquent\JsonEncodingException
-     */
-    public function toJson($options = 0)
-    {
-        $json = json_encode($this->jsonSerialize(), $options);
-
-        if (JSON_ERROR_NONE !== json_last_error()) {
-            throw JsonEncodingException::forResource($this, json_last_error_msg());
-        }
-
-        return $json;
-    }
-
-    /**
-     * Prepare the resource for JSON serialization.
-     *
-     * @return array
-     */
-    #[\ReturnTypeWillChange]
-    public function jsonSerialize()
-    {
-        return $this->resolve();
-    }
-
-    /**
      * Create the Insert query for the given table.
      *
      * @param  array  $columns
@@ -150,7 +73,7 @@ class ExportResource implements ArrayAccess, JsonSerializable, UrlRoutable
      * @param  array  $data
      * @return array|null
      */
-    protected function transform(array $columns, array $properties = null, array $data = null): ?array
+    protected function export(array $columns, array $properties = null, array $data = null): ?array
     {
         $result = [];
 
