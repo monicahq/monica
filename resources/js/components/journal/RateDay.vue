348f48cece8d80271989d6e6839476fb82d9248f
Alexis Saettler <alexis@saettler.org>
2022-01-01 19:45:08 +0100

    Update resources/js/components/journal/RateDay.vue

--- resources/js/components/journal/RateDay.vue
+++ resources/js/components/journal/RateDay.vue
@@ -64,7 +64,8 @@
                 </svg>
 
                 <!-- mediocre day monochrome -->
-                <svg v-if="!showMediocreSmileyColor" v-cy-name="'medium-reaction-button'" width="42px" height="41px" viewBox="0 0 42 41" version="1.1"
+                <svg v-if="!showMediocreSmileyColor" v-cy-name="'medium-reaction-button'" width="42px" height="41px" viewBox="0 0 42 41"
+                     version="1.1"
                      xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
                      class="pointer" :class="{ selected: day.rate == 2 }" @click="showComment(2)" @mouseover="showMediocreSmileyColor = true"
                 >
