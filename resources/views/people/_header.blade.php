46b39185694835fbf9885ac369b43bc6d942e37a
Alexis Saettler <alexis@saettler.org>
2022-01-09 19:07:50 +0100

    refactor: optimize stayInTouch data passed

--- resources/views/people/_header.blade.php
+++ resources/views/people/_header.blade.php
@@ -111,7 +111,7 @@
         @if(!$contact->is_dead && ! $contact->isMe())
           <li class="mb2 mb0-ns di-ns db tc {{ htmldir() == 'ltr' ? 'mr3-ns' : 'ml3-ns' }}">
             @include('partials.icons.header_stayintouch')
-            <stay-in-touch :contact="{{ $contact }}" hash="{{ $contact->hashID() }}" :limited="{{ \Safe\json_encode($accountHasLimitations) }}"></stay-in-touch>
+            <stay-in-touch :first-name="'{{ $contact->first_name }}'" :frequency="{{ $contact->stay_in_touch_frequency ?? 'null' }}" :trigger-date="'{{ $contact->stay_in_touch_trigger_date !== null ? \App\Helpers\DateHelper::getTimestamp($contact->stay_in_touch_trigger_date) : null }}'" hash="{{ $contact->hashID() }}" :limited="{{ \Safe\json_encode($accountHasLimitations) }}"></stay-in-touch>
           </li>
         @endif
       </ul>
