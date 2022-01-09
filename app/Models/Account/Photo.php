7b378e21dc4e1557e1e36de36840322a1abcb9ff
Alexis Saettler <alexis@saettler.org>
2022-01-09 19:28:12 +0100

    fix

--- app/Models/Account/Photo.php
+++ app/Models/Account/Photo.php
@@ -7,6 +7,7 @@ use App\Helpers\StorageHelper;
 use App\Models\Contact\Contact;
 use App\Models\ModelBinding as Model;
 use Intervention\Image\Facades\Image;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Contracts\Filesystem\FileNotFoundException;
