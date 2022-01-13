defcf43e0a0e6dbe20ae3a1313d5afa1b7cf6a0c
Alexis Saettler <alexis@saettler.org>
2022-01-13 21:21:42 +0100

    fix: fix weather short date (#5901)

--- resources/views/people/profile.blade.php
+++ resources/views/people/profile.blade.php
@@ -51,7 +51,7 @@
             </div>
 
             <p class="mb0{{ $weather->created_at->between(now()->subHours(6), now()) ? '' : ' light-silver' }}"
-              title="{{ $weather->location }} &mdash; {{ \App\Helpers\DateHelper::getShortDateWithTime($weather->date) }}">
+              title="{{ $weather->location }} &mdash; {{ $weather->date !== null ? \App\Helpers\DateHelper::getShortDateWithTime($weather->date) : '' }}">
               {{ $weather->emoji }} {{ $weather->summary }} / {{ trans('app.weather_current_temperature_'.auth()->user()->temperature_scale, ['temperature' => $weather->temperature(auth()->user()->temperature_scale)]) }}
             </p>
           </div>
