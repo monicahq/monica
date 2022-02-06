e930afb76d95199c131c7417367c7c973fa5868d
Alexis Saettler <alexis@saettler.org>
2022-02-06 10:17:48 +0100

    fix: fix APP_TRUST_PROXIES (#5955)
    Signed-off-by: Alexis Saettler <alexis@saettler.org>

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Monicahq\Cloudflare\Http\Middleware\TrustProxies as Middleware;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * Get the trusted proxies.
+     *
+     * @return array|string|null
+     */
+    protected function proxies()
+    {
+        return config('app.trust_proxies');
+    }
+}
