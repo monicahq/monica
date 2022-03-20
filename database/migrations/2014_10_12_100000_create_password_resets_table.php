0a5a9747d421d2512e46af751c2cbd65d89b374b
Gavin Mogan <github@gavinmogan.com>
2022-03-20 15:39:56 -0700

    fix: Disable requiring primary key for migration
    Digital ocean hosted database servers have sql_require_primary_key enabled. As this table doesn't have a primary key at this point, it fails to run the migration.
    
    Add in a one line session handler to disable the check for this migration.
    
    I added @ just in case your using a server where you can't set that flag.

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -13,6 +13,7 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
+        @\Illuminate\Support\Facades\DB::statement('SET SESSION sql_require_primary_key=0');
         Schema::create('password_resets', function (Blueprint $table) {
             $table->string('email')->index();
             $table->string('token')->index();
