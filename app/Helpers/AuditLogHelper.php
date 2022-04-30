6b5f67e9e5f86b47c8bbcc115d16017eecb04f41
Alexis Saettler <alexis@saettler.org>
2022-04-30 23:51:37 +0200

    fix

--- app/Helpers/AuditLogHelper.php
+++ app/Helpers/AuditLogHelper.php
@@ -11,7 +11,7 @@ class AuditLogHelper
     /**
      * Prepare a collection of audit logs that is displayed on the Settings page.
      *
-     * @param  \Illuminate\Contracts\Pagination\LengthAwarePaginator  $logs
+     * @param  \Illuminate\Contracts\Pagination\LengthAwarePaginator|Collection<array-key, \App\Models\Instance\AuditLog>  $logs
      * @return Collection
      */
     public static function getCollectionOfAudits($logs): Collection
