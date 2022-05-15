c64e5d3e8366d86720184e7babf4c64c13751d19
Mazarin <djaiss@users.noreply.github.com>
2022-05-15 07:23:49 -0400

    Update app/Models/Account/Account.php
    Co-authored-by: Alexis Saettler <alexis@saettler.org>

--- app/Models/Account/Account.php
+++ app/Models/Account/Account.php
@@ -83,7 +83,7 @@ class Account extends Model
     /**
      * The attributes that should be mutated to dates.
      *
-     * @var array
+     * @var array<string>
      */
     protected $dates = [
         'valid_until_at',
