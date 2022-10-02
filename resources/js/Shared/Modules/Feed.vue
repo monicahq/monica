bcccbad9fadbe94e4e9a75ff1448b92c4ad6083c
Mazarin <djaiss@users.noreply.github.com>
2022-10-02 15:29:19 -0400

    feat: better layout for the feed meta data (monicahq/chandler#240)

--- resources/js/Shared/Modules/Feed.vue
+++ resources/js/Shared/Modules/Feed.vue
@@ -3,15 +3,15 @@
     <div class="ml-4 border-l border-gray-200 dark:border-gray-700">
       <div v-for="feedItem in feed" :key="feedItem.id" class="mb-8">
         <!-- action & user -->
-        <div class="mb-3 flex">
+        <div class="mb-2 flex">
           <div class="icon-avatar relative w-6">
             <avatar
               :data="feedItem.author.avatar"
               :classes="'rounded-full border-gray-200 dark:border-gray-800 border relative h-5 w-5'" />
           </div>
 
-          <div>
-            <p class="mr-2 inline text-gray-400">
+          <div class="flex w-full items-center justify-between">
+            <p class="mr-2 text-gray-400">
               <!-- author name + link to profile -->
               <inertia-link
                 v-if="feedItem.author.url"
@@ -24,9 +24,22 @@
               <!-- action done -->
               <span class="ml-2">{{ feedItem.sentence }}</span>
             </p>
-            <p class="mr-2 inline">•</p>
-            <p class="inline text-sm text-gray-400">
-              {{ feedItem.created_at }}
+
+            <p class="flex items-center text-sm text-gray-400">
+              <span>{{ feedItem.created_at }}</span>
+
+              <svg
+                class="ml-2 h-4 w-4"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor">
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
+              </svg>
             </p>
           </div>
         </div>
