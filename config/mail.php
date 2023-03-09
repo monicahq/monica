d67a3d7f6cda1b5c48feee82615743fd90a416d6
bbrendon <bbrendon@users.noreply.github.com>
2023-03-08 17:23:30 -0800

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -41,7 +41,7 @@ return [
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
-            'verify_peer' => env('MAIL_VERIFY_PEER', true),            
+            'verify_peer' => env('MAIL_VERIFY_PEER', true),
         ],
 
         'ses' => [
