03b148c6a9b9bd4e89c7d485a4b89d68cdb697e8
Alexis Saettler <alexis@saettler.org>
2023-04-09 20:29:54 +0200

    build: switch to yarn v2+ (monicahq/chandler#467)

--- tests/Feature/ZiggyVersionCheckTest.php
+++ tests/Feature/ZiggyVersionCheckTest.php
@@ -12,17 +12,17 @@ class ZiggyVersionCheckTest extends TestCase
      */
     public function it_ckecks_ziggy_version_are_same()
     {
-        //# Get composer ziggy version
+        // Get composer ziggy version
         exec('composer show tightenco/ziggy --format=json', $composer);
         $composerJson = json_decode(implode('', $composer));
 
         $composerVersion = Str::of($composerJson->versions[0])->trim('v');
 
-        //# Get yarn ziggy version
-        exec('yarn list --pattern ziggy-js --depth=0 --json --non-interactive --no-progress', $yarn);
+        // Get yarn ziggy version
+        exec('yarn info ziggy-js --json', $yarn);
         $yarnJson = json_decode(implode('', $yarn));
 
-        $yarnVersion = Str::of($yarnJson->data->trees[0]->name)->replace('ziggy-js@', '');
+        $yarnVersion = Str::of($yarnJson->value)->replace('ziggy-js@npm:', '');
 
         $this->assertEquals($composerVersion, $yarnVersion, 'Ziggy version for composer and yarn are not the same');
     }
