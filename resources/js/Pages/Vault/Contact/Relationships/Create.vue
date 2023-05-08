485f1d5e64dbdd957a44b3ca43c7134850df39e3
Mazarin <djaiss@users.noreply.github.com>
2023-05-08 08:58:55 -0400

    fix: fix text not appearing on dark mode (monicahq/chandler#496)

--- resources/js/Pages/Vault/Contact/Relationships/Create.vue
+++ resources/js/Pages/Vault/Contact/Relationships/Create.vue
@@ -312,7 +312,7 @@
                   name="create-contact"
                   type="checkbox"
                   class="focus:ring-3 relative h-4 w-4 rounded border border-gray-300 bg-gray-50 focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 focus:dark:ring-blue-600" />
-                <label for="create-contact" class="ml-2 block cursor-pointer text-sm text-gray-900">
+                <label for="create-contact" class="ml-2 block cursor-pointer text-sm text-gray-900 dark:text-white">
                   {{ $t('Create a contact entry for this person') }}
                 </label>
               </div>
