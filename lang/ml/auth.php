d2094d9f006fba858db70d5b6a9ce920353c7e5f
Mazarin <djaiss@users.noreply.github.com>
2023-05-10 19:26:16 -0400

    feat: add malayalam language (monicahq/chandler#514)

---
+++ lang/ml/auth.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'failed' => 'ലോഗിൻ വിജയകരമായില്ല.',
+    'lang' => 'മലയാളം',
+    'login_provider_azure' => 'Microsoft',
+    'login_provider_facebook' => 'Facebook',
+    'login_provider_github' => 'GitHub',
+    'login_provider_google' => 'Google',
+    'login_provider_linkedin' => 'LinkedIn',
+    'login_provider_saml2' => 'SAML 2.0 പ്രോവൈഡർ',
+    'login_provider_twitter' => 'Twitter',
+    'password' => 'അസാധുവായ പാസ്സ്‌വേഡ്.',
+    'throttle' => 'ലോഗിൻ നിങ്ങളുടെ അക്കൗണ്ടിന് നൽകിയ ശ്രമങ്ങൾ കൂടുതൽ ആയിരിക്കുന്നു. ദയവായി :seconds സെക്കൻഡ് ശേഷം ശ്രമിക്കുക.',
+];
---
+++ lang/ml/format.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'date' => 'd. MMM YYYY',
+    'day_month_parenthesis' => 'dddd (d. MMM)',
+    'day_number' => 'DD',
+    'full_date' => 'dddd, d. MMM YYYY',
+    'long_month_day' => 'd. MMMM',
+    'long_month_year' => 'MMMM Y',
+    'short_date' => 'D MMM',
+    'short_date_year_time' => 'd. MMM YYYY H:mm',
+    'short_day' => 'ddd',
+    'short_month' => 'MMM',
+    'short_month_year' => 'MMM Y',
+];
