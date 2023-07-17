0c69d70405e636592a5b948cb52c5dbc8b9b7457
khalil Abu jayab <74875624+khalilJayab@users.noreply.github.com>
2023-07-18 00:54:41 +0300

    chore: Change 'Apply_filter' to 'apply_filter' in EL lang

--- resources/lang/el/journal.php
+++ resources/lang/el/journal.php
@@ -28,7 +28,7 @@ return [
     'journal_blank_cta' => 'Προσθέστε την πρώτη καταχώρηση ημερολογίου',
     'journal_blank_description' => 'Το ημερολόγιο σας επιτρέπει να γράφετε γεγονότα που σας συνέβησαν και να τα θυμάστε.',
     'delete_confirmation' => 'Είστε βέβαιοι ότι θέλετε να διαγράψετε αυτή την καταχώρηση ημερολογίου;',
-    'Apply_filter' => 'Apply filter',
+    'apply_filter' => 'Apply filter',
     'start_date' => 'Start Date',
     'end_date' => 'End Date',
     'per_page' => 'Per Page',
