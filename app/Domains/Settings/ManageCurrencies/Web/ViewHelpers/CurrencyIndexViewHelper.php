fe799c0c99a9bd0252c3a7d50d0207632970f225
Regis Freyd <djaiss@users.noreply.github.com>
2023-11-07 08:10:21 -0500

    feat: order currencies by code

--- app/Domains/Settings/ManageCurrencies/Web/ViewHelpers/CurrencyIndexViewHelper.php
+++ app/Domains/Settings/ManageCurrencies/Web/ViewHelpers/CurrencyIndexViewHelper.php
@@ -10,6 +10,7 @@ class CurrencyIndexViewHelper
     public static function data(Account $account, int $currencyId = null): Collection
     {
         $currenciesCollection = $account->currencies()
+            ->orderBy('code')
             ->where('active', true)->get()->map(function ($currency) use ($currencyId) {
                 return [
                     'id' => $currency->id,
