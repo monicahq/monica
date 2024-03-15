707fd41eae10936533b1a864168d268c9085cd59
Alexis Saettler <alexis@saettler.org>
2024-03-15 20:16:43 +0100

    fix

--- tests/Unit/Commands/SetupDocumentationTest.php
+++ tests/Unit/Commands/SetupDocumentationTest.php
@@ -15,6 +15,12 @@ class SetupDocumentationTest extends TestCase
     #[Test]
     public function it_run_scribe_setup(): void
     {
+        if (version_compare(phpversion(), '8.3.0', '>=')) {
+            $this->markTestSkipped('This test is skipped for PHP 8.3.0 or higher');
+
+            return;
+        }
+
         $this->artisan('scribe:setup', ['--clean' => true, '--force' => true, '-q' => true])
             ->assertExitCode(0);
     }
