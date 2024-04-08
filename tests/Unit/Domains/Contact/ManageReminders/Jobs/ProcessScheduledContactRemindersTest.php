7f932bddc21f80247ba10e20130689715ab49c3a
Alexis Saettler <alexis@saettler.org>
2024-04-08 23:53:53 +0200

    fix: skip contactreminders for deleted contacts (#7223)

--- tests/Unit/Domains/Contact/ManageReminders/Jobs/ProcessScheduledContactRemindersTest.php
+++ tests/Unit/Domains/Contact/ManageReminders/Jobs/ProcessScheduledContactRemindersTest.php
@@ -6,8 +6,8 @@ use App\Domains\Contact\ManageReminders\Jobs\ProcessScheduledContactReminders;
 use App\Models\ContactReminder;
 use App\Models\UserNotificationChannel;
 use App\Notifications\ReminderTriggered;
-use Carbon\Carbon;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Notification;
 use Tests\TestCase;
@@ -74,6 +74,39 @@ class ProcessScheduledContactRemindersTest extends TestCase
             'scheduled_at' => Carbon::now()->addMinutes(10),
             'triggered_at' => null,
         ]);
+
+        $job = new ProcessScheduledContactReminders();
+        $job->dispatch();
+        $job->handle();
+
+        Notification::assertNothingSent();
+    }
+
+    /**
+     * @test
+     */
+    public function it_does_not_process_reminders_for_deleted_contacts(): void
+    {
+        Notification::fake();
+
+        Carbon::setTestNow(Carbon::create(2018, 1, 1, 0, 0, 0));
+
+        $contactReminder = ContactReminder::factory()->create([
+            'type' => ContactReminder::TYPE_RECURRING_DAY,
+            'label' => 'test',
+        ]);
+        $channel = UserNotificationChannel::factory()->create([
+            'type' => UserNotificationChannel::TYPE_EMAIL,
+            'content' => 'admin@admin.com',
+        ]);
+        DB::table('contact_reminder_scheduled')->insertGetId([
+            'user_notification_channel_id' => $channel->id,
+            'contact_reminder_id' => $contactReminder->id,
+            'scheduled_at' => Carbon::now(),
+            'triggered_at' => null,
+        ]);
+
+        $contactReminder->contact->delete();
 
         $job = new ProcessScheduledContactReminders();
         $job->dispatch();
