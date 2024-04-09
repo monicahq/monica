91c1773e11addbdd9db76829ce3569cde59f978c
Alexis Saettler <alexis@saettler.org>
2024-04-09 21:37:42 +0200

    fix: fix privacy policy and terms of service display (#7224)

---
+++ resources/js/Pages/PrivacyPolicy.vue
@@ -0,0 +1,23 @@
+<script setup>
+import AuthenticationCardLogo from '@/Components/AuthenticationCardLogo.vue';
+
+defineProps({
+  policy: String,
+});
+</script>
+
+<template>
+  <div class="font-sans text-gray-900 antialiased">
+    <div class="pt-4 bg-gray-100">
+      <div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0">
+        <div>
+          <AuthenticationCardLogo />
+        </div>
+
+        <div
+          class="w-full sm:max-w-2xl mt-6 p-6 bg-white shadow-md dark:shadow-gray-700 overflow-hidden sm:rounded-lg prose"
+          v-html="policy"></div>
+      </div>
+    </div>
+  </div>
+</template>
