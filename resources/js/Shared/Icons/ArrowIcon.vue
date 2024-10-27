cdb17147912f9864c830e5f48ffa51b523b84818
Mazarin <djaiss@users.noreply.github.com>
2024-10-27 10:26:21 -0400

    fix: fix icons not aligned (#7427)

--- resources/js/Shared/Icons/ArrowIcon.vue
+++ resources/js/Shared/Icons/ArrowIcon.vue
@@ -13,14 +13,14 @@ defineProps({
     viewBox="0 0 24 24"
     stroke-width="1.5"
     stroke="currentColor"
-    class="h-6 w-6 text-gray-600">
+    class="h-6 w-6 inline text-gray-600">
     <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
   </svg>
 
   <svg
     v-if="type == 'right' && size == 'big'"
     xmlns="http://www.w3.org/2000/svg"
-    class="h-6 w-6"
+    class="h-6 w-6 inline"
     fill="none"
     viewBox="0 0 24 24"
     stroke="currentColor"
@@ -36,14 +36,14 @@ defineProps({
     viewBox="0 0 24 24"
     stroke-width="1.5"
     stroke="currentColor"
-    class="h-6 w-6 text-gray-600">
+    class="h-6 w-6 inline text-gray-600">
     <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
   </svg>
 
   <svg
     v-if="type == 'left' && size == 'big'"
     xmlns="http://www.w3.org/2000/svg"
-    class="h-6 w-6"
+    class="h-6 w-6 inline"
     fill="none"
     viewBox="0 0 24 24"
     stroke="currentColor"
--- resources/js/Shared/Icons/CheckedIcon.vue
+++ resources/js/Shared/Icons/CheckedIcon.vue
@@ -1,7 +1,7 @@
 <template>
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    class="h-6 w-6 text-green-700"
+    class="h-6 w-6 inline text-green-700"
     fill="none"
     viewBox="0 0 24 24"
     stroke="currentColor">
