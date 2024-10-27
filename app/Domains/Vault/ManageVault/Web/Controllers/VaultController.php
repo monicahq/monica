96d74d9f63e32622d52922e7280446d402751dba
Regis Freyd <djaiss@users.noreply.github.com>
2024-10-27 08:43:29 -0400

    fix

--- app/Domains/Vault/ManageVault/Web/Controllers/VaultController.php
+++ app/Domains/Vault/ManageVault/Web/Controllers/VaultController.php
@@ -64,7 +64,6 @@ class VaultController extends Controller
     public function show(Request $request, Vault $vault)
     {
         $contact = Auth::user()->getContactInVault($vault);
-        dd(ModuleLifeEventViewHelper::data($contact, Auth::user()));
 
         return Inertia::render('Vault/Dashboard/Index', [
             'layoutData' => VaultIndexViewHelper::layoutData($vault),
