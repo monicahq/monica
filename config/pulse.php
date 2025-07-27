4358ce8ad2cf4d97aaca6430491637af13dc3870
Alexis Saettler <alexis@saettler.org>
2025-07-27 18:25:50 +0200

    fix

--- config/pulse.php
+++ config/pulse.php
@@ -43,7 +43,7 @@ return [
     |
     */
 
-    'enabled' => env('PULSE_ENABLED', true),
+    'enabled' => env('PULSE_ENABLED', false),
 
     /*
     |--------------------------------------------------------------------------
