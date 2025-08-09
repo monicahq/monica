b2194ae26063a4c023729b0271b2e5f100da50db
Alexis Saettler <alexis@saettler.org>
2025-08-09 19:11:07 +0200

    u

--- app/Domains/Contact/ManageContactImportantDates/Dav/ImportImportantDates.php
+++ app/Domains/Contact/ManageContactImportantDates/Dav/ImportImportantDates.php
@@ -112,7 +112,7 @@ class ImportImportantDates extends Importer implements ImportVCardResource
                 'contact_id' => $contact->id,
                 'contact_important_date_id' => $importantDate->id,
                 'contact_important_date_type_id' => $this->birthdateType->id,
-                'label' => $importantDate->label,
+                'label' => $importantDate->label || trans('Birthday', [], $this->author()->locale),
                 'day' => $importantDate->day,
                 'month' => $importantDate->month,
                 'year' => $importantDate->year,
