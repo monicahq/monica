c01651a56bbe7cf64781d111db258424cbf4290d
Alexis Saettler <alexis@saettler.org>
2025-08-09 23:21:29 +0200

    fix: fix import important date type (#7801)

--- tests/Unit/Domains/Contact/ManageContactImportantDates/Dav/ImportImportantDatesTest.php
+++ tests/Unit/Domains/Contact/ManageContactImportantDates/Dav/ImportImportantDatesTest.php
@@ -227,12 +227,13 @@ class ImportImportantDatesTest extends TestCase
         $contact = Contact::factory()->create([
             'vault_id' => $vault->id,
         ]);
-        ContactImportantDateType::factory()->create([
+        $type = ContactImportantDateType::factory()->create([
             'vault_id' => $vault->id,
         ]);
 
         ContactImportantDate::factory()->create([
             'contact_id' => $contact->id,
+            'contact_important_date_type_id' => $type->id,
         ]);
 
         $vcard = new VCard([
@@ -246,6 +247,7 @@ class ImportImportantDatesTest extends TestCase
         $this->assertEquals(6, $importantDate->day);
         $this->assertEquals(11, $importantDate->month);
         $this->assertEquals(2025, $importantDate->year);
+        $this->assertEquals($type->id, $importantDate->contact_important_date_type_id);
     }
 
     #[Group('dav')]
@@ -263,13 +265,17 @@ class ImportImportantDatesTest extends TestCase
         $contact = Contact::factory()->create([
             'vault_id' => $vault->id,
         ]);
-        ContactImportantDateType::factory()->create([
+        $type = ContactImportantDateType::factory()->create([
             'vault_id' => $vault->id,
         ]);
 
         ContactImportantDate::factory()->create([
             'contact_id' => $contact->id,
+            'contact_important_date_type_id' => $type->id,
         ]);
+
+        $this->assertCount(1, $contact->importantDates);
+
         $vcard = new VCard([
             'BDAY' => '19811029',
         ]);
@@ -281,5 +287,48 @@ class ImportImportantDatesTest extends TestCase
         $this->assertEquals(29, $importantDate->day);
         $this->assertEquals(10, $importantDate->month);
         $this->assertEquals(1981, $importantDate->year);
+        $this->assertEquals($type->id, $importantDate->contact_important_date_type_id);
+    }
+
+    #[Group('dav')]
+    #[Test]
+    public function it_imports_bday_update_type()
+    {
+        $user = $this->createUser();
+        $vault = $this->createVaultUser($user, Vault::PERMISSION_MANAGE);
+        $importVCard = new ImportVCard;
+        $importVCard->author = $user;
+        $importVCard->vault = $vault;
+        $importer = new ImportImportantDates;
+        $importer->setContext($importVCard);
+
+        $contact = Contact::factory()->create([
+            'vault_id' => $vault->id,
+        ]);
+        $type = ContactImportantDateType::factory()->create([
+            'vault_id' => $vault->id,
+        ]);
+
+        $importantDate = ContactImportantDate::factory()->create([
+            'contact_id' => $contact->id,
+            'contact_important_date_type_id' => null,
+        ]);
+
+        $this->assertNull($importantDate->contact_important_date_type_id);
+        $this->assertCount(1, $contact->importantDates);
+
+        $vcard = new VCard([
+            'BDAY' => '19811029',
+        ]);
+
+        $contact = $importer->import($vcard, $contact);
+        $contact->refresh();
+
+        $this->assertCount(1, $contact->importantDates);
+        $importantDate = $contact->importantDates->first();
+        $this->assertEquals(29, $importantDate->day);
+        $this->assertEquals(10, $importantDate->month);
+        $this->assertEquals(1981, $importantDate->year);
+        $this->assertEquals($type->id, $importantDate->contact_important_date_type_id);
     }
 }
