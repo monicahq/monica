75c96e72b61fdc833551f5f57f0483201a5dc9e9
Kilian Domaratius <github@kdomaratius.de>
2025-09-02 20:53:13 +0200

    fix: Align icons in contact cards

--- resources/js/Shared/Icons/PeopleIcon.vue
+++ resources/js/Shared/Icons/PeopleIcon.vue
@@ -1,7 +1,7 @@
 <template>
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    class="icon-sidebar relative inline h-4 w-4"
+    class="icon-sidebar relative inline me-1 h-4 w-4"
     fill="none"
     viewBox="0 0 24 24"
     stroke="currentColor"
