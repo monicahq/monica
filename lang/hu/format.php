9a7d04b7fb6ee7b1a2068e828ae484a83318ca19
Petrekanics Máté <mate.petrekanics@webcapital.hu>
2025-10-02 17:27:32 +0200

    feat(lang): added hungarian language

---
+++ lang/hu/format.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'short_date_year_time' => 'YYYY. MMM DD. HH:mm',
+    'short_date' => 'MMM DD.',
+    'short_month_year' => 'YYYY. MMM',
+    'long_month_year' => 'YYYY. MMMM',
+    'short_month' => 'MMM',
+    'long_month_day' => 'MMMM Do.',
+    'day_month_parenthesis' => 'dddd (MMM Do.)',
+    'date' => 'YYYY. MMM DD.',
+    'full_month' => 'MMMM',
+    'full_date' => 'dddd, YYYY. MMM Do.',
+    'day' => 'dddd',
+    'short_day' => 'ddd',
+    'day_number' => 'DD.',
+    'day_short_month' => 'MMM Do.',
+];
---
+++ lang/hu/passwords.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'reset' => 'A jelszavad vissza lett állítva.',
+    'sent' => 'Elküldtük a jelszó-visszaállító linket az emailedre.',
+    'throttled' => 'Kérlek, várj mielőtt újra próbálkozol.',
+    'token' => 'Ez a jelszó-visszaállító token érvénytelen.',
+    'user' => 'Nem találunk felhasználót ezzel az email címmel.',
+];
